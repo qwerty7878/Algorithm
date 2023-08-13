c26dac7e5b46a37a05588c94c9037a758fb0b601
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-13 23:14:31 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    int num,a,b;
+    scanf("%d",&num);
+    for(int i = 1; i <= num; i++) {
+        scanf("%d%d",&a,&b);
+        printf("%d\n",a+b);
+    }
+    return 0;
+}
