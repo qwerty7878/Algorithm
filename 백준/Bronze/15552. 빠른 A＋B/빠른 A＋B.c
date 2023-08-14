ff79c9118ed920fb6a3d7fad3871515dfa5a9630
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-14 13:26:56 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 356 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+    int T;
+    scanf("%d",&T);
+    for(int i; i < T; i++){
+        int a,b;
+        scanf("%d%d",&a,&b);
+        printf("%d\n",a+b);
+    }
+    return 0;
+}
