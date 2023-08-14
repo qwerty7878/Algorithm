48353ddaa4bec99e4a30c994b086f5b22d7ffd12
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-14 14:23:59 +0900

    [Bronze II] Title: 상수, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        
+        num1 = (num1%10)*100 + ((num1%100)/10)*10 + (num1/100);
+        num2 = (num2%10)*100 + ((num2%100)/10)*10 + (num2/100);
+        
+        System.out.println(num1 > num2 ? num1 : num2);
+    }
+}
