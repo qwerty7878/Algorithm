faffb58f4ae090b63ce74ac83e3af0bba6d9194f
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-14 14:24:51 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 244 ms, Memory: 18348 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int k = 1;
+		int q = 1;
+		int l = 2;
+		int b = 2;
+		int kn = 2;
+		int p = 8;
+		
+		k = k - sc.nextInt();
+		q = q - sc.nextInt();
+		l = l - sc.nextInt();
+		b = b - sc.nextInt();
+		kn = kn - sc.nextInt();
+		p = p - sc.nextInt();
+		
+		System.out.print(k + " ");
+		System.out.print(q + " ");
+		System.out.print(l + " ");
+		System.out.print(b + " ");
+		System.out.print(kn + " ");
+		System.out.print(p);
+		
+
+	}
+
+}
