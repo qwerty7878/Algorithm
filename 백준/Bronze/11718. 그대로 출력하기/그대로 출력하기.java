148adab1f796808aacddddf452ffa2e1c4edb758
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-14 14:24:22 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 232 ms, Memory: 17952 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String S = sc.nextLine();
+            System.out.println(S);
+        }
+    }
+}
