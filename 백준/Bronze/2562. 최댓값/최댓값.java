f30a9c850a496b48c4eb45ac25a2e5764b4972e2
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-14 14:19:49 +0900

    [Bronze III] Title: 최댓값, Time: 228 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[9];
+        int max = arr[0];
+        int count = 0;
+        
+        for(int i = 0; i < 9; i++){
+            arr[i] = sc.nextInt();
+            if(max < arr[i]){
+                max = arr[i];
+                count = i + 1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(count);
+    }
+}
