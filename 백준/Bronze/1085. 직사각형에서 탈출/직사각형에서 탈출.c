edfd8c86fb4c9d8e0987d535d2b67f95552a5ff6
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-20 23:38:52 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void){
+    int x,y,w,h;
+    scanf("%d%d%d%d",&x,&y,&w,&h);
+    
+    int min = x;
+    if(min > w - x)
+        min = w - x;
+    if(min > y)
+        min = y;
+    if(min > h - y)
+        min = h - y;
+    printf("%d\n",min);
+}
