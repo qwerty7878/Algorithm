cf1407f629f4bde83bb8d4f9b23556c105b51a29
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-20 20:54:48 +0900

    [Bronze I] Title: 세로읽기, Time: 216 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);       
+        char arr[][] = new char [5][15];
+        
+        for(int i = 0; i < 5; i++){
+            String str = sc.next();
+            for(int j = 0; j < str.length(); j++){
+                arr[i][j] = str.charAt(j);
+            }
+        }
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j < 5; j++){
+                if(arr[j][i] == ' ' || arr[j][i] == '\0')
+                    continue;
+                
+                System.out.print(arr[j][i]);
+            }
+        }
+    }
+}
