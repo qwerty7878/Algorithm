56b84fad0a11067aa9aa8d22c88a4d22b87558e3
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-20 14:52:59 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(void){
+    int T;
+    scanf("%d",&T);
+    
+    for(int i = 0; i < T; i++){
+        char str[1000] = "";
+        scanf("%s",str);
+        printf("%c%c\n",str[0],str[strlen(str)-1]);
+    }
+    return 0;
+}
