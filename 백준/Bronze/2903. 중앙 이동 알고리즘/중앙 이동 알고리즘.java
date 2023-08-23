e34f24fa178b884ed3abbcf6e2b1163273455656
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-23 23:47:00 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 204 ms, Memory: 17612 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -4,7 +4,8 @@ public class Main{
         Scanner sc = new Scanner(System.in);
         
         int n = sc.nextInt();
-        long dot = 1 + (long)Math.pow(2,n);
+        long dot = 1 + (long)Math.pow(2,n); // 2가 들어간 이유는 4 9 25... 로 제곱이므로
+                                            // = 2의 (n+1)승이란 뜻임
         System.out.println(dot * dot);
     }
 }
