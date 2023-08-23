06dea7e4562499cf890cfd1b99947dd568513d56
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-23 17:52:42 +0900

    [Bronze I] Title: 소인수분해, Time: 160 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class Main{
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 2; i <= n; i++){
+            while(n % i == 0){
+                System.out.println(i);
+                n /= i;
+            }
+        }
+    }
+}
