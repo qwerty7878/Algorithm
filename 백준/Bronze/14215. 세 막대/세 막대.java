793f671c72ab743a26b9dabcfc8cd6cf78a17d77
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-23 17:28:54 +0900

    [Bronze III] Title: 세 막대, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int arr[] = new int [3];
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+        arr[2] = sc.nextInt();
+        
+        Arrays.sort(arr);
+        
+        if(arr[0]+arr[1] > arr[2]){
+            System.out.println(arr[0] + arr[1] + arr[2]);
+        }
+        else
+            System.out.println((arr[0] + arr[1])*2 - 1);
+    }
+}
