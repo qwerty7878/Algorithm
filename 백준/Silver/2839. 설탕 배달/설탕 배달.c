1dd85d695d7b6c5e73f3d6261ed702379a30a178
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-08-30 17:52:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void){
+        int n;
+        scanf("%d",&n);
+        
+        if(n == 4 || n == 7)
+            printf("-1");
+        else if(n % 5 == 0)
+            printf("%d",n/5);
+        else if(n % 5 == 1 || n % 5 == 3)
+            printf("%d",n/5 + 1);
+        else if(n % 5 == 2 || n % 5 == 4)
+            printf("%d",n/5 + 2);
+            return 0;
+}
