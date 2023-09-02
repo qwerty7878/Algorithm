68a08c443d3a1ea07a1884853fe41791f3c3f8d8
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-09-02 15:49:09 +0900

    [Silver V] Title: 소트인사이드, Time: 200 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+        char arr[] = n.toCharArray();
+        Arrays.sort(arr);
+        
+        for(int i = arr.length-1; i >= 0; i--){
+            System.out.print(arr[i]);
+        }
+    }
+}
