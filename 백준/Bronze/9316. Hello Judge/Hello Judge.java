8b33fa893f68dcf5945fa290cca00a2d548595e3
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-09-24 17:47:38 +0900

    [Bronze IV] Title: Hello Judge, Time: 236 ms, Memory: 18308 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 1; i <= t; i++){
+            System.out.println("Hello World, Judge " + i + "!");
+        }
+    }
+}
