c69c230438c8428df645bf23584c20ed027c4291
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-09-26 20:16:57 +0900

    [Bronze IV] Title: Tri-du, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b)
+        printf("%d\n",a);
+    else if(a<b)
+        printf("%d\n",b);
+    else
+        printf("%d\n",a);
+    return 0;
+}
