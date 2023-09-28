8f6bd41f029c2bf81e3f359375e059da9a6a2670
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-09-28 23:57:01 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(void){
+
+    int n;
+    int cnt;
+    for(int i = 0; i < 3; i++){
+        cnt = 0;
+        for(int j = 0; j < 4; j++){
+            scanf("%d",&n);
+            if(n == 0)
+                cnt++;
+        }
+        if(cnt == 0)
+            printf("E\n");
+        else if(cnt == 1)
+            printf("A\n");
+        else if(cnt == 2)
+            printf("B\n");
+        else if(cnt == 3)
+            printf("C\n");
+        else if(cnt == 4)
+            printf("D\n");
+    }
+    
+    return 0;
+}
