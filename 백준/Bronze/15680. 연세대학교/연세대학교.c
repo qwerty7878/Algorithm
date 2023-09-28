99d502e74fd0349e1ab786fe1c5835bd2694a85f
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-09-28 22:38:55 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main(void){
+    int t;
+    scanf("%d",&t);
+    if(t == 0)
+        printf("YONSEI\n");
+    else if(t == 1)
+        printf("Leading the Way to the Future\n");
+    return 0;
+}
