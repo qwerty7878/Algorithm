6939170cd8db2404ae9370c353432a50a4c7c888
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-10-01 18:31:32 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 112 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,9 @@
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("2023");
+        System.out.println("10");
+        System.out.println("01");
+        
+    }
+}
