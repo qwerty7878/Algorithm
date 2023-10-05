76118f2a615578c51e119127381a2b0205e92144
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-10-05 20:32:48 +0900

    [Bronze IV] Title: Darius님 한타 안 함?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20499. Darius님 한타 안 함？/Darius님 한타 안 함？.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    int k,d,a;
+    scanf("%d/%d/%d",&k,&d,&a);
+    if(k + a < d || d == 0)
+        printf("hasu");
+    else
+        printf("gosu");
+    return 0;
+}
