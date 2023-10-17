f81395b2beb69eb0cb19448a291207261ed2b87a
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-10-17 13:49:44 +0000

    Update 팩토리얼 2.java

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -1,3 +1,4 @@
+//반복문
 import java.util.*;
 public class Main{
     public static void main(String[] args) {
@@ -8,5 +9,23 @@ public class Main{
             factorial *= i;
         }
         System.out.println(factorial);
+    }
+}
+//재귀
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(factorial(n));
+    }
+
+    static long factorial(int n){
+        if(n <= 0){
+            return 1;
+        }
+        
+        return n*factorial(n-1);
     }
 }
