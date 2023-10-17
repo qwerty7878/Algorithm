2ada67803edefa63d6049381166ef53d0d90fd4c
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-10-17 21:26:22 +0900

    [Bronze V] Title: Site Score, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.println(a*56 + 24*b + 14*c + 6*d);
+    }
+}
