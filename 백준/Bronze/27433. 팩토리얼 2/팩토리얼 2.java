ee05507534b80ce387ae7d9e0656d3544d6c3e52
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-10-17 22:45:34 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 204 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -3,15 +3,10 @@ public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        System.out.println(factorial(n));
-    }
-
-    static long factorial(int n){
-        if(n <= 0){
-            return 1;
+        long factorial = 1;
+        for(int i = 1; i <= n; i++){
+            factorial *= i;
         }
-        
-        return n*factorial(n-1);
+        System.out.println(factorial);
     }
 }
