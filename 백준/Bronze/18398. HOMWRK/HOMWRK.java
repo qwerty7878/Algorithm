ddd1b0b9b9d5f58ee51f0bbb99621635fb21f12a
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-10-17 23:10:14 +0900

    [Bronze IV] Title: HOMWRK, Time: 336 ms, Memory: 21196 KB -BaekjoonHub

---
+++ 백준/Bronze/18398. HOMWRK/HOMWRK.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i < t; i++){
+            int n = sc.nextInt();
+            for(int j = 0; j < n; j++){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                
+                System.out.println(a+b + " " + a*b);
+            }
+        }
+    }
+}
