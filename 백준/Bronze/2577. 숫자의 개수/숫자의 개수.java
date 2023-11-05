318fd071095248c24c2ac1066ac937283cbeb3b2
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-11-05 16:34:31 +0900

    [Bronze II] Title: 숫자의 개수, Time: 212 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        String str = String.valueOf(a*b*c);
+        //charAt()을 쓰려면 string 형태여야해서
+
+        for(int i = 0; i <= 9; i++){ // 1~9까지 출력하려고
+            int cnt = 0;
+            for(int j = 0; j < str.length(); j++){
+                if((str.charAt(j) - '0') == i)
+                    cnt++;
+
+            }
+            System.out.println(cnt);
+        }
+        
+    }
+}
