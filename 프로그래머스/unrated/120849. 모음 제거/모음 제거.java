90598f02367f698e4cfb21059a5aa4e3ee05d87e
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-12-28 20:07:41 +0900

    Update 모음 제거.java

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -3,3 +3,17 @@ class Solution {
         return my_string.replaceAll("[aeiou]","");
     }
 }
+
+// 다른 풀이
+class Solution {
+    public String solution(String my_string) {
+        String answer = my_string;
+        String[] vowels = {"a", "e", "i", "o", "u"};
+        
+        for (String v : vowels) {
+            answer = answer.replaceAll(v, "");
+        }
+        
+        return answer;
+    }
+}
