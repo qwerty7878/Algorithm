1ebbdc8235100e62bf5cf7b8517f68df751d7ca3
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-12-29 01:39:33 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.52 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -3,8 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
-        int max1 = numbers[numbers.length-1] * numbers[numbers.length-2];
-        int max2 = numbers[0] * numbers[1];
-        return max1 > max2 ? max1 : max2;
+        return numbers[numbers.length-1] * numbers[numbers.length-2] > numbers[0] * numbers[1] ? numbers[numbers.length-1] * numbers[numbers.length-2] : numbers[0] * numbers[1];
     }
 }
