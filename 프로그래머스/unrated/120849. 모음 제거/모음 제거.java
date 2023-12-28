b2bfa27f3864e4e1596286bc8e66a614c9c220f2
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-12-28 20:05:55 +0900

    [level unrated] Title: 모음 제거, Time: 0.11 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -1,12 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string;
-        String[] vowels = {"a","e","i","o","u"};
-        
-        for(String v : vowels){
-            answer = answer.replace(v, "");
-        }
-        
-        return answer;
+        return my_string.replaceAll("[aeiou]","");
     }
 }
