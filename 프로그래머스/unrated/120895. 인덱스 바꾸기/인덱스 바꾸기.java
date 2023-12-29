76ccf807493566afd601e5def64f2d01a4770129
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2023-12-29 17:51:44 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.18 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] arr = my_string.split("");
+        
+        String temp = "";
+        temp = arr[num1];
+        arr[num1] = arr[num2];
+        arr[num2] = temp;
+        String answer = String.join("", arr);
+        
+        return answer;
+    }
+}
