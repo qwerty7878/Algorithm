4572799b3e441bb7311371631d79e5d104e2edb1
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2024-01-22 21:47:27 +0900

    [level 0] Title: A로 B 만들기, Time: 0.40 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String before, String after) {
+        int answer = 0;
+        char[] bef = before.toCharArray();
+        char[] aft = after.toCharArray();
+        
+        Arrays.sort(bef);
+        Arrays.sort(aft);
+        
+        if(Arrays.equals(bef, aft)){
+            answer = 1;
+        }
+        else{
+            answer = 0;
+        }
+        return answer;
+    }
+}
