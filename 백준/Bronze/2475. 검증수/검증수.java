a96b3115ffa5d6312472c22a4ddb8524ed91e158
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2024-01-23 11:57:26 +0900

    [Bronze V] Title: 검증수, Time: 208 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i = 0; i < 5; i++){
+            int n = sc.nextInt();
+            sum += Math.pow(n,2);   
+        }
+        System.out.println(sum%10);
+    }
+}
