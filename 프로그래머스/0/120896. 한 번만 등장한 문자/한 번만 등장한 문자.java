527741886cb37d7aa6822a505910ac7b66ed1c28
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2024-01-25 02:54:07 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 1.49 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        Arrays.sort(str);
+        
+        for(int i = 0; i < str.length; i++){
+            int cnt = 0;
+            for(int j = 0; j < str.length; j++){
+                if(str[i].equals(str[j]))
+                    cnt++;
+            }
+            
+            if(cnt == 1)
+                answer += str[i];
+        }
+        return answer;
+    }
+}
