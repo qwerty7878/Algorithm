a7d745afcc2454f5f248ae89c5b33de13c75e411
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2024-01-25 00:24:43 +0900

    [level 0] Title: 가까운 수, Time: 0.46 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        Arrays.sort(array);
+        for(int i = 1; i < array.length; i++){
+            if(Math.abs(array[i - 1] - n) > Math.abs(n - array[i]))
+                array[0] = array[i];
+        }
+        return array[0];
+    }
+}
