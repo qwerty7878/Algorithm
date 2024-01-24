ff4e06a2c5fa29780952a8a5859cb46de5e0a25c
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2024-01-25 03:26:24 +0900

    [level 0] Title: k의 개수, Time: 382.42 ms, Memory: 389 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,17 +1,12 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int cnt = 0;
-        String strK = String.valueOf(k);
+        String str = "";
+        
         for(int a = i; a <= j; a++){
-            String value = String.valueOf(a);
-            if(value.contains(strK)){
-                String[] arr = value.split("");
-                for(String alpha : arr){
-                    if(alpha.equals(strK))
-                        cnt++;
-                }
-            }
+            str += a+"";
         }
-        return cnt;
+        return str.length() - str.replace(k+"","").length();
+        //모든 문자열 길이 - k제외 나머지를 ""로 바꾼 후 뺀 길이 리턴
+        
     }
 }
