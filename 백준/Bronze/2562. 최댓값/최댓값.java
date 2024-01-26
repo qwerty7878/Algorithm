967f6955dcac7048e7870b69946a5da5ff0546c5
qwerty7878 <142138946+qwerty7878@users.noreply.github.com>
2024-01-26 21:15:13 +0900

    [Bronze III] Title: 최댓값, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,19 +1,22 @@
 import java.util.*;
+
 public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int arr[] = new int[9];
-        int max = arr[0];
-        int count = 0;
-        
-        for(int i = 0; i < 9; i++){
-            arr[i] = sc.nextInt();
-            if(max < arr[i]){
-                max = arr[i];
-                count = i + 1;
-            }
-        }
-        System.out.println(max);
-        System.out.println(count);
-    }
-}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			int arr[] = new int [9];
+			int max	= 0;
+			int cnt = 0;
+
+			for(int i = 0; i < arr.length; i++){
+				arr[i] = sc.nextInt();
+
+				if(max < arr[i]){
+					max = arr[i];
+					cnt = i + 1; // 배열은 0부터니까
+				}
+			}
+			System.out.println(max);
+			System.out.println(cnt);
+			
+		}
+	}
