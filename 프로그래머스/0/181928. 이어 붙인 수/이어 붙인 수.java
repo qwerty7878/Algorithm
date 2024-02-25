cc30ca6f0de2285e0238ce1563d54d2bc9da507d
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-02-25 17:15:17 +0900

    Update 이어 붙인 수.java

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -14,3 +14,20 @@ class Solution {
         return answer;
     }
 }
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        String odd = "";
+        String even = "";
+        
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0)
+                even = even + Integer.toString(num_list[i]);
+            else
+                odd = odd + Integer.toString(num_list[i]);
+        }
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        return answer;
+    }
+}
