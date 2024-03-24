b102afaccec418620c22db5ca6efdefe11ee6b32
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-03-24 18:56:40 +0900

    [Bronze III] Title: 홀수, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.java
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -1,21 +1,25 @@
 import java.util.*;
+
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        int min = 100;
+        int odd_sum = 0;
+        int min = 10000;
+
         for(int i = 0; i < 7; i++){
             int n = sc.nextInt();
-            if(n%2 == 1){
-                sum += n;
-                min = Math.min(min,n);
+            if(n % 2 == 1){
+                odd_sum += n;
+                min = Math.min(min, n);
             }
         }
-        if(sum == 0)
+
+        if(odd_sum == 0)
             System.out.println(-1);
         else{
-            System.out.println(sum);
+            System.out.println(odd_sum);
             System.out.println(min);
         }
+        sc.close();
     }
 }
