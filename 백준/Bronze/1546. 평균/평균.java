c57cc8aaeb2755423d32afb55b1f64f07580fbd8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-03-25 21:54:28 +0900

    Update 평균.java

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -16,4 +16,34 @@ public class Main{
         }
         System.out.print(sum/arr.length);
     }
+
+
+
+    // 2번째 방법
+    import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int [n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        int sum = 0;
+        int max = 0;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+            sum += arr[i];
+        }
+
+        System.out.println((double)sum * 100/n/max);
+    }
 }
