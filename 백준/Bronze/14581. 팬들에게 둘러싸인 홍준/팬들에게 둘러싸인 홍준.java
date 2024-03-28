b2f38981596285c67fbc1d178cbbd6561ded835a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-03-28 20:22:35 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 224 ms, Memory: 18184 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        System.out.println(":fan::fan::fan:");
+
+        System.out.println(":fan::" + s + "::fan:");
+        
+        System.out.println(":fan::fan::fan:");
+            
+    }
+}
