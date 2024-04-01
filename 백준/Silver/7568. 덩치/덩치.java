0d1bb5b268ab81041a59df505a1cb13f4ede634e
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-04-01 23:46:06 +0900

    [Silver V] Title: 덩치, Time: 216 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+
+		int n = sc.nextInt();
+		int arr[][] = new int [n][2];
+
+		for(int i = 0; i < n; i++){
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+
+		for(int i = 0; i < n; i++){
+			int cnt = 1;
+			for(int j = 0; j < n; j++){
+				// if(i == j) continue;
+				if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1])
+					cnt++;
+			}
+			sb.append(cnt).append(' ');
+		}
+		System.out.println(sb);
+	}
+}
