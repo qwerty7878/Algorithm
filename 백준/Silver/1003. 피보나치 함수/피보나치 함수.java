0db487d6265fe9c379d452bf0fa508495ec8d153
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-04-13 00:17:26 +0900

    Update 피보나치 함수.java

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -26,3 +26,42 @@ public class Main{
 		System.out.println(sb.toString());
 	}
 }
+
+
+//2번째 방법
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+
+	static Integer arr[][] = new Integer [41][2];
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int t = Integer.parseInt(br.readLine());
+
+		arr[0][0] = 1;
+		arr[0][1] = 0;
+		arr[1][0] = 0;
+		arr[1][1] = 1;
+		
+		for(int i = 0; i < t; i++){
+			int n = Integer.parseInt(br.readLine());
+			fibo(n);
+			sb.append(arr[n][0] + " " + arr[n][1]).append('\n');
+		}
+		System.out.println(sb.toString());
+	}
+
+	static Integer[] fibo(int n){
+		if(arr[n][0] == null || arr[n][1] == null){
+			arr[n][0] = fibo(n-1)[0] + fibo(n-2)[0];
+			arr[n][1] = fibo(n-1)[1] + fibo(n-2)[1];
+		}
+		return arr[n];
+	}
+}
