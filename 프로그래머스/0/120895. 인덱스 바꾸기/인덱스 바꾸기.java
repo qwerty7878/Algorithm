d08fdffdd0466ca962a7db20602f4be4c21d33c3
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-04-16 00:01:13 -0700

    [level 0] Title: 인덱스 바꾸기, Time: 0.22 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,26 +1,13 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        /*
-        풀이 1번 기본적인 temp 추가 후 바꾸기
-        
-        String[] arr = my_string.split("");
-        
         String temp = "";
-        temp = arr[num1];
-        arr[num1] = arr[num2];
-        arr[num2] = temp;
-        String answer = String.join("", arr);
+        String[] s = my_string.split("");
         
-        return answer;
-        */
+        temp = s[num1];
+        s[num1] = s[num2];
+        s[num2] = temp;
+        String answer = String.join("",s);
         
-        char[] arr = my_string.toCharArray();
-        //String이라는 my_string을 toCharArray로 char화 시킨 후 char 배열에 넣음
-        arr[num1] = my_string.charAt(num2);
-        arr[num2] = my_string.charAt(num1);
-        
-        String answer = my_string.valueOf(arr);
-        // valueOf 는 ()안에 적힌 형식으로 객체화 시키는 것임
         return answer;
     }
 }
