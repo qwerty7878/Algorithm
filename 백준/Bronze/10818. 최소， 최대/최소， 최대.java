71d49eb91aae1fe12dd0e57debc3715774e2eff2
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-01 11:56:38 +0900

    [Bronze III] Title: 최소, 최대, Time: 2476 ms, Memory: 320152 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,14 +1,20 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main{
-    public static void main(String args[]){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        int arr[] = new int[N];
-        for(int i = 0; i < N; i++){
+
+        int n = sc.nextInt();
+        int arr[] = new int [n];
+
+        for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextInt();
         }
+
         Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[N-1]);
+
+        System.out.println(arr[0] + " " + arr[n-1]);
+        sc.close();
     }
 }
