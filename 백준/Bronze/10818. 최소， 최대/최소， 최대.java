b99ed555e06e8e8eaf3b7ceed434db74da2a43b6
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-01 12:00:43 +0900

    [Bronze III] Title: 최소, 최대, Time: 1304 ms, Memory: 104292 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,20 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
         int arr[] = new int [n];
 
         for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
 
         System.out.println(arr[0] + " " + arr[n-1]);
-        sc.close();
+        br.close();
     }
 }
