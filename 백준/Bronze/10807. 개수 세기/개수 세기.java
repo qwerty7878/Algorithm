5cb4d39b7bd5c951c186c1d72c5b22b26b313aa3
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-01 11:40:13 +0900

    [Bronze V] Title: 개수 세기, Time: 216 ms, Memory: 17836 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr1[] = new int[n];
 
-        for(int i = 0; i< n; i++){
-            arr1[i] = sc.nextInt();
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int [n];
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = sc.nextInt();
         }
+        
+        int cnt = 0;
         int v = sc.nextInt();
-        int count = 0;
-        for(int i = 0; i< n; i++){
-            if(v == arr1[i])
-                count++;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == v)
+                cnt++;
         }
-        System.out.println(count);
+
+        System.out.println(cnt);
+        
+        sc.close();
     }
 }
