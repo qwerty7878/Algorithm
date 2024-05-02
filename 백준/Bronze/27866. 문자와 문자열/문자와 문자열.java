5133413ec1597c7238acbd1e4251b9f0d2c0ec87
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-02 11:41:55 +0900

    [Bronze V] Title: 문자와 문자열, Time: 216 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,9 +1,15 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String S = sc.next();
-        int i = sc.nextInt();
-        System.out.println(S.charAt(i-1)); // 배열이라 0부터임이므로 -1을 해줘야함
+
+        String s = sc.next();
+        int n = sc.nextInt();
+
+        System.out.println(s.charAt(n-1));
+
+        sc.close();
     }
 }
