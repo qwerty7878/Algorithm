fbc170957c4b8bc2c980201e93ced4d409b5558b
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-04 02:34:07 +0900

    [Bronze V] Title: 문자열, Time: 204 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,13 +1,17 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int T = sc.nextInt();
-        
-        for(int i = 0; i < T; i++){
-            String S =sc.next();
-            System.out.println(S.charAt(0)+""+S.charAt(S.length()-1));
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            String s = sc.next();
+            String arr[] = s.split("");
+            System.out.println(arr[0] + arr[s.length() - 1]);
         }
+
+        sc.close();
     }
 }
