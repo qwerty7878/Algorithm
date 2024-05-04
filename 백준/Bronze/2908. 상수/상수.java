f5379c73227afa73240a74ab7fda451646a92121
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-04 16:58:38 +0900

    [Bronze II] Title: 상수, Time: 208 ms, Memory: 17596 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+
         int a = sc.nextInt();
         int b = sc.nextInt();
-        
+
+        StringBuilder sb = new StringBuilder();
         String revA = sb.append(a).reverse().toString();
-        sb.setLength(0);
+//        sb.setLength(0);
+        sb = new StringBuilder();
         String revB = sb.append(b).reverse().toString();
 
         int nA = Integer.valueOf(revA);
