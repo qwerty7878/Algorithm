9cd91208d4fb40975584f358f8f8a4610c13d270
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-05 19:41:25 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 200 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,13 +1,22 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        String str[] = {"c=","c-","dz=","d-","lj","nj","s=","z="};
-        for(int i = 0; i < str.length; i++){
-            if(s.contains(str[i]))
-                s = s.replace(str[i],"0");
-        }
-        System.out.println(s.length());
+
+        String s = sc.next()
+                .replace("c=","0")
+                .replace("c-","0")
+                .replace("dz=","0")
+                .replace("d-","0")
+                .replace("lj","0")
+                .replace("nj","0")
+                .replace("s=","0")
+                .replace("z=","0");
+
+        String sAr[] = s.split("");
+        System.out.println(sAr.length);
+
+        sc.close();
     }
 }
