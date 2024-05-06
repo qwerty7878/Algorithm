59cdc79ab34ec585a05dc7642219f0afa686f524
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-06 18:23:55 +0900

    [Silver V] Title: 너의 평점은, Time: 244 ms, Memory: 18100 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,52 +1,57 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public  class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        double aa = 0;
-        double point = 0;
+        double majorSum = 0;
+        double scoreSum = 0;
+
         for(int i = 0; i < 20; i++){
-            String title = sc.next();
-            double a = sc.nextDouble();
-            String b = sc.next();
-            
-            if(b.equals("A+")){
-                aa += 4.5 * a;
-                point += a;
-            }
-            else if(b.equals("A0")){
-                aa += 4.0 * a;
-                point += a;
-            }
-            else if(b.equals("B+")){
-                aa += 3.5 * a;
-                point += a;
-            }
-            else if(b.equals("B0")){
-                aa += 3.0 * a;
-                point += a;
-            }
-            else if(b.equals("C+")){
-                aa += 2.5 * a;
-                point += a;
-            }
-            else if(b.equals("C0")){
-                aa += 2.0 * a;
-                point += a;
-            }
-            else if(b.equals("D+")){
-                aa += 1.5 * a;
-                point += a;
-            }
-            else if(b.equals("D0")){
-                aa += 1.0 * a;
-                point += a;
-            }
-            else if(b.equals("F")){
-                aa += 0.0 * a;
-                point += a;
+            String major = sc.next();
+            double score = sc.nextDouble();
+            String grade = sc.next();
+
+            if(grade.equals("P"))   continue;
+            else{
+                if(grade.equals("A+")){
+                    scoreSum += score;
+                    majorSum += score * 4.5;
+                }
+                else if(grade.equals("A0")){
+                    scoreSum += score;
+                    majorSum += score * 4.0;
+                }
+                else if(grade.equals("B+")){
+                    scoreSum += score;
+                    majorSum += score * 3.5;
+                }
+                else if(grade.equals("B0")){
+                    scoreSum += score;
+                    majorSum += score * 3.0;
+                }
+                else if(grade.equals("C+")){
+                    scoreSum += score;
+                    majorSum += score * 2.5;
+                }
+                else if(grade.equals("C0")){
+                    scoreSum += score;
+                    majorSum += score * 2.0;
+                }
+                else if(grade.equals("D+")){
+                    scoreSum += score;
+                    majorSum += score * 1.5;
+                }
+                else if(grade.equals("D0")){
+                    scoreSum += score;
+                    majorSum += score * 1.0;
+                }
+                else if(grade.equals("F")){
+                    scoreSum += score;
+                    majorSum += score * 0.0;
+                }
             }
         }
-        System.out.printf("%.6f",aa/point);
+        System.out.println(majorSum / scoreSum);
     }
 }
+
