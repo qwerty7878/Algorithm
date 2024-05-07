e32251c04ea8713447947acc7eb5d7a5c28ed2da
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-07 15:26:44 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 312 ms, Memory: 19428 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,18 +1,20 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc =  new Scanner(System.in);
-        
-        int T = sc.nextInt();
-        
-        for(int  i = 0; i < T; i++){
-            int cost = sc.nextInt();
-            int quarter = cost/25;
-            int dime = (cost - quarter * 25)/10;
-            int nickel = (cost - quarter *25 - dime * 10)/5;
-            int penny = cost - quarter * 25 - dime * 10 - nickel * 5;
-            
-            System.out.println(quarter + " " + dime + " " + nickel + " " + penny);
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; i++){
+            int c = sc.nextInt();
+
+            int q = c / 25;
+            int d = (c % 25) /10;
+            int n = ((c % 25) % 10) / 5;
+            int p = (((c % 25) % 10) % 5) / 1;
+            System.out.println(q + " "  + d + " " + n + " " + p);
         }
+
     }
 }
