9ca3bc418e1ec5eb7fba7e4f393dc3ca2523caaa
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-08 21:41:53 +0900

    [Bronze II] Title: 진법 변환, Time: 132 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,25 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String n = st.nextToken();
+        int b = Integer.parseInt(st.nextToken());
 
-        String s = sc.next();
-        int n = sc.nextInt();
-
-        int sum = 0;
-        int pow = 1;
-
-        for(int i = s.length() -1; i  >= 0; i--){
-            char c = s.charAt(i);
-            if(c >= 'A' && c <= 'z')
-                sum += (c - 'A' + 10) * pow;
-            else
-                sum += (c - '0') * pow;
-
-            pow *= n;
-        }
-        System.out.println(sum);
-        sc.close();
+        final int res = Integer.parseInt(n,b);
+        System.out.println(res);
     }
 }
