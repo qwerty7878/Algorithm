491fee2d498d31a040b6db62ad0b0b4dc44cc958
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-17 00:21:11 +0900

    Update 문자열 내 p와 y의 개수.java

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,9 +1,6 @@
-import java.util.*;
-
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        StringBuilder sb = new StringBuilder();
         int cntP = 0;
         int cntY = 0;
         for(int i = 0; i < s.length(); i++){
