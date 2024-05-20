a27ca699437f156333b7bc1addfb258d2706a828
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-20 20:31:15 +0900

    [Bronze I] Title: 약수들의 합, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,35 +1,39 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        
-        while(true){
-            int n = sc.nextInt();
-            if(n == -1)
-                break;
-            
-            int arr[] = new int[n];
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true){
+            StringBuilder sb = new StringBuilder();
+            int n = Integer.parseInt(br.readLine());
             int sum = 0;
-            int index = 0;
+            ArrayList<Integer> list = new ArrayList<>();
+
+            if(n == -1) break;
+
             for(int i = 1; i < n; i++){
-                if(n%i == 0){
-                    arr[index++] = i;
+                if(n % i == 0){
+                    list.add(i);
                     sum += i;
                 }
             }
-            if(sum != n){
-                System.out.println(n + " is NOT perfect.");
-                continue;
-            }
-            System.out.print(n + " = ");
-            for(int i = 0; i < index; i++){
-                if(i == index -1)
-                    System.out.print(arr[i]);
-                else
-                    System.out.print(arr[i] + " + ");
 
-            }
-            System.out.println();
+            if(sum == n){
+                sb.append(n).append(" = ");
+                for(int i = 0; i < list.size(); i++){
+                    if(i != list.size() -1)
+                        sb.append(list.get(i)).append(" + ");
+                    else
+                        sb.append(list.get(i));
+                }
+            } else
+                sb.append(n).append(" is NOT perfect.");
+
+            System.out.println(sb);
         }
     }
 }
