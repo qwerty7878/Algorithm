beb0a428348267a41623c5520e4223bbd3515017
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-20 18:44:24 +0900

    [Bronze III] Title: 약수 구하기, Time: 224 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,19 +1,30 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int K = sc.nextInt();
-        int count = 0;
-        for(int i = 1; i <= N; i++){
-            if(N % i == 0)
-                count++;
-            if(count == K) {
-                System.out.println(i);
-                break;
-            }
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0)
+                list.add(i);
         }
-        if(count < K)
+
+        int arr[] = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++){
+            arr[i] = list.get(i);
+        }
+
+        if(arr.length > k - 1)
+            System.out.println(arr[k - 1]);
+        else if(arr.length == 1)
+            System.out.println(1);
+        else
             System.out.println(0);
     }
 }
