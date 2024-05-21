a93ece2b32ed95a30dd4909c30bd3b91b2e1993b
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-21 12:13:06 +0900

    [Bronze II] Title: 소수 찾기, Time: 216 ms, Memory: 17808 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,16 +1,18 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int cnt = 0;
+
         for(int i = 0; i < n; i++){
-            int pn = sc.nextInt();
-            for(int j = 2; j <= pn; j++){
-                if(j == pn)
-                    cnt++;
-                if(pn%j == 0)
-                    break;
+            int m = sc.nextInt();
+
+            for(int j = 2; j <= m; j++){
+                if(m == j) cnt++;
+                if(m % j == 0) break;
             }
         }
         System.out.println(cnt);
