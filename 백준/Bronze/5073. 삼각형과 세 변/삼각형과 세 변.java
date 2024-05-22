ccfe77b3f2254edde1d3c258ef9def65d7b419eb
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-23 00:32:14 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 196 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -1,49 +1,23 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        while(true){
+        while (true) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             int c = sc.nextInt();
-            
-            if(a == 0 && b == 0 && c ==0)
-                break;
-            int max = Math.max(Math.max(a,b),c);
-            
-            if(max == a){
-                int sum = b + c;
-                if(a >= sum){
-                    System.out.println("Invalid");
-                    continue;
-                }
+
+            int sum = a + b + c;
+
+            if(a == 0 && b == 0 && c == 0)  break;
+
+            if(Math.max(Math.max(a,b),c) < (sum - Math.max(Math.max(a,b),c))) {
+                if (a == b && a == c && b == c) System.out.println("Equilateral");
+                else if (a == b || a == c || b == c) System.out.println("Isosceles");
+                else if (a != b && a != c && c != b) System.out.println("Scalene");
             }
-            if(max == b){
-                int sum = a + c;
-                if(b >= sum){
-                    System.out.println("Invalid");
-                    continue;
-                }
-            }if(max == c){
-                int sum = b + a;
-                if(c >= sum){
-                    System.out.println("Invalid");
-                    continue;
-                }
-            }
-            if(a != b && a != c && b != c){
-                System.out.println("Scalene");
-                continue;
-            }
-            if(a == b && a == c && b == c){
-                System.out.println("Equilateral");
-                continue;
-            }
-            if(a == b || a == c || b == c){
-                System.out.println("Isosceles");
-                continue;
-            } 
+            else System.out.println("Invalid");
         }
     }
 }
