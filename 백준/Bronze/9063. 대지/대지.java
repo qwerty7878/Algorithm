45526da08b6e5cf8798d30a0a80d42e863c2897f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-22 21:50:05 +0900

    [Bronze III] Title: 대지, Time: 944 ms, Memory: 109500 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -1,34 +1,25 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc =  new Scanner(System.in);
-        
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int x[] = new int [n];
-        int y[] = new int [n];
-        
+        int arrx[] = new int[n];
+        int arry[] = new int[n];
+
         for(int i = 0; i < n; i++){
-            x[i] = sc.nextInt();
-            y[i] = sc.nextInt();
+            arrx[i] = sc.nextInt();
+            arry[i] = sc.nextInt();
         }
-        int min_x = x[0];
-        int max_x = x[0];
-        int min_y = y[0];
-        int max_y = y[0];
-        
-        for(int i = 0; i < n; i++){
-            if(min_x > x[i])
-                min_x = x[i];
-            if(min_y > y[i])
-                min_y = y[i];
-            if(max_x < x[i])
-                max_x = x[i];
-            if(max_y < y[i])
-                max_y = y[i];
-        }
-        if(n == 1)
-            System.out.println(0);
-        else
-            System.out.println((max_x - min_x) * (max_y - min_y));
+        Arrays.sort(arrx);
+        Arrays.sort(arry);
+
+        int maxx = arrx[n -1];
+        int maxy = arry[n -1];
+        int minx = arrx[0];
+        int miny = arry[0];
+
+        System.out.println((maxx - minx) * (maxy - miny));
     }
 }
