69a8c54e6578f24d44c8d72c444f953f0fee23fb
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-23 19:04:41 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -1,9 +1,16 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
-        System.out.println(n*n*n);
-        System.out.println('3');
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        long n = Long.parseLong(br.readLine());
+
+        sb.append(n * n * n)
+                .append("\n")
+                .append(3);
+        System.out.println(sb);
+    }
 }
