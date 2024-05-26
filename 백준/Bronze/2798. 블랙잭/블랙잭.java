04f1703fedda2831bac0bfc73af04114bcfed5a8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-26 23:16:56 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,26 +1,33 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int arr[] = new int [n];
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int arr[] = new int[n];
         int sum = 0;
-        int temp = 0;
-        
+        int res = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                for(int k = j + 1; k < n; k++){
+
+        for(int i = 0; i < n -2; i++){
+            for(int j = i + 1; j < n -1; j++){
+                for(int k = j + 1; k < n; k++) {
                     sum = arr[i] + arr[j] + arr[k];
-                    if(temp < sum && sum <= m)
-                        temp = sum;
+                    if(sum <=m && sum > res)
+                        res = sum;
+                    }
                 }
             }
-        }
-        System.out.println(temp);
+        System.out.println(res);
     }
 }
