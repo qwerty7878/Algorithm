ec6acc42743e8611d73b7c8061cae82392e7ecce
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-26 19:25:17 +0900

    [Bronze II] Title: OX퀴즈, Time: 144 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,31 +1,29 @@
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
- 
-		String arr[] = new String[sc.nextInt()];
- 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.next();
-		}
-		
-		for (int i = 0; i < arr.length; i++) {
-			
-			int cnt = 0;
-			int sum = 0;
-			
-			for (int j = 0; j < arr[i].length(); j++) {
-				
-				if (arr[i].charAt(j) == 'O') {
-					cnt++;
-				} 
-				else {
-					cnt = 0;
-				}
-				sum += cnt;
-			}
-			System.out.println(sum);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            String s = br.readLine();
+            int cnt = 0;
+            int sum = 0;
+
+            for(int j = 0; j < s.length(); j++){
+                if(s.charAt(j) == 'O'){
+                    cnt++;
+                    sum += cnt;
+                }
+                else
+                    cnt = 0;
+            }
+            sb.append(sum + "\n");
+        }
+        System.out.println(sb);
+    }
 }
