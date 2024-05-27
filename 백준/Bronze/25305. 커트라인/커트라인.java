8208a7134ba2f022e3b9df7f069466183a3987c0
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-28 02:28:18 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -1,15 +1,26 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int arr[] = new int [n];
-        
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int arr[] = new int[n];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        System.out.println(arr[n-k]);
+        sb.append(arr[n - k]);
+        System.out.println(sb);
     }
 }
