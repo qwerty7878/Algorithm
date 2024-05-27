38d9189a2459dd54cb2eb8b235c607d06a57dbe2
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-28 01:29:42 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,19 +1,25 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int d = sc.nextInt();
-        int e = sc.nextInt();
-        int f = sc.nextInt();
-        for(int i = -999; i < 1000; i++){
-            for(int j = -999; j < 1000; j++){
-                if(a*i + b*j == c && d*i + e*j == f){
-                    System.out.println(i + " " + j);
-                }
-            }
-        }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        int x = (c*e - b*f)/(a*e - b*d);
+        int y = (d*c - a*f)/(d*b - a*e);
+
+        sb.append(x).append(" ").append(y);
+        System.out.println(sb);
     }
 }
