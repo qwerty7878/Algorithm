ae3a8c67d69abd5d91276e234e547037d026c7d1
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-28 02:25:22 +0900

    [Bronze II] Title: 대표값2, Time: 120 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,17 +1,22 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int arr[] = new int [5];
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int arr[] = new int[5];
         int sum = 0;
-        
-        for(int i = 0; i < 5; i++){
-            int n = sc.nextInt();
-            arr[i] += n;
-            sum += n;
+
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
-        System.out.println(sum/5);
         Arrays.sort(arr);
-        System.out.println(arr[2]);
+        sb.append(sum / 5).append("\n").append(arr[2]);
+        System.out.println(sb);
     }
 }
