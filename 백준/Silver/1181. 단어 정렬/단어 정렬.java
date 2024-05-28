f362b03f9006356c642ebb5de5c714e63e06cbcc
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-28 19:05:52 +0900

    [Silver V] Title: 단어 정렬, Time: 372 ms, Memory: 26716 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,25 +1,36 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
         String arr[] = new String[n];
-        
+
         for(int i = 0; i < n; i++){
-            arr[i] = sc.next();
+            arr[i] = br.readLine();
         }
-        Arrays.sort(arr, new Comparator<String>() {
-            public int compare(String s1, String s2){
-                if(s1.length() == s2.length())
-                    return s1.compareTo(s2);
-                else
-                    return s1.length() - s2.length();
+
+        Arrays.sort(arr, new Comparator<String>(){
+           public int compare(String s1, String s2){
+               if(s1.length() == s2.length())
+                   return s1.compareTo(s2);
+               else return s1.length() - s2.length();
             }
         });
-        System.out.println(arr[0]);
-        for(int i = 1; i < n; i++){
-            if(!arr[i].equals(arr[i-1]))
-                System.out.println(arr[i]);
+
+        sb.append(arr[0]).append("\n");
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i].equals(arr[i - 1]))   continue;
+            else sb.append(arr[i]).append("\n");
         }
+
+        bw.write(sb.toString());
+        bw.flush();
     }
 }
