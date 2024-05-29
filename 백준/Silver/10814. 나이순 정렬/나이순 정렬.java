bc61be1e68ecd118367714934f7d1979fe3e6ddd
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-29 11:58:41 +0900

    [Silver V] Title: 나이순 정렬, Time: 684 ms, Memory: 50900 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,23 +1,50 @@
-import java.util.*;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String arr[][] = new String[n][2];
-        
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        Member m[] = new Member[n];
         for(int i = 0; i < n; i++){
-            arr[i][0] = sc.next();
-            arr[i][1] = sc.next();
+            st = new StringTokenizer(br.readLine(), " ");
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            m[i] = new Member(age, name);
         }
 
-        Arrays.sort(arr, new Comparator<String[]>() {
-            public int compare(String[] s1,String[] s2) {
-                return Integer.parseInt(s1[0]) - Integer.parseInt(s2[0]);
+        Arrays.sort(m, new Comparator<Member>() {
+            @Override
+            public int compare(Member s1, Member s2) {
+                return s1.age - s2.age;
             }
         });
 
-        for(int i = 0; i < n; i++){
-            System.out.println(arr[i][0] + " " + arr[i][1]);
+        for(int i = 0; i < m.length; i++){
+            sb.append(m[i]);
+        }
+
+        System.out.println(sb);
+    }
+
+    public static class Member {
+        int age;
+        String name;
+
+        public Member(int age,String name){
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return age + " " + name + "\n";
         }
     }
 }
