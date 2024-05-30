07ca75065bdccb90cb5b06e00fc6f23d1167fc72
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-05-30 14:49:54 +0900

    [Bronze I] Title: 최소공배수, Time: 332 ms, Memory: 22320 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,20 +1,20 @@
-import java.util.*;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+import java.util.Scanner;
 
-        for(int i = 0; i < T; i++){
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {;
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            
-            System.out.println(a*b/gcd(a,b));
+            System.out.println(a*b / gcd(a,b));
         }
-    }
-    public static int gcd(int a, int b){
-        if(b == 0)
-            return a;
+   }
 
-        return gcd(b, a%b);
-    }
+   public static int gcd(int n, int m){
+        if(m == 0)  return n;
+        return gcd(m, n%m);
+   }
 }
