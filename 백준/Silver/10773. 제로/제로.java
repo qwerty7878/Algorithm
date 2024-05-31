f15af458c21029c8e231166b55faf66af5da844d
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-01 05:27:56 +0900

    [Silver IV] Title: 제로, Time: 260 ms, Memory: 23216 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,26 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Stack<Integer> stack = new Stack<>();
 
-        int k = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> s = new Stack<>();
 
-        for(int i = 0; i < k; i++){
-            int n = sc.nextInt();
-
-            if(n == 0)
-                stack.pop();
-            else
-                stack.push(n);
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int isZero = Integer.parseInt(br.readLine());
+            if (isZero == 0) {
+                sum -= s.peek();
+                s.pop();
+            } else {
+                s.push(isZero);
+                sum += s.peek();
+            }
         }
-        long sum = 0;
-
-        for(int j:stack){
-            sum += j;
-        }
-
         System.out.println(sum);
     }
 }
