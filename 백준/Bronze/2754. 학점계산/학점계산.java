3bbb1c7b57e6c6f13deed36124000a3fb1751dd4
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-01 16:37:47 +0900

    [Bronze III] Title: 학점계산, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.java
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -1,49 +1,28 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		switch(s){
-			case("A+"):
-				System.out.println(4.3);
-				break;
-			case("A0"):
-				System.out.println(4.0);
-				break;
-			case("A-"):
-				System.out.println(3.7);
-				break;
-			case("B+"):
-				System.out.println(3.3);
-				break;
-			case("B0"):
-				System.out.println(3.0);
-				break;
-			case("B-"):
-				System.out.println(2.7);
-				break;
-			case("C+"):
-				System.out.println(2.3);
-				break;
-			case("C0"):
-				System.out.println(2.0);
-				break;
-			case("C-"):
-				System.out.println(1.7);
-				break;
-			case("D+"):
-				System.out.println(1.3);
-				break;
-			case("D0"):
-				System.out.println(1.0);
-				break;
-			case("D-"):
-				System.out.println(0.7);
-				break;
-			case("F"):
-				System.out.println(0.0);
-				break;
-		}
-	}
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String score = br.readLine();
+
+        if(score.equals("A+")) sb.append(4.3);
+        else if(score.equals("A0")) sb.append(4.0);
+        else if(score.equals("A-")) sb.append(3.7);
+        else if(score.equals("B+")) sb.append(3.3);
+        else if(score.equals("B0")) sb.append(3.0);
+        else if(score.equals("B-")) sb.append(2.7);
+        else if(score.equals("C+")) sb.append(2.3);
+        else if(score.equals("C0")) sb.append(2.0);
+        else if(score.equals("C-")) sb.append(1.7);
+        else if(score.equals("D+")) sb.append(1.3);
+        else if(score.equals("D0")) sb.append(1.0);
+        else if(score.equals("D-")) sb.append(0.7);
+        else sb.append(0.0);
+
+        System.out.println(sb);
+    }
 }
