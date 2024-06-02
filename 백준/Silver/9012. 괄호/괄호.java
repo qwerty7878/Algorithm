985be4e103edb8da436347be476bd9b9a714bbb4
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-02 15:35:36 +0900

    [Silver IV] Title: 괄호, Time: 140 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,33 +1,35 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
-class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-		for(int i = 0; i < t; i++){
-			String s = br.readLine();
-			Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            Stack<Character> stack = new Stack<>();
 
-			for(int j = 0; j < s.length(); j++){
-				if(s.charAt(j) == '(')
-					stack.push(s.charAt(j));
-				else{
-					if(stack.isEmpty()){
-						stack.push(s.charAt(j));
-						break;
-					}
-					else
-						stack.pop();
-				}
-			}
-			if(stack.isEmpty())
-				System.out.println("YES");
-			else
-				System.out.println("NO");
-		}
-	}
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '(') {
+                    stack.push(s.charAt(j));
+                } else {
+                    if (stack.empty()) {
+                        stack.push(s.charAt(j));
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+            if (stack.empty()) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
 }
