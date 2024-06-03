565574763e8f6fceab39975c85b8bbd45c363de8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-04 00:30:22 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45716 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,19 +1,28 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Queue<Integer> queue = new LinkedList<>();
-        int n = sc.nextInt();
 
-        for(int i = 1; i <= n; i++){
-            queue.add(i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue<Integer> q = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
         }
 
-        while(queue.size() > 1){
-            queue.poll();
-            queue.add(queue.poll());
+        while (q.size() != 1) {
+
+            q.poll();
+            q.offer(q.poll());
+
         }
-        System.out.println(queue.peek());
+        System.out.println(q.peek());
     }
 }
+
