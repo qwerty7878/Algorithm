bed00f8af3ea158e4044cbcf1eceb24ca00941e1
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-04 14:39:46 +0900

    [Silver IV] Title: 덱 2, Time: 1092 ms, Memory: 255732 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -3,31 +3,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		Deque<Integer> deque = new LinkedList<>();
 
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Deque<Integer> dq = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
 
-		for(int i = 0; i < n; i++){
-			st = new StringTokenizer(br.readLine());
-			switch(st.nextToken()){
-				case "1" : deque.addFirst(Integer.parseInt(st.nextToken())); break;
-				case "2" : deque.addLast(Integer.parseInt(st.nextToken())); break;
-				case "3" : sb.append(deque.isEmpty() ? -1 : deque.removeFirst()).append('\n'); break;
-				case "4" : sb.append(deque.isEmpty() ? -1 : deque.removeLast()).append('\n'); break;
-				case "5" : sb.append(deque.size()).append('\n'); break;
-				case "6" : sb.append(deque.isEmpty() ? 1 : 0).append('\n'); break;
-				case "7" : sb.append(deque.isEmpty() ? -1 : deque.getFirst()).append('\n'); break;
-				case "8" : sb.append(deque.isEmpty() ? -1 : deque.getLast()).append('\n'); break;
-			}
-		}
-		System.out.println(sb);
-	}
+            if (command == 1) {
+                dq.addFirst(Integer.parseInt(st.nextToken()));
+            } else if (command == 2) {
+                dq.addLast(Integer.parseInt(st.nextToken()));
+            } else if (command == 3) {
+                sb.append(dq.isEmpty() ? -1 : dq.pollFirst()).append("\n");
+            } else if (command == 4) {
+                sb.append(dq.isEmpty() ? -1 : dq.pollLast()).append("\n");
+            } else if (command == 5) {
+                sb.append(dq.size()).append("\n");
+            } else if (command == 6) {
+                sb.append(dq.isEmpty() ? 1 : 0).append("\n");
+            } else if (command == 7) {
+                sb.append(dq.isEmpty() ? -1 : dq.peekFirst()).append("\n");
+            } else if (command == 8) {
+                sb.append(dq.isEmpty() ? -1 : dq.peekLast()).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
+
