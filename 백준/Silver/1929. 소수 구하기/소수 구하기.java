53d38f9bcf9fa96429cf31f8e352a5e8646dc92c
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-06 15:20:09 +0900

    [Silver III] Title: 소수 구하기, Time: 272 ms, Memory: 23300 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,40 +1,37 @@
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
 
-	public static boolean primeNum[];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
+        boolean isPrime[] = new boolean[m + 1];
+        Arrays.fill(isPrime, true);
 
-		int m = sc.nextInt();
-		int n = sc.nextInt();
+        isPrime[0] = isPrime[1] = false;
 
-		primeNum = new boolean [n + 1]; // 0~n까지
-		isPrime();
+        for (int i = 2; i <= (int) (Math.sqrt(m) + 1); i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= m; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
 
-		for(int i = m; i <=n; i++){
-			// false 값이 소수
-			if(primeNum[i] == false)
-				sb.append(i).append('\n');
-
-		}
-		System.out.println(sb.toString());
-	}
-
-	public static void isPrime() {
-
-		// 에라토스테네스의 체를  이용한 알고리즘
-
-		primeNum[0] = primeNum[1] = true; //소수는 0,1은 아니니까
-		// k=2 부터 √N 이하까지 반복하여 자연수들 중 k를 제외한 k의 배수들을 제외
-		for(int i = 2; i < Math.sqrt(primeNum.length); i++){
-			if(primeNum[i])	continue;
-			// i의 배수인지 확인하는 방법
-			for(int j = i * i; j < primeNum.length; j += i){
-				primeNum[j] = true;
-			}
-		}
-	}
+        for (int i = n; i <= m; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 }
