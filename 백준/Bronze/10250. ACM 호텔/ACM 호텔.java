9c7f106b05809ed943b6139294b81de5c7a2ceab
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-19 15:13:36 +0900

    [Bronze III] Title: ACM 호텔, Time: 128 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,19 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		for(int i = 0; i < t; i++){
-			int h = sc.nextInt();
-			int w = sc.nextInt();
-			int n = sc.nextInt();
-			
-			if(n % h == 0)
-				System.out.println((h*100) + (n/h));
-			else
-				System.out.println(((n%h)*100) + (n/h)+1);
-		}
-	}
+        int T = Integer.parseInt(br.readLine());
+        int floor = -1;
+        int room = -1;
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int cu = Integer.parseInt(st.nextToken());
+
+            if (cu % h == 0) {
+                floor = h * 100;
+                room = cu / h;
+            } else {
+                floor = (cu % h) * 100;
+                room = (cu / h) + 1;
+            }
+            sb.append(floor + room).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
