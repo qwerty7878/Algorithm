73ddbbf0d2633e60621459b610dcb986553ff1e0
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-19 13:57:08 +0900

    [Silver II] Title: 스택 수열, Time: 328 ms, Memory: 29516 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,40 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
- 
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();	// 출력할 결과물 저장
-		Stack<Integer> stack = new Stack<>();
-		
-		int n = sc.nextInt();
-		int start = 0;
-		
-		// N 번 반복
-		while(n -- > 0) {
-			
-			int value = sc.nextInt();
-			
-			if(value > start) {
-				// start + 1부터 입력받은 value 까지 push를 한다.
-				for(int i = start + 1; i <= value; i++) {
-					stack.push(i);
-					sb.append('+').append('\n');	// + 를 저장한다. 
-				}
-				start = value; 	// 다음 push 할 때의 오름차순을 유지하기 위한 변수 초기화 
-			}
-			
-			// top에 있는 원소가 입력받은 값과 같이 않은 경우  
-			else if(stack.peek() != value) {
-				System.out.println("NO");
-				return;		// 또는 System.exit(0); 으로 대체해도 됨. 
-			}
-			
-			stack.pop();
-			sb.append('-').append('\n');
-			
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+        int start = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num > start) {  
+                //  오름차순으로 푸시
+                for (int j = start + 1; j <= num; j++) {
+                    
+//                    i 가 1로 준다면 stack에 계속 1~반복될것임
+//                    따라서 1부터 시작하지만,오름차순으로 중복은 없어야하므로
+//                    start는 반복문이 종료할때마다 값을 초기화해주고
+//                    start 값보다 작으면 들어갈 수 없게 조건을 설정해놨기 때문에 오름차순으로 push된다.
+                    
+                    stack.push(j);
+                    sb.append("+").append("\n");
+                }
+                start = num;
+            } else if (stack.peek() != num) {   
+                
+                System.out.println("NO");
+                System.exit(0); 
+                // 0이되면 종료
+            }
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+        System.out.println(sb);
+    }
 }
