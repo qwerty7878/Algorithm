229809ffbd92df182ad5bd881ce401e0cb63af5c
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-19 12:11:17 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 496 ms, Memory: 294816 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,27 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Queue<Integer> queue = new LinkedList<>();
-		StringBuilder sb = new StringBuilder();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        Queue<Integer> q = new LinkedList<>();
 
-		int n = sc.nextInt();
-		int k = sc.nextInt();
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-		sb.append("<");
+        for (int i = 1; i <= n; i++) {
+            q.add(i);
+        }
+        sb.append("<");
+        while (!q.isEmpty()) {
+            for (int i = 1; i < k; i++) {
+                q.add(q.poll());
+            }
+            sb.append(q.poll());
+            if(q.size() != 0)
+                sb.append(", ");
+        }
+        sb.append(">");
+        System.out.println(sb);
 
-		for(int i = 1; i <= n; i++){
-			queue.add(i);
-		}
-
-		while(queue.size() > 1){
-			for(int i = 0; i < k - 1; i++){
-				queue.add(queue.poll());
-			}
-			sb.append(queue.poll() + ", ");
-		}
-		sb.append(queue.poll() + ">");
-		System.out.println(sb);
-	}
+    }
 }
