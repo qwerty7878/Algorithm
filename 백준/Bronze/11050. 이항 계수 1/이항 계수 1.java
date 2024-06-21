d47a7dcc04f019c57fdc1a1010b3ca246240217f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-21 17:32:29 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st. nextToken());
-		
-		System.out.println(factorial(n)/(factorial(k) * factorial(n - k)));
-	}
+        sb.append(factorial(n) / ((factorial(n - k)) * factorial(k))).append("\n");
+        System.out.println(sb);
+    }
 
-	private static int factorial(int n){
-		if(n <= 1)
-			return 1;
-
-		return n * factorial(n - 1);
-	}
+    static int factorial(int a) {
+        if(a == 0)  return 1;
+        if(a == 0)  return 1;
+        return a * factorial(a - 1);
+    }
 }
