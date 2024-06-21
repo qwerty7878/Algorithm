99832f0c42f563dbb17f20ed106401ed7ba3b7e8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-21 19:18:22 +0900

    [Silver IV] Title: solved.ac, Time: 376 ms, Memory: 37140 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -3,27 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		int arr[] = new int [n];
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
 
-		for(int i = 0; i < n; i++){
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-		Arrays.sort(arr);
+        int except = (int)Math.round(n * 0.15);
 
-		int out = (int)Math.round(n * 0.15);
-		double sum = 0;
-
-		for(int i = out; i < n - out; i++){
-			sum += arr[i];
-		}
-
-		int res = (int)Math.round(sum / (n - out * 2));
-		System.out.println(res);
-	}
+        double sum = 0;
+        for (int i = except; i < arr.length - except; i++) {
+            sum += arr[i];
+        }
+        sb.append(Math.round(sum / (arr.length - 2 * except))).append("\n");
+        System.out.println(sb);
+    }
 }
