b52a335fe4056decc1475126f47ab45a094da652
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-22 20:25:05 +0900

    [Silver IV] Title: 수 찾기, Time: 620 ms, Memory: 45396 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,28 +4,51 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
-		int arr[] = new int [n];
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int m = Integer.parseInt(br.readLine());
+        int arr[] = new int[m];
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < n; i++){
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
 
-		int m = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < m; i++){
-			if(Arrays.binarySearch(arr, Integer.parseInt(st.nextToken())) >= 0)
-				sb.append(1).append('\n');
-			else
-				sb.append(0).append('\n');
-			}
-		System.out.println(sb.toString());
-	}
+        int n = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            int searchKey = Integer.parseInt(st.nextToken());
+            if (BinarySearch(arr, searchKey) >= 0) {
+//                Arrays.binarySearch(arr,searchKey) >= 0 으로 대체가능
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+
+    static int BinarySearch(int arr[],int searchKey) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high)/2;
+
+            if (arr[mid] < searchKey) {
+                low = mid + 1;
+            } else if (arr[mid] > searchKey) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return  -1;
+    }
 }
