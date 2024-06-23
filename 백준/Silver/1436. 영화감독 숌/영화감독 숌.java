376660d825a1af2157172cbaec17ef88df85d41b
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-23 19:27:57 +0900

    [Silver V] Title: 영화감독 숌, Time: 308 ms, Memory: 85056 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,16 +1,22 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
         int num = 666;
         int cnt = 1;
-        
-        while(cnt != n){
+
+        while (n != cnt) {
             num++;
-            if(String.valueOf(num).contains("666"))
+            String s = "" + num;
+            if (s.contains("666")) {
                 cnt++;
+            }
         }
         System.out.println(num);
     }
