92cc4ff87fa6268512fe397b69a6645c7b8d47d1
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-23 20:44:00 +0900

    [Silver V] Title: 덩치, Time: 144 ms, Memory: 15852 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,26 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
 
-		int n = sc.nextInt();
-		int arr[][] = new int [n][2];
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int arr[][] = new int[n][2];
 
-		for(int i = 0; i < n; i++){
-			arr[i][0] = sc.nextInt();
-			arr[i][1] = sc.nextInt();
-		}
 
-		for(int i = 0; i < n; i++){
-			int cnt = 1;
-			for(int j = 0; j < n; j++){
-				if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1])
-					cnt++;
-			}
-			sb.append(cnt).append(' ');
-		}
-		System.out.println(sb);
-	}
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for (int i = 0; i < n; i++) {
+            int rank = 1;
+            for (int j = 0; j < n; j++) {
+                if(i == j)  continue;   // arr[j] arr[i] 가 같으면 같은 사람임
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            System.out.print(rank + " ");
+        }
+    }
 }
