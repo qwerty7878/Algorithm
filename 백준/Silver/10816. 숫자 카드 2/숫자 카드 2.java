91492d3d551cf5eda5346646c0ebddf1d548533a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-24 01:59:30 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1376 ms, Memory: 122752 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,54 +1,64 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
 
-		int n = sc.nextInt();
-		int card[] = new int [n];
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		for(int i = 0; i < n; i++){
-			card[i] = sc.nextInt();
-		}
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
 
-		Arrays.sort(card);
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		int m = sc.nextInt();
+        Arrays.sort(arr);
 
-		for(int i = 0; i < m; i++){
-			int num = sc.nextInt();
-			sb.append(end(card,num) - start(card,num)).append(' ');
-		}
-		System.out.println(sb);
-	}
-		public static int start(int arr[], int key) {
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < m; i++) {
+            int key = Integer.parseInt(st.nextToken());
+            sb.append(upperBinary(arr, key) - lowerBinary(arr, key)).append(" ");
+        }
+        System.out.println(sb);
+    }
 
-			int left = 0;
-			int right = arr.length;
-			
-			while(left < right){
-				int mid = (left + right)/2;
-				if(key <= arr[mid])
-					right = mid;
-				else
-					left = mid + 1;
-			}
-			return left;
-		}
+    static int upperBinary(int arr[], int key) {    //  초과
+        int low = 0;
+        int high = arr.length;
 
-		public static int end(int arr[], int key) {
-			
-			int left = 0;
-			int right = arr.length;
-			
-			while(left < right){
-				int mid = (left + right)/2;
-				if(key < arr[mid])
-					right = mid;
-				else
-					left = mid + 1;
-			}
-			return left;
-		}
+        while (low < high) {
+            int mid = (low + high) / 2;
+
+            if (key < arr[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    static int lowerBinary(int arr[], int key) {    //  이상
+        int low = 0;
+        int high = arr.length;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+
+            if (key <= arr[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
 }
