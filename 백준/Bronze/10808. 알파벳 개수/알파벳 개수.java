e39b6b77d264f96a71f166a89a3f63e63e837ff6
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-25 14:29:35 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,16 +1,24 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-			int arr[] = new int [26];
-			String s = sc.next();
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-			for(int i = 0; i < s.length(); i++){
-				arr[s.charAt(i) - 97]++; 
-			}
-			for(int i = 0; i < arr.length; i++){
-				System.out.print(arr[i] + " ");
-			}
-		}
-	}
+        int alpha[] = new int[26];
+
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            alpha[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < alpha.length; i++) {
+            sb.append(alpha[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
