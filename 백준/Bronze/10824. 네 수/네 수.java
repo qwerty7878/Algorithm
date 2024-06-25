3efa65d5bb6789918afa055336d25d08b8a200a4
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-06-25 14:57:17 +0900

    [Bronze III] Title: 네 수, Time: 160 ms, Memory: 16088 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        long d = Long.parseLong(st.nextToken());
+
+        String s1 = "" + a + "" + b;
+        String s2 = "" + c + "" + d;
+
+        sb.append(Long.parseLong(s1) + Long.parseLong(s2));
+        System.out.println(sb);
+    }
+}
