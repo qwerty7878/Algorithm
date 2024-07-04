1667a6075c1905bdc0c19f3e176beafe6f2117ea
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-04 18:30:16 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,16 +1,26 @@
+import java.io.*;
 import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        
-        if(n == 4 || n == 7)
-            System.out.println(-1);
-        else if(n % 5 == 0)
-            System.out.println(n/5);
-        else if(n % 5 == 1 || n % 5 == 3)
-            System.out.println(n/5 + 1);
-        else if(n % 5 == 2 || n % 5 == 4)
-            System.out.println(n/5 + 2);
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        while (n > 0) {
+            if (n % 5 == 0) {
+                cnt += n / 5;
+                break;
+            }
+            else {
+                n -= 3;
+                cnt++;
+            }
+            if (n < 0) {
+                cnt = -1;
+            }
+        }
+        System.out.println(cnt);
     }
 }
