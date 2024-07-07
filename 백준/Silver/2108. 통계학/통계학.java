811cdf6cc1c692d0a36784c6cbb76c02ddc9b1eb
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-08 00:27:41 +0900

    [Silver III] Title: 통계학, Time: 472 ms, Memory: 47208 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,49 +1,45 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+public class Main {
 
-		int n = sc.nextInt();
-		int arr[] = new int [n];
-		double sum = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
 
-		for(int i = 0; i < n; i++){
-			arr[i] = sc.nextInt();
-			sum += arr[i];
-		}
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
 
-		Arrays.sort(arr);
+        int cnt = 0;
+        int max = -1;   //  cnt가 0이 존재하므로
+        boolean check = false;
+        int mod = arr[0];
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
 
-		int cnt = 0;
-		int max = -1;
-		int mod = arr[0];
-		boolean check = false;
-		//	최빈값
-		for(int i = 0; i < n - 1; i++){
-			if(arr[i] == arr[i + 1])	
-			//	정렬해서 i당시 배열값이랑 다음 값이랑 같으면 cnt 증가시킴
-				cnt++;
-			else
-				cnt = 0;
+            if (max < cnt) {
+                max = cnt;  //  최댓값 변경
+                mod = arr[i];   //  최빈값 설정
+                check = true;   //  두번째 최빈값임을 보여주기 위해 사용
+            } else if (max == cnt && check == true) {
+                mod = arr[i];   //   check가 true라는 것은 max 값이 더 크지 않다는 것, max == cnt 는 두번째 값이 됨
+                check = false;
+            }
+        }
 
-			if(max < cnt){
-				max = cnt;
-				mod = arr[i];
-				check = true;
-			}
-			else if(max == cnt && check == true){	
-			//	if문이 처리된 이후 max = cnt와 check = true가 되었기 때문에 mod 초기화 라고 생각
-				mod = arr[i];
-				check = false;
-			}
-		}
-
-		System.out.println(Math.round(sum/n));
-		System.out.println(arr[(n-1)/2]);
-		System.out.println(mod);
-		System.out.println(arr[n-1] - arr[0]);
-		
-	}
+        System.out.println(Math.round(sum/n));
+        System.out.println(arr[n/2]);
+        System.out.println(mod);
+        System.out.println(arr[n-1] - arr[0]);
+    }
 }
