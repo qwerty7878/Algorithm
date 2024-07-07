43bda7f7d23fba5e93899fe1cadde5cd63d25cd5
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-07 17:30:11 +0900

    [Bronze V] Title: SciComLove, Time: 124 ms, Memory: 14120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/21598. SciComLove/SciComLove.java
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -1,9 +1,15 @@
+import java.io.*;
 import java.util.*;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
             System.out.println("SciComLove");
         }
     }
