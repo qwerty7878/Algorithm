f2d5f405b2d5199515b618717f16862bde1793fb
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-08 04:24:15 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,25 +1,28 @@
+import java.io.*;
 import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int Cpaper = sc.nextInt();
-        int arr[][] = new int [100][100];
-        int cnt = 0;
-        
-        for(int i = 0; i < Cpaper; i++){
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            
-            for(int j = x; j < x+10; j++){
-                for(int k = y; k < y+10; k++){
-                    arr[j][k] = 1;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());    //  색종이 수 
+        boolean arr[][] = new boolean[101][101];    //  가로 세로 크기 각각 100인 정사각형 모양 흰색 도화지
+        int cnt = 0;    //  검은색 영역
+
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int n = Integer.parseInt(st.nextToken());   //  왼쪽
+            int m = Integer.parseInt(st.nextToken());   //  오른쪽
+
+            for (int j = n; j < n + 10; j++) {  // 10을 더하는 이유는 크기가 10인 검은색 색종이를 붙이니까
+                for (int k = m; k < m + 10; k++) {  //  색종이 아래쪽 변과 도화지의 아래쪽 변사이 거리
+                    // 두 거리를 곱하는 느낌?
+                    if (!arr[j][k]) {   //  도화지에 검은색 부분이니까 cnt값 증가
+                        arr[j][k] = true;
+                        cnt++;
+                    }
                 }
-            }
-        }
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[i].length; j++){
-                if(arr[i][j] == 1)
-                    cnt += arr[i][j];
             }
         }
         System.out.println(cnt);
