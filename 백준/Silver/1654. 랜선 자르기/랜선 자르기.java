c6c1fccdc27e4175181d4b65f9492959f37c4131
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-16 00:57:52 +0900

    [Silver II] Title: 랜선 자르기, Time: 192 ms, Memory: 17452 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,65 +1,47 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		// 이진탐색
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-		int K = sc.nextInt();
-		int N = sc.nextInt(); // 자를 랜선 갯수
- 
-		int[] arr = new int[K];
-		
-		long max = 0;
-		
-		// 입력과 동시에 해당 랜선의 길이가 최댓값인지를 확인하고 max를 갱신
-		for (int i = 0; i < K; i++) {
-			arr[i] = sc.nextInt();
-			if(max < arr[i]) {
-				max = arr[i];
-			}
-		}
-		
-		// 반드시 max에서 +1 값이어야 한다.
-		max++; 
- 
-		long min = 0; // 탐색 길이 최솟값
-		long mid = 0; 
- 
-		while (min < max) { 
-			
-			// 범위 내에서 중간 길이를 구한다.
-			mid = (max + min) / 2;
-			
- 
-			long cnt = 0;
-			
-			// 구해진 중간 길이로 잘라서 총 몇 개가 만들어지는지를 구한다.
- 
-			for (int i = 0; i < arr.length; i++) {
-				cnt += (arr[i] / mid);
-			}
-			
-			/*
-			 *  [upper bound 형식]
-			 *  
-			 *  mid길이로 잘랐을 때의 개수가 만들고자 하는 랜선의 개수보다 작다면
-			 *  자르고자 하는 길이를 줄이기 위해 최대 길이를 줄인다.
-			 *  그 외에는 자르고자 하는 길이를 늘려야 하므로 최소 길이를 늘린다.
-			 */
-			if(cnt < N) {
-				max = mid;
-			}
-			else {
-				min = mid + 1;
-			}
-			
- 
-		}
-		
-		// UpperBound로 얻어진 값(min)에 -1이 최대 길이가 된다.
-		System.out.println(min - 1);
-	}
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());   //  보유한 랜선 갯수
+        int n = Integer.parseInt(st.nextToken());   //  필요한 랜선 갯수
+
+        int arr[] = new int[k];
+        for (int i = 0; i < k; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);   //  최대값 찾기 위해 정렬
+
+        long max = arr[k - 1];  //  정렬된 배열 맨 마지막값이 최대값
+        long min = 1;
+        long mid;
+        long cnt;
+
+        while (min <= max) {
+            mid = (max + min) / 2;  //  이분 탐색을 위한 중간값
+            cnt = 0;
+            for (int i = 0; i < arr.length; i++) {
+                cnt += (arr[i] / mid);
+            }
+
+            if (cnt < n) {
+                //  예상치보다 적다면 분모값을 늘려야한다
+                //  분모인 mid의 크기를 작게해야한다
+                //  작게하려면 mid에서 max값을 줄인다
+                max = mid - 1;
+            } else {
+                //  예상치보다 크다면 분모값을 줄여야한다
+                //  분모인 mid의 크기를 크게해야한다
+                //  크게하려면 mid에서 min값을 줄인다
+                min = mid + 1;
+            }
+        }
+        System.out.println(max);
+    }
 }
