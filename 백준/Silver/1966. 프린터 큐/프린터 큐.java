bc3dd40258ed52fdad828156a4fbdd507d285cae
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-15 19:15:37 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 15180 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,51 +1,60 @@
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		int t = sc.nextInt();
+        int t = Integer.parseInt(br.readLine());    //  테스트케이스
 
-		for(int i = 0; i < t; i++){
-			int n = sc.nextInt();
-			int m = sc.nextInt();
+        while (t --> 0){    //  테스트 케이스 갯수만큼 반복
+            st = new StringTokenizer(br.readLine());
 
-			LinkedList<int[]> q = new LinkedList<>();
+            int n = Integer.parseInt(st.nextToken());   //  문서 갯수
+            int m = Integer.parseInt(st.nextToken());   //  몇번째에 놓을지
 
-			for(int j = 0; j < n; j++){
-				int num = sc.nextInt();
-				q.add(new int[] {j,num});
-			}
+            LinkedList<int []> q = new LinkedList<>();  //  인덱스 값을 사용하기 위해 배열을 사용
 
-			int cnt = 0;
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                q.add(new int[]{i, Integer.parseInt(st.nextToken())});  //  중요도
+            }
 
-			while(!q.isEmpty()){
-				int front[] = q.poll();
-				boolean isMax = true;
+            int cnt = 0;
+            while (!q.isEmpty()) {  //  한 테스트 케이스 확인
 
-				for(int k = 0; k < q.size(); k++){
-					if(front[1] < q.get(k)[1]){
-						q.add(front);
-						for(int j = 0; j < k; j++){
-							q.add(q.poll());
-						}
+                int temp[] = q.poll();
+                boolean isMax = true;
+                // 최대값 초기값 설정
 
-						isMax = false;
-						break;
-					}
-				}
-				if(isMax == false){
-					continue;
-				}
+                for (int i = 0; i < q.size(); i++) {
+                    if (temp[1] < q.get(i)[1]) {
+                        //  현재 max값보다 큰 값이면 현재값은 q에 추가
+                        q.add(temp);
+                        for (int j = 0; j < i; j++) {
+                            q.add(q.poll());    //  최대값 찾기
+                        }
 
-				cnt++;
-				if(front[0] == m)
-					break;
-			}
-			sb.append(cnt).append('\n');
-		}
-		System.out.println(sb.toString());
-	}
+                        isMax = false;  //  최대값일때 정지
+                        break;
+                    }
+                }
+
+                if (isMax == false) {
+                    continue;
+                }
+
+                cnt++;
+                if (temp[0] == m) { //  인덱스 값과 중요도가 같다면 정지
+                    break;
+                }
+            }
+
+            sb.append(cnt).append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
