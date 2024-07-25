0d52d03f057381c8482fe5dc4380c7571f0743b8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-25 21:33:27 +0900

    [Silver II] Title: 최대 힙, Time: 792 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n != 0) {
+                q.add(n);
+            } else {
+                if (q.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(q.poll());
+                }
+            }
+        }
+    }
+}
