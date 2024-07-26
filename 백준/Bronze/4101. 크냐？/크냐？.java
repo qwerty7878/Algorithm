aa99a6c5a8c3b6b828a3b5fadbfd88238db3566d
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-26 22:54:07 +0900

    [Bronze V] Title: 크냐?, Time: 132 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.java
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -1,17 +1,24 @@
-import java.util.*;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(true){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            if(a == 0 && b == 0)
-                break;
-            else if(a > b)
-                System.out.println("Yes");
-            else
-                System.out.println("No");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(a == 0 && b == 0)    break;
+
+            if (a > b) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
         }
     }
 }
