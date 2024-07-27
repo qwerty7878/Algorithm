d977f65c7dafa4980fa382e436a4b2d1dfa247f4
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-07-28 02:55:31 +0900

    [Gold IV] Title: 오큰수, Time: 988 ms, Memory: 145068 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -4,34 +4,37 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Stack<Integer> stack = new Stack<>();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(br.readLine());
-		int arr[] = new int [n];
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < n; i++){
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		for(int i = 0; i < n; i++){
-			while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
-				arr[stack.pop()] = arr[i];
-			}
-			stack.push(i);
-		}
+        Stack<Integer> stack = new Stack<>();
 
-		while(!stack.isEmpty()){
-			arr[stack.pop()] = -1;
-		}
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
 
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < n; i++){
-			sb.append(arr[i]).append(' ');
-		}
-		System.out.println(sb);
-	}
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb.toString());
+    }
 }
