1eba36d7da7db168ed8585eae30de71898a0dbbb
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-08-06 00:48:43 +0900

    [Bronze V] Title: We love kriii, Time: 100 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/10718. We love kriii/We love kriii.java
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("강한친구 대한육군");
         System.out.println("강한친구 대한육군");
-            
+
     }
 }
