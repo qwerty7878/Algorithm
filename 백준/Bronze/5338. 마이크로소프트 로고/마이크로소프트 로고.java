088cb671923790101721144a7326b6c407c7db6d
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-08-09 23:44:15 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 13928 KB -BaekjoonHub

--- 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -1,10 +1,9 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
         System.out.println("       _.-;;-._");
         System.out.println("'-..-'|   ||   |");
         System.out.println("'-..-'|_.-;;-._|");
         System.out.println("'-..-'|   ||   |");
         System.out.println("'-..-'|_.-''-._|");
-        
     }
 }
