9c74ffdeaaae3b9f791860af98acf4812cb8c98f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-08-22 02:16:26 +0900

    [Bronze V] Title: 카드 게임, Time: 168 ms, Memory: 17676 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5522. 카드 게임/카드 게임.java
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -1,9 +1,11 @@
 import java.util.*;
-public class Main{
-    public static void main(String[] args) {
+
+class Main{
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        
         int sum = 0;
-        for(int i = 1; i <= 5; i++){
+        for(int i = 0; i < 5; i++){
             int n = sc.nextInt();
             sum += n;
         }
