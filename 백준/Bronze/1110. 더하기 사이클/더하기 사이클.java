911e3d7154c0f97be8434db32463340689d6d144
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-08-29 17:04:22 +0900

    [Bronze I] Title: 더하기 사이클, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,18 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main{
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int num = n;
-		int cnt = 0;
-		while(true){
-			n = (n%10)*10 + (((n/10) + (n%10))%10);
-			cnt++;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-			if(num == n)
-				break;
-		}
-		System.out.println(cnt);
-	}
+        int n = Integer.parseInt(br.readLine());
+        int number = n;
+        int cnt = 1;
+
+         do {
+            int first = number % 10;
+            int second = number / 10;
+            int sum = (first + second) % 10;
+            
+            number = first * 10 + sum;
+
+            if (n == number) {
+                break;
+            } else {
+                cnt++;
+            }
+        }while (true);
+
+        System.out.println(cnt);
+    }
 }
