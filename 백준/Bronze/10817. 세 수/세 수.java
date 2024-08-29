fafa9dad42e84c62feacb2f8d3f09844f61e2e44
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-08-29 17:29:09 +0900

    [Bronze III] Title: 세 수, Time: 100 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,12 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int arr[] = new int [3];
-        for(int i = 0; i < 3; i++){
-            arr[i] = sc.nextInt();
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int arr[] = new int[3];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
+
         System.out.println(arr[1]);
     }
 }
