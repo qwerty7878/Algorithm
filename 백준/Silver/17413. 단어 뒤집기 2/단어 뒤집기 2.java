7a0a9784a8add3cdbac9e04788b1ca592b849fae
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-09-13 20:52:52 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 216 ms, Memory: 22736 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -3,46 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		Stack<Character>stack = new Stack<>();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
 
-		String s = br.readLine();
-		boolean check = false;
+        boolean isClose = false;
 
-		for(int i = 0; i < s.length(); i++){
-			if(s.charAt(i) == '<'){
-				while(!stack.isEmpty()){
-					sb.append(stack.pop());
-				}
-				check = true;
-			}
-			else if(s.charAt(i) == '>'){
-				check = false;
-				sb.append(s.charAt(i));
-				continue;
-			}
-			if(check == true){
-				sb.append(s.charAt(i));
-			}
-			else if(check == false){
-				if(s.charAt(i) == ' '){
-					while(!stack.isEmpty()){
-						sb.append(stack.pop());
-					}
-					sb.append(' ');
-					continue;
-				}
-				else
-					stack.push(s.charAt(i));
-			}
-			if(i == s.length() - 1){
-				while(!stack.isEmpty())
-					sb.append(stack.pop());
-			}
-		}
-		System.out.println(sb);
-	}
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '<') {
+                isClose = true; //  괄호 표시
+                while (!stack.isEmpty()) {
+                    sb.append(stack.pop());
+                }
+                sb.append(s.charAt(i)); //  < 추가
+            } else if (s.charAt(i) == '>') {
+                isClose = false;    //  괄호 끝
+                sb.append(s.charAt(i)); //  > 추가
+            } else if (isClose == true) {
+                sb.append(s.charAt(i)); //  괄호 안 내용은 그대로 출력
+            } else if (isClose == false) {
+                if (s.charAt(i) == ' ') {
+                    while (!stack.isEmpty()) {
+                        sb.append(stack.pop());
+                    }
+                    sb.append(s.charAt(i));
+                } else {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        System.out.println(sb.toString());
+    }
 }
