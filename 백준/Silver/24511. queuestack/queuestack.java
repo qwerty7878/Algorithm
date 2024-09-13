cd28a445bba4e0962fb6e10ee76760cd68b92cde
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-09-14 01:27:26 +0900

    [Silver III] Title: queuestack, Time: 552 ms, Memory: 63460 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.java
+++ 백준/Silver/24511. queuestack/queuestack.java
@@ -1,40 +1,46 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		Deque<Integer> dq = new ArrayDeque<>();
 
-		int n = Integer.parseInt(br.readLine()); // 갯수
-		int arr[] = new int [n]; // 객수 n 인 배열 생성
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++){ // n 크기 배열에 스택인지 팝인지 값 할당
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
+        Deque<Integer> dq = new ArrayDeque<>();
 
-		st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
 
-		for(int i = 0; i < n; i++){
-			int num = Integer.parseInt(st.nextToken()); // 수열 B 값 할당
-			if(arr[i] == 0) // 큐는 FIFO
-				dq.addLast(num);
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		int m = Integer.parseInt(br.readLine()); // 삽입할 수열 길이
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < m; i++){ 	// 삽입할 값 할당
-			dq.addFirst(Integer.parseInt(st.nextToken()));
-			bw.write(dq.pollLast() + " ");
-		}
-		bw.flush();
-	}
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (arr[i] == 0) {  //  어차피 스택일땐 push하고 pop하면 그대로임
+                dq.addLast(num);
+            }
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        int insert[] = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            insert[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            dq.addFirst(insert[i]);
+            sb.append(dq.pollLast() + " ");
+        }
+        System.out.println(sb.toString());
+    }
 }
