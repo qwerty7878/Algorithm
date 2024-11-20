3f35186219912cb7ffa883d222ae5eeeb253345d
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2024-11-21 01:31:20 +0900

    [Bronze III] Title: 웰컴 키트, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -1,38 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int cnt = 0;
-        int n = Integer.parseInt(br.readLine());
-        int size[] = new int[6];
+        int n = sc.nextInt();
 
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < size.length; i++) {
-            size[i] = Integer.parseInt(st.nextToken());
+        int arr[] = new int[6];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
         }
-
-        st = new StringTokenizer(br.readLine(), " ");
-        int t = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-
-        for (int i = 0; i < size.length; i++) {
-            cnt += size[i]/t;
-            if (size[i] % t != 0) {
+        int t = sc.nextInt();
+        int p = sc.nextInt();
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            cnt += arr[i] / t;
+            if (arr[i] % t != 0) {
                 cnt++;
             }
         }
 
-        sb.append(cnt)
-                .append("\n")
-                .append((n / p) + " " + (n % p));
-
-        System.out.println(sb);
+        System.out.println(cnt);
+        System.out.printf("%d %d",(n / p),(n % p));
     }
 }
