2f077230ab435c8c66b22766039fcfceb6cdcfd0
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2024-12-10 00:32:55 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,20 +1,16 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-
-        int student[] = new int [31];
-
-        for(int i = 0; i < 28; i++){
-            int hw = sc.nextInt();
-            student[hw] = 1;
+        int student[] = new int[31];
+        for(int i = 1; i <= 28; i++){
+            int good = sc.nextInt();
+            student[good] = 1;
         }
-
-        for(int i = 1; i <= 30; i++){
-            if(student[i] != 1) System.out.println(i);
+        for(int i = 1; i < student.length; i++){
+            if(student[i] != 1)
+                System.out.println(i);
         }
-
-        sc.close();
+        
     }
 }
