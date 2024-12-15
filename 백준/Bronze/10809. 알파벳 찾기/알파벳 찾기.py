038c802e1bcfbe72bf10eba32e5330d7f95a27be
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2024-12-15 14:36:03 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,3 +1,10 @@
+# str = input()
+# for i in 'abcdefghijklmnopqrstuvwxyz':
+#     print(str.find(i), end=" ")
+
 str = input()
 for i in 'abcdefghijklmnopqrstuvwxyz':
-    print(str.find(i), end=" ")
+    if i in str:
+        print(str.index(i), end=" ")
+    else:
+        print(-1, end=" ")
