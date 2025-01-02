417843e26c85a5a9d8da1de7dfbb1467999bfe1e
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-01-02 16:53:07 +0900

    [Bronze V] Title: A+B - 3, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,5 +1,5 @@
-num = int(input())
+t = int(input())
 
-for i in range(num):
-    a,b = map(int,input().split())
-    print(a+b)
+for i in range(t):
+    a,b = map(int, input().split())
+    print(a + b)
