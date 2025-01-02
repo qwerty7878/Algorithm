63758ac186f665dda54f317b7d41ee66ad310173
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-01-03 02:20:25 +0900

    [Bronze II] Title: 나머지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,6 +1,9 @@
 arr = []
+
 for i in range(10):
     num = int(input())
-    if (num % 42 not in arr):
-        arr.append(num % 42)
-print(len(arr))
+    # if num % 42 not in arr:
+    arr.append(num % 42)
+
+# print(len(arr))
+print(len(set(arr)))
