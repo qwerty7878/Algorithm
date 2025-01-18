4a8211be0f8657ac898db2b3e97d492466af2f35
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-01-18 22:23:01 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,19 +1,13 @@
 arr = []
 
-for i in range(9):
-    arr.append(list(map(int, input().split())))
-
-maxNum = 0
-row = 0
-col = 0
+for _ in range(9):
+    num = list(map(int, input().split()))
+    arr.append(num)
 
 for i in range(9):
     for j in range(9):
-        if arr[i][j] >= maxNum:
-        # if arr[i][j] > maxNum: 로 하면 이 전에 같은 경우일 때 행 렬이 그대로 되어 있음
-            maxNum = arr[i][j]
-            row = i + 1
-            col = j + 1
+        if arr[i][j] == max(map(max,arr)):
+            row,col = i + 1, j + 1
 
-print(maxNum)
-print(row, col)
+print(max(map(max,arr)))
+print(row,col)
