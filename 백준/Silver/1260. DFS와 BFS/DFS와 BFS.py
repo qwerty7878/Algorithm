e13a992e3d64492bf4907c0c86be3e54aebb3b21
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-01-20 21:34:01 +0900

    [Silver II] Title: DFS와 BFS, Time: 404 ms, Memory: 40416 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,33 +1,36 @@
-# 입력
-n, m, v = map(int, input().split())
+from collections import deque
 
-graph = [[0] * (n+1) for _ in range(n+1)]
+n,m,v = map(int,input().split())
+arr = [[0] * (n + 1) for i in range(n + 1)]
 
 for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = graph[b][a] = 1
+    a,b = map(int,input().split())
+    arr[a][b] = 1
+    arr[b][a] = 1
 
-visited_dfs = [0] * (n+1)
-visited_bfs = [0] * (n+1)
+visited_dfs = [False] * (n + 1)
+visited_bfs = [False] * (n + 1)
 
 def dfs(v):
-    visited_dfs[v] = 1
+    visited_dfs[v] = True
     print(v, end=' ')
-    for i in range(1, n+1):
-        if graph[v][i] == 1 and visited_dfs[i] == 0:
+
+    for i in range(1, n + 1):
+        if not visited_dfs[i] and arr[v][i] == 1:
             dfs(i)
 
-from collections import deque
 def bfs(v):
     queue = deque([v])
-    visited_bfs[v] = 1
+    visited_bfs[v] = True
+
     while queue:
-        node = queue.popleft()
-        print(node, end=' ')
-        for i in range(1, n+1):
-            if graph[node][i] == 1 and visited_bfs[i] == 0:
+        v = queue.popleft()
+        print(v, end=' ')
+
+        for i in range(1, n + 1):
+            if not visited_bfs[i] and arr[v][i] == 1:
                 queue.append(i)
-                visited_bfs[i] = 1
+                visited_bfs[i] = True
 
 dfs(v)
 print()
