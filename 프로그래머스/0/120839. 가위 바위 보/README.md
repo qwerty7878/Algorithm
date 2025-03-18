ca8f618e773888676465989cc2d54ff9ecf116d4
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-03-18 19:11:57 +0900

    [level 0] Title: 가위 바위 보, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/README.md
+++ 프로그래머스/0/120839. 가위 바위 보/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가위 바위 보 - 120839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.02 ms
+메모리: 9.21 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 17일 17:59:59
+2025년 03월 18일 19:11:54
 
 ### 문제 설명
 
--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,10 +1,11 @@
 def solution(rsp):
     answer = ''
-    for i in rsp:
-        if i == '0':
+    for i in range(len(rsp)):
+        if rsp[i] == '0':
             answer += '5'
-        elif i == '5':
-            answer += '2'
-        elif i == '2':
+        elif rsp[i] == '2':
             answer += '0'
+        elif rsp[i] == '5':
+            answer += '2'
+            
     return answer
