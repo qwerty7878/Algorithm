1dbc1661eb8d0646ecda75bd35739d22fb519090
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-03-18 16:07:07 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/README.md
+++ 프로그래머스/0/120841. 점의 위치 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 점의 위치 구하기 - 120841 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120841?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120841) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.00 ms
+메모리: 9.14 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 01일 17:42:01
+2025년 03월 18일 16:07:05
 
 ### 문제 설명
 
--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,11 +1,11 @@
 def solution(dot):
-    answer = 0
-    if (dot[0] > 0 and dot[1] > 0 ):
-        answer = 1
-    elif(dot[0] < 0 and dot[1] > 0):
-        answer = 2
-    elif(dot[0] < 0 and dot[1] < 0):
-        answer = 3
-    elif(dot[0] > 0 and dot[1] < 0):
-        answer = 4
-    return answer
+    if dot[0] > 0 and dot[1] > 0:
+        return 1
+    elif dot[0] < 0 and dot[1] > 0:
+        return 2
+    elif dot[0] < 0 and dot[1] < 0:
+        return 3
+    if dot[0] > 0 and dot[1] < 0:
+        return 4
+    
+    
