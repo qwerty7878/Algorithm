954b8c3bd45dc94aef38ccf462ec6f2c07cea4bf
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-03-19 14:06:23 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 9.42 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/README.md
+++ 프로그래머스/0/181939. 더 크게 합치기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 더 크게 합치기 - 181939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181939) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 9.42 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 18일 22:55:21
+2025년 03월 19일 14:06:21
 
 ### 문제 설명
 
--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,11 +1,6 @@
 def solution(a, b):
-    answer1 = ''
-    answer2 = ''
+    if int(str(a) + str(b)) >= int(str(b) + str(a)):
+        return int(str(a) + str(b))
+    elif int(str(a) + str(b)) < int(str(b) + str(a)):
+        return int(str(b) + str(a))
     
-    answer1 = str(a) + str(b)
-    answer2 = str(b) + str(a)
-    
-    if answer1 > answer2:
-        return int(answer1)
-    else:
-        return int(answer2)
