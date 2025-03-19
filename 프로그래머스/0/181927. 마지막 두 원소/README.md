3e3d9b4c23ae010abcc3303f5ff81abfd831abe4
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-03-19 19:49:23 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/README.md
+++ 프로그래머스/0/181927. 마지막 두 원소/README.md
@@ -1,10 +1,10 @@
 # [level 0] 마지막 두 원소 - 181927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181927?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181927) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 9.23 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 02일 18:31:46
+2025년 03월 19일 19:49:21
 
 ### 문제 설명
 
--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,10 +1,8 @@
 def solution(num_list):
-    answer = []
-    for i in num_list:
-        answer.append(i)
-    
-    if num_list[len(num_list) - 1] > num_list[len(num_list) - 2]:
-        answer.append(num_list[len(num_list) - 1] - num_list[len(num_list) - 2])
+    answer = [i for i in num_list]
+    if num_list[-1] > num_list[-2]:
+        answer.append(num_list[-1] - num_list[-2])
     else:
-        answer.append(num_list[len(num_list) - 1] * 2)
+        answer.append(num_list[-1] * 2)
+        
     return answer
