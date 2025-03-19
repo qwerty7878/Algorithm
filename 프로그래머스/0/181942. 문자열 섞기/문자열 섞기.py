f5cf0fc65e330d970ec7f98ab2c5022874e43a11
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-03-19 16:34:11 +0900

    [level 0] Title: 문자열 섞기, Time: 0.01 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,8 +1,5 @@
 def solution(str1, str2):
     answer = ''
-    for i in range(0,len(str1 + str2)):
-        if len(answer) % 2 == 0:
-            answer += str1[i//2]
-        else:
-            answer += str2[i//2]
+    for i in range(0,len(str1)):
+        answer += str1[i] + str2[i]
     return answer
