17ba41db01e43bbd5446da84a193a0bfa2f9e435
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-03-19 14:41:32 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/README.md
+++ 프로그래머스/0/181934. 조건 문자열/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건 문자열 - 181934 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181934?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181934) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 9.16 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 16:37:55
+2025년 03월 19일 14:41:30
 
 ### 문제 설명
 
--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,15 +1,23 @@
 def solution(ineq, eq, n, m):
-    answer = 0
+    if ineq == '>' and eq == '=':
+        if n >= m:
+            return 1
+        else:
+            return 0
+    if ineq == '>' and eq == '!':
+        if n > m:
+            return 1
+        else:
+            return 0
     if ineq == '<' and eq == '=':
         if n <= m:
-            answer = 1
-    elif ineq == '>' and eq == '=':
-        if n >= m:
-            answer = 1
-    elif ineq == '<' and eq == '!':
+            return 1
+        else:
+            return 0
+    if ineq == '<' and eq == '!':
         if n < m:
-            answer = 1
-    elif ineq == '>' and eq == '!':
-        if n > m:
-            answer = 1
-    return answer
+            return 1
+        else:
+            return 0
+    
+    
