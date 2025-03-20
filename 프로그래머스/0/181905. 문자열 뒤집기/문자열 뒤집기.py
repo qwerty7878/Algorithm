e538d79135262a8f9ba9dd6f35722774dacbcfaf
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-03-20 14:41:48 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -2,3 +2,5 @@ def solution(my_string, s, e):
     answer = list(my_string)
     answer[s:e + 1] = reversed(answer[s:e + 1])
     return ''.join(answer)
+
+    # return my_string[:s] + my_string[s:e + 1][::-1] + my_string[e + 1:]
