76c44fe6cea0e18a705b6d309e75955dd09d17cb
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-04-20 05:13:23 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,10 +1,21 @@
-dial = ['ABC','DEF','GHI','JKL','MNO','PQRS','TUV','WXYZ']
-word = input()
-ans = 0
-for i in dial:
-    for j in word:
-        if j in i:
-            ans += dial.index(i) + 2
+str = input()
+time = len(str)
+for i in str:
+    if i == 'A' or i == 'B' or i == 'C':
+        time += 2
+    elif i == 'D' or i == 'E' or i == 'F':
+        time += 3
+    elif i == 'G' or i == 'H' or i == 'I':
+        time += 4
+    elif i == 'J' or i == 'K' or i == 'L':
+        time += 5
+    elif i == 'M' or i == 'N' or i == 'O':
+        time += 6
+    elif i == 'P' or i == 'Q' or i == 'R' or i == 'S':
+        time += 7
+    elif i == 'T' or i == 'U' or i == 'V':
+        time += 8
+    elif i == 'W' or i == 'X' or i == 'Y' or i == 'Z':
+        time += 9
 
-ans += len(word)
-print(ans)
+print(time)
