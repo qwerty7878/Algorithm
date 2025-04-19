237474c8a6e7f6f72fc0f3aa32a3cdc420452644
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-04-20 01:44:55 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,13 +1,8 @@
 n,m = map(int,input().split())
-
 arr = [i for i in range(1,n+1)]
 
-for _ in range(m):
-    i,j = map(int,input().split())
+for i in range(m):
+    a,b = map(int,input().split())
+    arr[a-1:b] = reversed(arr[a-1:b])
 
-    temp = arr[i - 1: j]
-    temp.reverse()
-    arr[i - 1:j] = temp
-
-for i in arr:
-    print(i, end=' ')
+print(*arr)
