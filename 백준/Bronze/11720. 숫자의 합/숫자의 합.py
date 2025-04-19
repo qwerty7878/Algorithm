c6a58c155d49735a73ff3583e6927d5e44b7ef30
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-04-20 04:58:47 +0900

    [Bronze IV] Title: 숫자의 합, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,6 +1,14 @@
-n = int(input())
-str = input()
+n = input()
+num = int(input())
+
 sum = 0
-for i in range(n):
-    sum += int(str[i])
+while num > 10:
+    sum += num % 10
+    num = num // 10
+
+sum += num % 10
+
+if num >= 10:
+    num = num // 10
+    sum += num % 10
 print(sum)
