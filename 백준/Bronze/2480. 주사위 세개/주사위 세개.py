927a88c185702450ed18cda42751453754b8d1f9
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-04-19 23:08:40 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,16 +1,13 @@
-a,b,c = map(int, input().split())
-prize = 0
-if(a == b and b == c and a == c):
-    prize = 10000 + (a * 1000)
-    print(prize)
-elif(a != b and b != c and a != c):
-    prize = max(a,b,c) * 100
-    print(prize)
+a,b,c = map(int,input().split())
+
+if a == b and b == c and c == a:
+    print(10000 + (a * 1000))
+elif a != b and  a != c and b != c:
+    print(max(a,b,c) * 100)
 else:
-    if(a == b):
-        prize = 1000 + (a * 100)
-    elif (a == c):
-        prize = 1000 + (a * 100)
-    elif (c == b):
-        prize = 1000 + (b * 100)
-    print(prize)
+    if a == b:
+        print(1000 + (a * 100))
+    if a == c:
+        print(1000 + (a * 100))
+    if c == b:
+        print(1000 + (c * 100))
