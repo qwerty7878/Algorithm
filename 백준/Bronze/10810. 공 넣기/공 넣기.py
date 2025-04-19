3eb62275cdfef793315ce6dff00bfd49ff07fec7
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-04-20 01:11:04 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,12 +1,8 @@
 n,m = map(int,input().split())
+arr = [0] * n
+for i in range(m):
+    a,b,c = map(int,input().split())
+    for j in range(a -1,b):
+        arr[j] = c
 
-arr = [0 for i in range(n)]
-
-for _ in range(m):
-    i,j,k = map(int,input().split())
-
-    for a in range(i,j + 1):
-        arr[a - 1] = k
-
-for i in arr:
-    print(i,end=" ")
+print(*arr)
