3771710ca2914c27a6201eb43c8b6da26434bf7d
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-04-20 01:25:45 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,8 +1,8 @@
-arr = [i for i in range(1,31)]
-
+student = [i for i in range(1, 31)]
 for i in range(28):
-    n = int(input())
-    arr.remove(n)
+    num = int(input())
+    student.remove(num)
 
-print(min(arr))
-print(max(arr))
+student.sort()
+for i in student:
+    print(i)
