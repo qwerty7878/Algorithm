8fb63e6d0b525f0453379d7f443aacbcea7fb33d
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-05-01 14:23:22 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,12 +1,26 @@
+# 단어 개수
 n = int(input())
-cnt = n
 
+# 개수 저장
+count = n
+
+# 단어 개수 반복
 for i in range(n):
+
+    # 단어 입력
     word = input()
-    for j in range(0,len(word) - 1):
+
+    # 단어 하나하나 확인하기
+    for j in range(0, len(word) - 1):
+        # 바로 그 다음에 나오는 경우   ex) happy
         if word[j] == word[j + 1]:
-            pass
+            continue
+
+        # 바로 그 다음 경우를 제외하고 이후에 나오는 경우
         elif word[j] in word[j + 1:]:
-            cnt -= 1
+
+            # 더이상 볼 가치도 없음
+            count -= 1
             break
-print(cnt)
+# 출력
+print(count)
