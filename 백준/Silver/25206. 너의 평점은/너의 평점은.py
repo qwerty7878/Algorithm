779fe419187cf619b2742fe7fc16f45c8fe4e473
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-05-01 14:39:35 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,33 +1,44 @@
-total = 0
-div = 0
+# 점수 합 초기값
+totalsum = 0
+scoresum = 0
 
-for _ in range(20):
+#   20개 반복
+for i in range(20):
+
+    #   과목명, 학점, 등급
     name,score,grade = input().split()
+
+    #   학점은 숫자로
     score = float(score)
 
+    #   전공평점은 전공과목별 (학점 × 과목평점)의 합
+    if grade == 'A+':
+        totalsum += (score * 4.5)
+    if grade == 'A0':
+        totalsum += (score * 4.0)
+    if grade == 'B+':
+        totalsum += (score * 3.5)
+    if grade == 'B0':
+        totalsum += (score * 3.0)
+    if grade == 'C+':
+        totalsum += (score * 2.5)
+    if grade == 'C0':
+        totalsum += (score * 2.0)
+    if grade == 'D+':
+        totalsum += (score * 1.5)
+    if grade == 'D0':
+        totalsum += (score * 1.0)
+    if grade == 'F':
+        totalsum += (score * 0.0)
+    if grade == 'P':
+        pass
+
+    #   p는 계산에서 제외
     if grade == 'P':
         continue
 
-    if grade == 'A+':
-        grade_score = 4.5
-    elif grade == 'A0':
-        grade_score = 4.0
-    elif grade == 'B+':
-        grade_score = 3.5
-    elif grade == 'B0':
-        grade_score = 3.0
-    elif grade == 'C+':
-        grade_score = 2.5
-    elif grade == 'C0':
-        grade_score = 2.0
-    elif grade == 'D+':
-        grade_score = 1.5
-    elif grade == 'D0':
-        grade_score = 1.0
-    elif grade == 'F':
-        grade_score = 0.0
+    #   학점의 총합
+    scoresum += score
 
-    total += score * grade_score
-    div += score
-
-print(f'{total / div :.6f}')
+#   6자리까지 출력
+print(f'{totalsum / scoresum:0.6f}')
