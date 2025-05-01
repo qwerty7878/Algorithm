7625fb2bbd89db489a3042fff5d15e9a540056f1
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-05-01 14:49:15 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,16 +1,24 @@
+#   입력
 n,m = map(int,input().split())
 
-a = []
-for i in range(n):
-    num1 = list(map(int,input().split()))
-    a.append(num1)
+#   리스트 형태로 한줄에 저장하므로 2차원 배열이지만 1차원 배열을 만들어도 가능함
+arrA = []
+arrB = []
 
-b = []
+#   리스트 전체를 한줄에 저장
 for i in range(n):
-    num1 = list(map(int,input().split()))
-    b.append(num1)
+    arrA.append(list(map(int,input().split())))
 
+#   3 3 3 이렇게 한번에 입력되므로
+for i in range(n):
+    arrB.append(list(map(int,input().split())))
+
+#   n * m 배열 전체 순회
 for i in range(n):
     for j in range(m):
-        print(a[i][j] + b[i][j],end=' ')
+        
+        #   배열 덧셈
+        print(arrA[i][j] + arrB[i][j], end=" ")
+    
+    #   줄 띄어쓰기
     print()
