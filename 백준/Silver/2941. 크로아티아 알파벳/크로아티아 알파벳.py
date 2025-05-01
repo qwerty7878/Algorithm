ca899cd6932dfa4e4495ebc3d7e7a88a14492899
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-05-01 12:37:45 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,10 +1,31 @@
-str = input()
-str = str.replace('c=','@')
-str = str.replace('c-','@')
-str = str.replace('dz=','@')
-str = str.replace('d-','@')
-str = str.replace('lj','@')
-str = str.replace('nj','@')
-str = str.replace('s=','@')
-str = str.replace('z=','@')
-print(len(str))
+# 문자열 입력
+str = input().strip()
+changeStr = str
+
+# 크로아티아 알파벳 변경
+if 'c=' in str:
+    changeStr = changeStr.replace('c=', '*')
+
+if 'c-' in str:
+    changeStr = changeStr.replace('c-', '*')
+
+if 'dz=' in str:
+    changeStr = changeStr.replace('dz=', '*')
+
+if 'd-' in str:
+    changeStr = changeStr.replace('d-', '*')
+
+if 'lj' in str:
+    changeStr = changeStr.replace('lj', '*')
+
+if 'nj' in str:
+    changeStr = changeStr.replace('nj', '*')
+
+if 's=' in str:
+    changeStr = changeStr.replace('s=', '*')
+
+if 'z=' in str:
+    changeStr = changeStr.replace('z=', '*')
+
+# 문자열 개수 출력
+print(len(changeStr))
