61801a1ab35f05560bdc35ba18610ede60360cf0
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-05-22 00:56:38 +0900

    [Bronze II] Title: 벌집, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,8 +1,8 @@
 n = int(input())
-cnt = 1
-num = 1
-while num < n:
-    num += 6 * cnt
-    cnt += 1
 
+cnt,room,index = 1,1,1
+while n > room:
+    room += 6 * index
+    cnt += 1
+    index += 1
 print(cnt)
