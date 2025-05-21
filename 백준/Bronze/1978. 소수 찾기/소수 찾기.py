da448ca234e0518e30e0951bb78c20e0395fcfa4
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-05-22 02:54:15 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,20 +1,13 @@
-import math
-
 n = int(input())
 arr = list(map(int, input().split()))
-cnt = 0
 
+cnt = 0
 for i in arr:
     if i < 2:
         continue
-
-    isPrime = True
-    for j in range(2, int(math.sqrt(i)) + 1):
+    for j in range(2, int(i ** 0.5) + 1):
         if i % j == 0:
-            isPrime = False
             break
-
-    if isPrime:
+    else:
         cnt += 1
-
 print(cnt)
