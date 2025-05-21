7145aabdea62441631522854df587ce091e28e78
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-05-22 01:57:57 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,9 +1,11 @@
-import math
-
 a,b,v = map(int,input().split())
-day = 1
-v -= a
-climb = a - b
-day += math.ceil(v / climb)
 
-print(day)
+day = 1
+v = v - a # 이미 하루 올라간 상태
+
+day += v / (a - b) # 이후 갭 만큼 증가
+
+if day == int(day):
+    print(int(day))
+else:
+    print(int(day) + 1)
