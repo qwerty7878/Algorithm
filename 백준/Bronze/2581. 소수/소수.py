5212c313e7d3493fec473d92d4d99a69e60df6df
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-05 01:22:11 +0900

    [Bronze II] Title: 소수, Time: 2228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,24 +1,16 @@
-import math
-
 m = int(input())
 n = int(input())
+
 arr = []
-
 for i in range(m,n + 1):
-    if i < 2:
-        continue
-
-    isPrime = True
-    for j in range(2, int(math.sqrt(i)) + 1):
-        if i % j == 0:
-            isPrime = False
+    for j in range(2,n + 1):
+        if i != j and i % j == 0:
             break
+        elif i == j:
+            arr.append(i)
 
-    if isPrime:
-        arr.append(i)
-
-if len(arr) != 0:
+if (len(arr) == 0):
+    print(-1)
+else:
     print(sum(arr))
     print(min(arr))
-else:
-    print(-1)
