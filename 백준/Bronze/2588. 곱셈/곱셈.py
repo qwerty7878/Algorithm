74481384a4fa64acaa048867889a3a63a0381eb5
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-07 17:52:32 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,7 +1,6 @@
 a = int(input())
 b = int(input())
-
-print(a * (b % 10)) # 3
-print(a * ((b // 10) % 10)) # 4
-print(a * (b // 100)) # 5
+print(a * (b % 10))
+print(a * ((b // 10) % 10))
+print(a * (b // 100))
 print(a * b)
