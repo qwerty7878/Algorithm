53ae335c07582ff86b9f72abdfb86ef0d50f9745
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-07 22:29:44 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,11 +1,10 @@
 n,m = map(int,input().split())
 arr = [i for i in range(1,n+1)]
 
-for i in range(m):
-    a,b = map(int,input().split())
-
-    temp = arr[a-1]
-    arr[a-1] = arr[b-1]
-    arr[b-1] = temp
+for a in range(m):
+    i,j = map(int,input().split())
+    temp = arr[i - 1]
+    arr[i - 1] = arr[j - 1]
+    arr[j - 1] = temp
 
 print(*arr)
