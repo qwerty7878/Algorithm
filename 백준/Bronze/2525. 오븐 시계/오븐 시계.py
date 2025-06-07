d0b9f45f1551ef6c095333cf7bfb43ae056531ac
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-07 19:08:37 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,11 +1,4 @@
-a,b = map(int,input().split())
+h,m = map(int,input().split())
 c = int(input())
 
-time = b + c
-
-if time < 60:
-    print(a, time)
-if time >= 60 and (a + (time // 60)) < 24:
-    print(a + (time // 60), time % 60)
-if time >= 60 and (a + (time // 60)) >= 24:
-    print((a + (time // 60)) % 24, time % 60)
+print((h + ((m + c) // 60)) % 24,(m + c) % 60)
