32fa3b2c813dc6208d1443abd97f0a439ef17a3f
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-07 23:01:28 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,8 +1,9 @@
 n,m = map(int,input().split())
-arr = [i for i in range(1,n+1)]
+basket = [i for i in range(1,n+1)]
 
-for i in range(m):
-    a,b = map(int,input().split())
-    arr[a-1:b] = reversed(arr[a-1:b])
+for _ in range(m):
+    i,j = map(int,input().split())
+    basket[i - 1:j] = basket[i - 1:j][::-1]
+#     basket[i - 1:j:-1] 로 하면 빈 배열이 돌아감!
 
-print(*arr)
+print(*basket)
