d98503147c52484882dbf76a83d8fae56528b4b5
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-09 19:25:39 +0900

    [Bronze I] Title: 단어 공부, Time: 112 ms, Memory: 36488 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,22 +1,10 @@
-# 대소문자 구분 x
+from collections import Counter
+
 str = input().upper().strip()
+c = Counter(str)
+mc = c.most_common(2)
 
-# 단어 중복 검열 M I S P / Z A
-list = list(set(str))
-
-# 개수 저장 배열
-cnt = []
-
-# 각 개수 확인용 반복
-for i in list:
-    # 문자열에서의 개수 확인
-    count = str.count(i)
-    # 개수 저장
-    cnt.append(count)
-
-# 만약 가장 많이 사용된 알파벳이 여러개라면
-if cnt.count(max(cnt)) >= 2:
+if len(mc) > 1 and mc[0][1] == mc[1][1]:
     print('?')
 else:
-# 가장 많이 사용 되는 값
-    print(list[cnt.index(max(cnt))])
+    print(mc[0][0])
