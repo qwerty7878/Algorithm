1d4b7158ed8d2cb6fc44134bcc26aee65272f3a0
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-09 19:34:49 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,31 +1,24 @@
-# 문자열 입력
-str = input().strip()
-changeStr = str
+str = input()
+# if 'c=' in str:
+#     str = str.replace('c=','@')
+# if 'c-' in str:
+#     str = str.replace('c-','@')
+# if 'dz=' in str:
+#     str = str.replace('dz=','@')
+# if 'd-' in str:
+#     str = str.replace('d-','@')
+# if 'lj' in str:
+#     str = str.replace('lj','@')
+# if 'nj' in str:
+#     str = str.replace('nj','@')
+# if 's=' in str:
+#     str = str.replace('s=','@')
+# if 'z=' in str:
+#     str = str.replace('z=','@')
+# print(len(str))
 
-# 크로아티아 알파벳 변경
-if 'c=' in str:
-    changeStr = changeStr.replace('c=', '*')
+alpha = ['z=', 's=', 'nj', 'lj', 'd-', 'dz=', 'c-', 'c=']
+for i in alpha[::-1]:
+    str = str.replace(i, '@')
 
-if 'c-' in str:
-    changeStr = changeStr.replace('c-', '*')
-
-if 'dz=' in str:
-    changeStr = changeStr.replace('dz=', '*')
-
-if 'd-' in str:
-    changeStr = changeStr.replace('d-', '*')
-
-if 'lj' in str:
-    changeStr = changeStr.replace('lj', '*')
-
-if 'nj' in str:
-    changeStr = changeStr.replace('nj', '*')
-
-if 's=' in str:
-    changeStr = changeStr.replace('s=', '*')
-
-if 'z=' in str:
-    changeStr = changeStr.replace('z=', '*')
-
-# 문자열 개수 출력
-print(len(changeStr))
+print(len(str))
