53a22d4abadfc4c196638596af64b6070a22e387
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-09 21:55:38 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,44 +1,37 @@
-# 점수 합 초기값
-totalsum = 0
-scoresum = 0
-
-#   20개 반복
+sum = 0
+denominator = 0
 for i in range(20):
-
-    #   과목명, 학점, 등급
-    name,score,grade = input().split()
-
-    #   학점은 숫자로
+    name,score,grade = map(str,input().split())
     score = float(score)
 
-    #   전공평점은 전공과목별 (학점 × 과목평점)의 합
     if grade == 'A+':
-        totalsum += (score * 4.5)
-    if grade == 'A0':
-        totalsum += (score * 4.0)
-    if grade == 'B+':
-        totalsum += (score * 3.5)
-    if grade == 'B0':
-        totalsum += (score * 3.0)
-    if grade == 'C+':
-        totalsum += (score * 2.5)
-    if grade == 'C0':
-        totalsum += (score * 2.0)
-    if grade == 'D+':
-        totalsum += (score * 1.5)
-    if grade == 'D0':
-        totalsum += (score * 1.0)
-    if grade == 'F':
-        totalsum += (score * 0.0)
-    if grade == 'P':
-        pass
-
-    #   p는 계산에서 제외
-    if grade == 'P':
+        sum += (score * 4.5)
+        denominator += score
+    elif grade == 'A0':
+        sum += (score * 4.0)
+        denominator += score
+    elif grade == 'B+':
+        sum += (score * 3.5)
+        denominator += score
+    elif grade == 'B0':
+        sum += (score * 3.0)
+        denominator += score
+    elif grade == 'C+':
+        sum += (score * 2.5)
+        denominator += score
+    elif grade == 'C0':
+        sum += (score * 2.0)
+        denominator += score
+    elif grade == 'D+':
+        sum += (score * 1.5)
+        denominator += score
+    elif grade == 'D0':
+        sum += (score * 1.0)
+        denominator += score
+    elif grade == 'P':
         continue
+    elif grade == 'F':
+        sum += (score * 0.0)
+        denominator += score
 
-    #   학점의 총합
-    scoresum += score
-
-#   6자리까지 출력
-print(f'{totalsum / scoresum:0.6f}')
+print(f'{sum / denominator:.6f}')
