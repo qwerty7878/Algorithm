669ce573c175bec7dcacdc8cdb66fff65e7e0000
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-10 20:14:35 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,28 +1,14 @@
-#   배열 초기 선언
 arr = []
-
-#   9개 반복 리스트 전체를 한줄에 저장
+max = 0
 for i in range(9):
-    arr.append(list(map(int,input().split())))
-
-#   최대값 선언
-max = arr[0][0]
-
-#   초기 행 렬 초기 선언
-row = 1
-col = 1
-
-#   최대값 찾기
-for i in range(9):
-    for j in range(9):
-
-        #   더 크면 최대값 변경
-        if max < arr[i][j]:
-            max = arr[i][j]
-
-            #   인덱스는 0 부터 시작하니까
-            row = i + 1
-            col = j + 1
+    arr.append(list(map(int, input().split())))
+    for j in arr[i]:
+        if max <= j:
+            max = j
 
 print(max)
-print(row,col)
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] == max:
+            print(i + 1, j + 1)
+            break
