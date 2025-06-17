e49efa6be7e62b45790c1349b72a4441b912e5de
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-17 20:18:50 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.py
+++ 프로그래머스/1/70128. 내적/내적.py
@@ -1,5 +1,8 @@
 def solution(a, b):
-    answer = 0
+    answer = 1234567890
+    
+    sum = 0
     for i in range(len(a)):
-        answer += (a[i] * b[i])
+        sum += (a[i] * b[i])
+    answer = sum
     return answer
