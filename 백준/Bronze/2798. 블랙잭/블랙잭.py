5b0878212db1a3a2ba1a1cebf1537d2ca15b174f
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-18 21:28:09 +0900

    [Bronze II] Title: 블랙잭, Time: 716 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,13 +1,14 @@
 n,m = map(int,input().split())
 arr = list(map(int,input().split()))
-
 sum = 0
-sumArr = []
-for i in range(0, n):
-    for j in range(i + 1, n):
-        for k in range(j + 1, n):
-            sum = arr[i] + arr[j] + arr[k]
-            if sum <= m:
-                sumArr.append(sum)
-
-print(max(sumArr))
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if arr[i] == arr[j] or arr[i] == arr[k] or arr[j] == arr[k]:
+                continue
+            else:
+                if arr[i] + arr[j] + arr[k] <= m and sum < arr[i] + arr[j] + arr[k]:
+                   sum = arr[i] + arr[j] + arr[k]
+                elif arr[i] + arr[j] + arr[k] > m:
+                    continue
+print(sum)
