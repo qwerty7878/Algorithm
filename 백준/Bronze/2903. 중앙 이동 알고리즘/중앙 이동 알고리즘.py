1fd0104e6e44a1efa17dca7c604aff81cfbfaab9
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-20 00:37:04 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.py
@@ -1,13 +1,12 @@
 import math
 
 n = int(input())
-arr = [0] * 17
+
+arr = [0] * (n + 1)
 
 arr[0] = 4
-arr[1] = 9
-arr[2] = 25
 
-for i in range(3, n + 1):
-    arr[i] = ((math.sqrt(arr[i - 1]) * 2) - 1) ** 2
+for i in range(1, n + 1):
+	arr[i] = ((math.sqrt(arr[i - 1]) * 2) - 1) ** 2
 
 print(int(arr[n]))
