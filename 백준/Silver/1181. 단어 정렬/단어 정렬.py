be198d632fa26f50fcb91f08c0d12b5bfa5e25d6
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-21 22:48:30 +0900

    [Silver V] Title: 단어 정렬, Time: 544 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,12 +1,14 @@
-t = int(input())
+import sys
+
+t = int(sys.stdin.readline())
 arr = []
 
 for i in range(t):
-    arr.append(input())
+	arr.append(input())
 
 arr = list(set(arr))
 arr.sort()
 arr.sort(key=len)
 
 for i in arr:
-    print(i)
+	print(i)
