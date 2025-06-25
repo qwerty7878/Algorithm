cf8c3c54f5dea1fe45ad650046c54fe00bd46ac8
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-25 20:37:14 +0900

    [Silver V] Title: 영화감독 숌, Time: 740 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,13 +1,13 @@
 n = int(input())
 
-num = 0
-cnt = 0
-
+count = 0
+num = 1
 while True:
-    num += 1
-    if '666' in str(num):
-        cnt += 1
-
-    if cnt == n:
+    if count == n:
         print(num)
         break
+        
+    num += 1
+    temp = str(num)
+    if '666' in temp:
+        count += 1
