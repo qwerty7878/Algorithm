18c046f946960318de9afb0af3c475a2a4aeefa2
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-26 22:41:05 +0900

    [Silver IV] Title: 문자열 집합, Time: 96 ms, Memory: 37672 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,30 +1,14 @@
-n,m = map(int,input().split())
+import sys
 
-arr1 = []
+n,m = (map(int,sys.stdin.readline().split()))
+
+arr = []
 for i in range(n):
-    arr1.append(input())
-
-arr2 = []
-for i in range(m):
-    arr2.append(input())
-
-arr1.sort()
-def binary_search(arr, target):
-    start = 0
-    end = len(arr) - 1
-    while start <= end:
-        mid = (start + end) // 2
-        if arr[mid] == target:
-            return True
-        elif arr[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return False
+    arr.append(sys.stdin.readline())
+arr = set(arr)
 
 count = 0
-for i in arr2:
-    if binary_search(arr1, i):
+for i in range(m):
+    if sys.stdin.readline() in arr:
         count += 1
-
 print(count)
