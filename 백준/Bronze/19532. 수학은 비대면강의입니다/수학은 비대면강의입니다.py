0fc7925b21956448e6cdde1d15dad7e572767263
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-06-30 21:47:43 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,45 +1,11 @@
-a,b,c,d,e,f = map(int,input().split())
+import sys
 
-ax = a * d
-bx = b * d
-cx = c * d
+a,b,c,d,e,f = map(int, sys.stdin.readline().split())
 
-dx = d * a
-ex = e * a
-fx = f * a
+# x
+x = ((e * c) - (b * f)) // ((a * e) - (b * d))
 
-x = 0
-y = 0
-if(ax > 0 and dx < 0) or (ax < 0 and dx > 0):
-    ax = ax + dx
-    bx = bx + ex
-    cx = cx + fx
-
-else:
-    ax = ax - dx
-    bx = bx - ex
-    cx = cx - fx
-
-y = cx // bx
-
-ay = a * e
-by = b * e
-cy = c * e
-
-dy = d * b
-ey = e * b
-fy = f * b
-
-if (by > 0 and ey < 0) or (by < 0 and ey > 0):
-    ay = ay + dy
-    by = by + ey
-    cy = cy + fy
-
-else:
-    ay = ay - dy
-    by = by - ey
-    cy = cy - fy
-
-x = cy // ay
+# y
+y = ((f * a) - (c * d)) // ((e * a) - (b * d))
 
 print(x,y)
