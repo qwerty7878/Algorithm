17f56f4c6b37813531ef816e2cddc204982a2ef7
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-07-08 23:56:58 +0900

    [Bronze I] Title: 진법 변환 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,9 +1,20 @@
-n,b = map(int,input().split())
-answer = ''
-arr = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+import sys
 
-while n > 0:
-    answer += arr[n % b]
-    n //= b
+n,b = map(int,sys.stdin.readline().split())
 
-print(answer[::-1])
+arr = []
+while True:
+      if n < b:
+            arr.append(n)
+            break
+
+      arr.append(n % b)
+      n = n // b
+
+ans = ''
+for i in reversed(arr):
+      if i < 10:
+            ans += str(i)
+      else:
+            ans += chr(ord('A') + i - 10)
+print(ans)
