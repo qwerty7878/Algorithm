25994fa8148b62e3f2bed61e27fe89a4967a01a3
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-07-09 22:09:07 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,30 +1,27 @@
-n,m = map(int,input().split())
+import sys
 
-arr = [list(input()) for i in range(n)]
-# 2차원 배열 문자열씩 저장
-min_cnt = float('inf')
-# 최대값 저장
-for i in range(n - 7):
-    for j in range(m - 7):
-        # 8 * 8 만 뽑으니까 해당 범위를 제외한 나머지에서 진행하는 횟수
-        cnt1 = 0
-        cnt2 = 0
+n,m = map(int,sys.stdin.readline().split())
+board = []
 
-        for x in range(i, i + 8):
-            for y in range(j, j + 8):
-                # 선택된 범위에서 부터 8 * 8 뽑기
-                if (x + y) % 2 == 0:
-                    # 퐁당퐁당 반복되므로 홀수 짝수마다 바뀌어야함
-                    if arr[x][y] != 'W':
-                        cnt1 += 1
-                    if arr[x][y] != 'B':
-                        cnt2 += 1
-                else:
-                    if arr[x][y] != 'B':
-                        cnt1 += 1
-                    if arr[x][y] != 'W':
-                        cnt2 += 1
+for _ in range(n):
+      board.append(list(sys.stdin.readline()))
 
-        min_cnt = min(min_cnt, cnt1 ,cnt2)
-
-print(min_cnt)
+ans = sys.maxsize
+for x in range(n - 7):
+      for y in range(m - 7):
+            bcnt = 0
+            wcnt = 0
+            for i in range(x,x + 8):
+                  for j in range(y,y + 8):
+                        if (i + j) % 2 == 0:
+                              if board[i][j] != 'B':
+                                    bcnt += 1
+                              else:
+                                    wcnt += 1
+                        else:
+                              if board[i][j] != 'W':
+                                    bcnt += 1
+                              else:
+                                    wcnt += 1
+            ans = min(ans, bcnt, wcnt)
+print(ans)
