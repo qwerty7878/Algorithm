457d5a06cea08d000a9b444f4c9b94e2f9945526
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-07-12 20:28:00 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+import sys
+
+def backtracking(start):
+      if len(arr) == m:
+            print(' '.join(map(str, arr)))
+            return
+
+      for i in range(start, n + 1):
+            arr.append(i)
+            backtracking(i)
+            arr.pop()
+
+n,m = map(int, sys.stdin.readline().split())
+arr = []
+backtracking(1)
