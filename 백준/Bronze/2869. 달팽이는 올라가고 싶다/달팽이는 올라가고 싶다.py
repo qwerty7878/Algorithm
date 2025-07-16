330ce8290eb535e73f3dbf450fbbe391eae17f7e
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-07-16 21:45:51 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 48 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,11 +1,9 @@
-a,b,v = map(int,input().split())
+import math
+import sys
 
+a,b,v = map(int,sys.stdin.readline().split())
 day = 1
-v = v - a # 이미 하루 올라간 상태
-
-day += v / (a - b) # 이후 갭 만큼 증가
-
-if day == int(day):
-    print(int(day))
-else:
-    print(int(day) + 1)
+diff = a - b
+v -= a
+day += math.ceil(v / diff)
+print(day)
