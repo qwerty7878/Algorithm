d4c99fec72108ae3687d68bf7f34e252df7dba7d
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-07-28 22:44:34 +0900

    [Bronze III] Title: 윷놀이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.py
+++ 백준/Bronze/2490. 윷놀이/윷놀이.py
@@ -1,13 +1,14 @@
 for i in range(3):
-    arr = list(map(int, input().split()))
-    sum(arr)
-    if sum(arr) == 0:
-        print("D")
-    elif sum(arr) == 1:
-        print("C")
-    elif sum(arr) == 2:
-        print("B")
-    elif sum(arr) == 3:
-        print("A")
-    elif sum(arr) == 4:
-        print("E")
+    yut = list(map(int, input().split()))
+    sum(yut)
+
+    if sum(yut) == 3:
+        print('A')
+    elif sum(yut) == 2:
+        print('B')
+    elif sum(yut) == 1:
+        print('C')
+    elif sum(yut) == 0:
+        print('D')
+    else:
+        print('E')
