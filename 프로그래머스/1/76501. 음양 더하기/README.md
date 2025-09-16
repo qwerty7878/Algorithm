027cbb5f4f409fcdf2db94ac06eb2daca8a0ed65
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-09-16 11:28:47 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/README.md
+++ 프로그래머스/1/76501. 음양 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 음양 더하기 - 76501 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76501) 
 
 ### 성능 요약
 
-메모리: 63.1 MB, 시간: 0.06 ms
+메모리: 9.17 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 05일 06:57:26
+2025년 09월 16일 11:28:45
 
 ### 문제 설명
 
--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,10 +1,8 @@
 def solution(absolutes, signs):
-    answer = 123456789
-    sum = 0
-    for i in range(len(absolutes)):
+    answer = 0
+    for i in range(len(signs)):
         if signs[i] == True:
-            sum += absolutes[i]
+            answer += absolutes[i]
         else:
-            sum -= absolutes[i]
-    answer = sum
+            answer -= absolutes[i]
     return answer
