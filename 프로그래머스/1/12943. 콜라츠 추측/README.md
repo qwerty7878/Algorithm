5d52662ba2ff52414259593c23452fb88acfc6ad
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-09-16 11:35:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
 
 ### 성능 요약
 
-메모리: 9.24 MB, 시간: 0.14 ms
+메모리: 9.35 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 17일 19:43:15
+2025년 09월 16일 11:35:25
 
 ### 문제 설명
 
--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,14 +1,15 @@
 def solution(num):
     answer = 0
-    for _ in range(500):
-        if num == 1:
-            break
+    while num != 1:
         if num % 2 == 0:
             num //= 2
             answer += 1
-        elif num % 2 != 0:
-            num = (num * 3) + 1
+        else:
+            num *= 3
+            num += 1
             answer += 1
-    if num != 1:
-        answer = -1
+            
+        if answer > 500:
+            answer = -1
+            break
     return answer
