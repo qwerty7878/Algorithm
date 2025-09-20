be9ba77c5ddb5bac543bf8e0f6c538605ca993f3
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-09-21 00:35:40 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.09 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,9 +1,8 @@
 def solution(n):
     answer = ''
-    
-    for i in range(n):
+    for i in range(1, n + 1):
         if i % 2 == 0:
-            answer += '수'
-        else:
             answer += '박'
+        else:
+            answer += '수'
     return answer
