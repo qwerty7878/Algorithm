4cef869d219ca73ff0df817b5affdf937012d090
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-09-25 14:18:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,14 +1,25 @@
-def solution(strr):
-    s = str(strr)
-    s = s.replace("zero",'0')
-    s = s.replace("one",'1')
-    s = s.replace("two",'2')
-    s = s.replace("three",'3')
-    s = s.replace("four",'4')
-    s = s.replace("five",'5')
-    s = s.replace("six",'6')
-    s = s.replace("seven",'7')
-    s = s.replace("eight",'8')
-    s = s.replace("nine",'9')
-
-    return int(s)
+def solution(s):
+    answer = 0
+    
+    if 'zero' in s:
+        s = s.replace('zero', '0')
+    if 'one' in s:
+        s = s.replace('one', '1')
+    if 'two' in s:
+        s = s.replace('two', '2')
+    if 'three' in s:
+        s = s.replace('three', '3')
+    if 'four' in s:
+        s = s.replace('four', '4')
+    if 'five' in s:
+        s = s.replace('five', '5')
+    if 'six' in s:
+        s = s.replace('six', '6')
+    if 'seven' in s:
+        s = s.replace('seven', '7')
+    if 'eight' in s:
+        s = s.replace('eight', '8')
+    if 'nine' in s:
+        s = s.replace('nine', '9')
+    answer = int(s)
+    return answer
