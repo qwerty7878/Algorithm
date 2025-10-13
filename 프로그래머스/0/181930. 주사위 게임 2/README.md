a3b62886b79ebf43ee52e2346abc1575de4c76ba
hyeonjun <142138946+qwerty7878@users.noreply.github.com>
2025-10-14 03:15:17 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/README.md
+++ 프로그래머스/0/181930. 주사위 게임 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 2 - 181930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930) 
 
 ### 성능 요약
 
-메모리: 9.17 MB, 시간: 0.00 ms
+메모리: 9.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 11일 20:18:46
+2025년 10월 14일 03:15:14
 
 ### 문제 설명
 
--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,9 +1,9 @@
 def solution(a, b, c):
     answer = 0
     if a == b and b == c and c == a:
-        answer = (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+        answer = ((a ** 3) + (b ** 3) + (c ** 3)) * (a + b + c) * ((a ** 2) + (b ** 2) + (c ** 2))
     elif a != b and b != c and c != a:
-        answer = (a + b + c)
+        answer = a + b + c
     else:
-        answer = (a + b + c) * (a**2 + b**2 + c**2)
+        answer = (a + b + c) * ((a ** 2) + (b ** 2) + (c ** 2))
     return answer
