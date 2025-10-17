60b94d9552eafca0d6d6257af766f5e4eeaeead8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-10-17 13:29:41 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.py
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.py
@@ -1,6 +1,5 @@
 def solution(num_list):
-    num_list.sort()
     answer = []
-    for i in range(0, 5):
-        answer.append(num_list[i])
+    num_list.sort()
+    answer = num_list[:5]
     return answer
