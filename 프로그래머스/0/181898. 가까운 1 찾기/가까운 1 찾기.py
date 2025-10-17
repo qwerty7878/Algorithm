7e832dac31dad022170e44b377d8b536964a7c8f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-10-17 13:53:25 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.80 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -0,0 +1,9 @@
+def solution(arr, idx):
+    answer = -1
+    for i in range(len(arr)):
+        if i >= idx and arr[i] == 1:
+            answer = i
+            break
+        else:
+            continue
+    return answer
