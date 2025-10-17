d3e3900b87098153475be20833173ba2ea5940e9
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-10-17 13:35:57 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,9 +1,9 @@
-import math
-
 def solution(a, b):
+    answer = 0
     if a % 2 != 0 and b % 2 != 0:
-        return a ** 2 + b ** 2
+        answer = (a ** 2) + (b ** 2)
     elif a % 2 != 0 or b % 2 != 0:
-        return 2 * (a + b)
+        answer = 2 * (a + b)
     else:
-        return abs(a - b)
+        answer = abs(a - b)
+    return answer
