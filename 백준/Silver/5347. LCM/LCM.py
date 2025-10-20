6e4ded960e7d08e6c26f9c9bbf1ebdd8a4204fa7
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-10-20 23:19:59 +0900

    [Silver V] Title: LCM, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+def gcd(a,b):
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+def lcm(a,b):
+    return a * b // gcd(a,b)
+
+for _ in range(t):
+    a,b = map(int, input().split())
+    print(lcm(a,b))
