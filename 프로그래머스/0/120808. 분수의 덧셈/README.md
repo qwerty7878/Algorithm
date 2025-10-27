012611a504d82fa5d3c8117235588f789f2cf5bc
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-10-27 21:18:56 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=python3) 
 
 ### 성능 요약
 
-메모리: 63.2 MB, 시간: 0.02 ms
+메모리: 9.35 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 27일 21:15:07
+2025년 10월 27일 21:18:54
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,12 +1,14 @@
-import math
-
 def solution(numer1, denom1, numer2, denom2):
-    down = denom1 * denom2
-    up = (numer1 * denom2) + (numer2 * denom1)
     
-    if up % math.gcd(up,down) == 0:
-        newup = up // math.gcd(up,down)
-        newdown = down // math.gcd(up,down)
-        return [newup,newdown]
-    else:
-        return [up,down]
+    resultNumer = (numer1 * denom2) + (numer2 * denom1)
+    resultDenom = denom1 * denom2
+    
+    gcdNum = gcd(resultNumer, resultDenom)
+    
+    answer = [resultNumer//gcdNum, resultDenom//gcdNum]
+    return answer
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
