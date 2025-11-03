12bff27dc2a5872a39e095570c6d049f9b1b50d2
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-11-03 16:14:26 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.kt
+++ 프로그래머스/0/120898. 편지/편지.kt
@@ -1,7 +1,3 @@
 class Solution {
-    fun solution(message: String): Int {
-        var answer: Int = 0
-        answer = message.length * 2
-        return answer
-    }
+    fun solution(message: String) = message.length * 2
 }
