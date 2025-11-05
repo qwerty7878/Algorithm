75545da59dce8094daaeeb779d47899fb32427f8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-11-05 12:37:14 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.08 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/README.md
+++ 프로그래머스/0/120824. 짝수 홀수 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수 홀수 개수 - 120824 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120824?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120824) 
 
 ### 성능 요약
 
-메모리: 64.4 MB, 시간: 6.85 ms
+메모리: 62.6 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 04일 22:10:51
+2025년 11월 05일 12:37:12
 
 ### 문제 설명
 
--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
@@ -1,19 +1,7 @@
 class Solution {
     fun solution(num_list: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
-        var res = mutableListOf<Int>()
-        var even: Int = 0
-        var odd: Int = 0
-        for (i in num_list){
-            if (i % 2 != 0){
-                odd += 1
-            }else{
-                even += 1
-            }
-        }
-        res.add(even)
-        res.add(odd)
-        answer = res.toIntArray()
-        return answer
+        var even = num_list.filter { it % 2 == 0 }.size
+        var odd = num_list.filter { it % 2 != 0 }.size
+        return intArrayOf(even, odd)
     }
 }
