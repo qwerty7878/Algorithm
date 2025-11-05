4277e751e5bb457c355535f846d51137df4812c9
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-11-05 14:05:01 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 20.42 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
 
 ### 성능 요약
 
-메모리: 63.8 MB, 시간: 19.15 ms
+메모리: 64.9 MB, 시간: 20.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 05일 10:46:05
+2025년 11월 05일 14:04:59
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.kt
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.kt
@@ -1,11 +1,12 @@
 class Solution {
     fun solution(numbers: IntArray): Int {
-        var answer: Int = 0
-        var newarr = numbers.sorted()
-        if ((newarr[0] * newarr[1]) > (newarr[numbers.size - 1] * newarr[numbers.size -2])){
-            answer = newarr[0] * newarr[1]
+        var numbers = numbers
+            .sorted()
+        var answer = 0
+        if (numbers.get(0) * numbers.get(1) > numbers.get(numbers.size - 1) * numbers.get(numbers.size -2)){
+            answer = numbers.get(0) * numbers.get(1)
         }else{
-            answer = newarr[numbers.size - 1] * newarr[numbers.size - 2]
+            answer = numbers.get(numbers.size - 1) * numbers.get(numbers.size - 2)
         }
         return answer
     }
