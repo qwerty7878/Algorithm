1004ca4fe329793736e20601f064d8ec3f363fe7
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 18:57:35 +0900

    [Bronze V] Title: A+B - 4, Time: 192 ms, Memory: 17916 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextInt()){
+
+        while (sc.hasNext()) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            System.out.println(a+b);
+            System.out.println(a + b);
         }
     }
 }
