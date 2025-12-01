c71e9bd1c07024c253c5544b18d8972c9f65daba
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 18:18:31 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 176 ms, Memory: 17784 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,14 +1,9 @@
-//첫 번째 줄에는 문제의 정수 $N$이 주어진다. $(4\le N\le 1\, 000$; $N$은 $4$의 배수$)$ 
-//혜아가 $N$바이트 정수까지 저장할 수 있다고 생각하는 정수 자료형의 이름을 출력하여라.
+import java.util.*;
 
-import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 1; i <= n/4; i++){
-            System.out.print("long ");
-        }
-        System.out.print("int");
+        System.out.println("long ".repeat(n / 4) + "int");
     }
 }
