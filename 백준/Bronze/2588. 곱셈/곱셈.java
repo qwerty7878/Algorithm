e0b409f892c5bdf4f6af163ad776289a87bfa6e3
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 16:40:42 +0900

    [Bronze III] Title: 곱셈, Time: 96 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,13 +1,21 @@
-//첫째 줄부터 넷째 줄까지 차례대로 (3), (4), (5), (6)에 들어갈 값을 출력한다.
-import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        System.out.println(a*((b%100)%10));
-        System.out.println(a*((b%100)/10));
-        System.out.println(a*(b/100));
-        System.out.println(a*b);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        int hun = b / 100;
+        int ten = (b % 100) / 10;
+        int one = (b % 100) % 10;
+
+        System.out.println(a * one);
+        System.out.println(a * ten);
+        System.out.println(a * hun);
+        System.out.println(a * b);
     }
 }
