8e27e3c31c9c76f6a190c66931d1bcff685f32a5
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 16:25:43 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
+
+public class Main {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+        String name = sc.nextLine();
+
         System.out.println(name + "??!");
     }
 }
