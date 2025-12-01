85b72e563edcab829317ae17a4540fe2e07e372f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 22:40:31 +0900

    [Bronze II] Title: 공 바꾸기, Time: 196 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,23 +1,27 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 바구니
-        int M = sc.nextInt();
-        int temp; //잠시 넣어두었다가 빠질 공간
-        int arr[] = new int[N];
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = i + 1; // 배열은 0부터시작이고 첫번째 바구니부터 들어가니까
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
         }
-        for(int j = 0; j < M; j++){
-            int I = sc.nextInt();
-            int J = sc.nextInt();
-            temp = arr[I-1];
-            arr[I-1] = arr[J-1];
-            arr[J-1] = temp;
+
+        for (int a = 0; a < m; a++) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+
+            int temp = arr[i - 1];
+            arr[i - 1] = arr[j - 1];
+            arr[j - 1] = temp;
         }
-        for(int k = 0; k < arr.length; k++){
-            System.out.print(arr[k] + " ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
