c267b525f6bb10d8ceadd2f7c2a882f2806fd7ed
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 17:11:47 +0900

    [Bronze III] Title: 오븐 시계, Time: 172 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,14 +1,16 @@
-//첫째 줄에는 현재 시각이 나온다. 현재 시각은 시 A (0 ≤ A ≤ 23) 와 분 B (0 ≤ B ≤ 59)가 정수로 빈칸을 사이에 두고 순서대로 주어진다. 두 번째 줄에는 요리하는 데 필요한 시간 C (0 ≤ C ≤ 1,000)가 분 단위로 주어진다. 
-//첫째 줄에 종료되는 시각의 시와 분을 공백을 사이에 두고 출력한다. (단, 시는 0부터 23까지의 정수, 분은 0부터 59까지의 정수이다. 디지털 시계는 23시 59분에서 1분이 지나면 0시 0분이 된다.)
 import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        int M = (C + B + (A*60))%60;
-        int H = (C + B + (A*60))/60%24;
-        System.out.println(H + " " + M);
+        int h = sc.nextInt();
+        int m =  sc.nextInt();
+        int c =  sc.nextInt();
+
+        int min_sum = m + c;
+        m = min_sum % 60;
+        h =  (h + (min_sum / 60)) % 24;
+
+        System.out.printf("%d %d",h,m);
     }
 }
