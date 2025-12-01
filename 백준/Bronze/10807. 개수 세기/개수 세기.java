08d3ab61216ca779573226e2a64afc7b5ad882c6
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 19:05:49 +0900

    [Bronze V] Title: 개수 세기, Time: 184 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,25 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        int arr[] = new int [n];
+        List<Integer> list = new ArrayList<>();
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+             int num = sc.nextInt();
+             list.add(num);
         }
-        
-        int cnt = 0;
         int v = sc.nextInt();
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == v)
-                cnt++;
-        }
-
-        System.out.println(cnt);
-        
-        sc.close();
+        int count = (int) list.stream().filter(a -> a == v).count();
+        System.out.println(count);
     }
 }
