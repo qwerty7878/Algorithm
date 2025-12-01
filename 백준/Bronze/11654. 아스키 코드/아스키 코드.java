a10d7c946c1fe17d53c9ae1c973fed147d9a4a3a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-02 00:56:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17588 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -1,8 +1,10 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.next().charAt(0);
-        System.out.println(a);
+        char c = sc.next().charAt(0);
+        int num = (int) c;
+        System.out.println(num);
     }
 }
