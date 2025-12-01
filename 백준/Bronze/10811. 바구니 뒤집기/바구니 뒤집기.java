d0897f67e5fe84a133de004169ca74b65192a0d9
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-02 00:33:50 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 192 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -3,32 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int temp;
         int n = sc.nextInt();
         int m = sc.nextInt();
-        int arr[] = new int[n + 1];
 
-        for(int i = 1; i <= n; i++){
-            arr[i] = i;
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
         }
 
-        for(int a = 1; a <= m; a++){
+        for (int x = 0; x < m; x++) {
             int i = sc.nextInt();
             int j = sc.nextInt();
-            for(int b = i; b < j; b++){
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                j--;
-                i++;
-            }
+            swapArray(arr, i, j);
         }
 
-        for(int i = 1; i <= n; i++){
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
 
-        sc.close();
+    }
+
+    private static void swapArray(int[] arr, int i, int j) {
+        while (i < j) {
+            int temp = arr[i-1];
+            arr[i-1] = arr[j-1];
+            arr[j-1] = temp;
+            i++;
+            j--;
+        }
     }
 }
