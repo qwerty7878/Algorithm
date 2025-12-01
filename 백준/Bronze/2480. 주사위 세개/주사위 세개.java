46c3bae80a46fb370d75c65c83d315a7ab80e9ff
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 17:46:13 +0900

    [Bronze IV] Title: 주사위 세개, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,65 +1,22 @@
-//import java.util.Scanner;
-// public class Main{
-//     public static void main(String[] args){
-//         Scanner sc = new Scanner(System.in);
-//         int a = sc.nextInt();
-//         int b = sc.nextInt();
-//         int c = sc.nextInt();
-//         if(a==b && a==c){
-//             System.out.println(10000+(a*1000));
-//         }
-//         else if(a==b && a != c){
-//             System.out.println(1000+(a*100));
-//         }
-//         else if(a==c && a != b){
-//             System.out.println(1000+(a*100));
-//         }
-//         else if(c==b && c != a){
-//             System.out.println(1000+(b*100));
-//         }
-//         else if(a != b && a != c){
-//             if(a>b){
-//                 if(a>c)
-//                     System.out.println(a*100);
-//                 else
-//                     System.out.println(c*100);
-//             }
-//             else if(b>c){
-//                 if(b>a)
-//                     System.out.println(b*100);
-//                 else
-//                     System.out.println(a*100);
-//             }
-//             else if(c>a){
-//                 if(c>b)
-//                     System.out.println(c*100);
-//                 else
-//                     System.out.println(b*100);
-//             }
-//         }
-//     }
-// }
-
-
 import java.util.*;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		
-		if(a == b && a == c && b ==c)
-			System.out.println(10000 + a * 1000);
-		else if(a == b && a != c)
-			System.out.println(1000 + a * 100);
-		else if(a==c && a != b)
-			System.out.println(1000 + a * 100);
-		else if(b==c && b != a)
-			System.out.println(1000 + b * 100);
-		else if(a != b && a != c && b != c)
-			System.out.println(Math.max(Math.max(a, b),c) * 100);
-	}
-}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        List<Integer> dice = new ArrayList<>();
+        dice.add(sc.nextInt());
+        dice.add(sc.nextInt());
+        dice.add(sc.nextInt());
+        Collections.sort(dice);
+
+        Set<Integer> setDice = new HashSet<>(dice);
+        if (setDice.size() == 1) {
+            System.out.println(10000 + (dice.get(0) * 1000));
+        } else if (setDice.size() == 2) {
+            System.out.println(1000 + (dice.get(1) * 100));
+        } else {
+            System.out.println(100 * dice.get(2));
+        }
+    }
+}
