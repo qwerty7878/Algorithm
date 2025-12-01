537a88911004bac98bd9703c2e6adbed9605d834
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 22:47:18 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 176 ms, Memory: 17820 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,16 +1,16 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int student[] = new int[31];
-        for(int i = 1; i <= 28; i++){
-            int good = sc.nextInt();
-            student[good] = 1;
-        }
-        for(int i = 1; i < student.length; i++){
-            if(student[i] != 1)
-                System.out.println(i);
-        }
-        
+        List<Integer> list = new LinkedList<>();
+
+        IntStream.range(0, 28).forEach(i -> list.add(sc.nextInt()));
+        IntStream.rangeClosed(1, 30)
+                .filter(i -> !list.contains(i))
+                .forEach(System.out::println);
     }
 }
