a4775f1f97932276cc9602e2a67420753360aa9a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 16:49:10 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 14016 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,11 +1,5 @@
-//|\_/|
-//|q p|   /}
-//( 0 )"""\
-//|"^"`    |
-//||_/=\\__|
-
-public class Main{
-    public static void main(String args[]){
+public class Main {
+    public static void main(String[] args) {
         System.out.println("|\\_/|");
         System.out.println("|q p|   /}");
         System.out.println("( 0 )\"\"\"\\");
