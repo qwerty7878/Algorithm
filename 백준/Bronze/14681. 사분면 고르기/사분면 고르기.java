a52551e32f03d992d78087273d98a2b20eb89267
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 16:57:41 +0900

    [Bronze V] Title: 사분면 고르기, Time: 168 ms, Memory: 17604 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
-        Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-        int y = scan.nextInt();
-        if(x>0 && y>0)
-            System.out.println("1");
-        else if(x<0 && y>0)
-            System.out.println("2");
-        else if(x>0 && y<0)
-            System.out.println("4");
-        else if(x<0 && y<0)
-            System.out.println("3");
-        
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        }else if (x > 0 && y < 0) {
+            System.out.println(4);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else {
+            System.out.println(2);
+        }
     }
 }
