11324b8590fa10e910266d12c7e8f4104fa5a4b9
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 17:05:17 +0900

    [Bronze III] Title: 알람 시계, Time: 192 ms, Memory: 18552 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,27 +1,23 @@
-//첫째 줄에 두 정수 H와 M이 주어진다. (0 ≤ H ≤ 23, 0 ≤ M ≤ 59) 그리고 이것은 현재 상근이가 설정한 알람 시간 H시 M분을 의미한다.
-//입력 시간은 24시간 표현을 사용한다. 24시간 표현에서 하루의 시작은 0:0(자정)이고, 끝은 23:59(다음날 자정 1분 전)이다. 시간을 나타낼 때, 불필요한 0은 사용하지 않는다.
-//첫째 줄에 상근이가 창영이의 방법을 사용할 때, 설정해야 하는 알람 시간을 출력한다. (입력과 같은 형태로 출력하면 된다.)
 import java.util.Scanner;
-public class Main{
-    public static void main(String args[]){
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int H = sc.nextInt();
-        int M = sc.nextInt();
-        if(M < 45){
-            if(H == 0){
-                H = 23;
-                M = M +15;
-                System.out.println(H + " " + M);
+        int h = sc.nextInt();
+        int m =  sc.nextInt();
+
+        if (m - 45 < 0) {
+            if (h == 0) {
+                h = 23;
+                m += 15;
+            } else {
+                h -= 1;
+                m += 15;
             }
-            else{
-                H = H - 1;
-                M = M + 15;
-                System.out.println(H + " " + M);
-            }
+        } else {
+            m -= 45;
         }
-        else{
-            M = M - 45;
-            System.out.println(H + " " + M);
-        }
+
+        System.out.println(h + " " + m);
     }
 }
