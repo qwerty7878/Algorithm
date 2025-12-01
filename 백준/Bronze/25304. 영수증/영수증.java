2ce55e418d3a48f53d0ffd674415636270fcf67b
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 17:54:19 +0900

    [Bronze IV] Title: 영수증, Time: 184 ms, Memory: 17824 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,23 +1,24 @@
-//첫째 줄에는 영수증에 적힌 총 금액 $X$가 주어진다.
-//둘째 줄에는 영수증에 적힌 구매한 물건의 종류의 수 $N$이 주어진다.
-//이후 $N$개의 줄에는 각 물건의 가격 $a$와 개수 $b$가 공백을 사이에 두고 주어진다.
-//구매한 물건의 가격과 개수로 계산한 총 금액이 영수증에 적힌 총 금액과 일치하면 Yes를 출력한다. 일치하지 않는다면 No를 출력한다.
+import java.util.*;
 
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();//총 금액
-        int n = sc.nextInt();//물건 종류
+        int totalPrice = sc.nextInt();
+        int t = sc.nextInt();
+
         int sum = 0;
-        for(int i = 1; i <= n; i++){
-            int a = sc.nextInt();//각 물건 가격
-            int b = sc.nextInt();//각 물건 개수
-            sum = (a*b) + sum;
+        for (int i = 0; i < t; i++) {
+            int price = sc.nextInt();
+            int count = sc.nextInt();
+
+            int total = price * count;
+            sum += total;
         }
-        if(sum == x)
+
+        if (sum == totalPrice) {
             System.out.println("Yes");
-        else
+        } else {
             System.out.println("No");
+        }
     }
 }
