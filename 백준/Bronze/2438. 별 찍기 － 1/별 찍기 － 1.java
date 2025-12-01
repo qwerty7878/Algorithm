7cd1044b531637fd5a7cc5b73ee054e80e025019
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 18:41:26 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j <= i; j++){
-                System.out.print("*");
-            }
-            System.out.print("\n");
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 1; i < t + 1; i++) {
+            System.out.println("*".repeat(i));
         }
     }
 }
