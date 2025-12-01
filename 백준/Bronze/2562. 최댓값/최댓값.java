8238a82170048f25ef8343fa26ccdb807c74d052
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-01 22:13:36 +0900

    [Bronze III] Title: 최댓값, Time: 172 ms, Memory: 17816 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,20 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-			int arr[] = new int [9];
-			int max	= 0;
-			int cnt = 0;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-			for(int i = 0; i < arr.length; i++){
-				arr[i] = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            int num = sc.nextInt();
+            list.add(num);
+        }
 
-				if(max < arr[i]){
-					max = arr[i];
-					cnt = i + 1; // 배열은 0부터니까
-				}
-			}
-			System.out.println(max);
-			System.out.println(cnt);
-			
-		}
-	}
+        int max = list.stream().max(Integer::compareTo).get();
+        int index = list.indexOf(max);
+        System.out.printf("%d\n",max);
+        System.out.printf("%d\n",index + 1);
+    }
+}
