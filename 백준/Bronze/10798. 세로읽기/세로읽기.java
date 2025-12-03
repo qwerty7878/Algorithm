03b7b2c832dc9f8b188fb56dbd00acc9d30b1e40
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-04 04:06:05 +0900

    [Bronze I] Title: 세로읽기, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,21 +1,22 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);       
-        char arr[][] = new char [5][15];
-        
-        for(int i = 0; i < 5; i++){
-            String str = sc.next();
-            for(int j = 0; j < str.length(); j++){
-                arr[i][j] = str.charAt(j);
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char arr[][] = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String s = sc.nextLine();
+            for (int j = 0; j < s.length(); j++) {
+                arr[i][j] = s.charAt(j);
             }
         }
-        for(int i = 0; i < 15; i++){
-            for(int j = 0; j < 5; j++){
-                if(arr[j][i] == ' ' || arr[j][i] == '\0')
-                    continue;
-                
-                System.out.print(arr[j][i]);
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] != '\u0000') {
+                    System.out.print(arr[j][i]);
+                }
             }
         }
     }
