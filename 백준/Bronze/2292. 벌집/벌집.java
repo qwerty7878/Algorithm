f4891d6afc2687639ec5af68b555c4de0d058620
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-04 23:17:33 +0900

    [Bronze II] Title: 벌집, Time: 180 ms, Memory: 17696 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,19 +1,18 @@
-import java.util.*;
-public class Main{
-   public static void main(String args[]){
-   Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       int range = 2;
-       int count = 1;
-       
-       if(n == 1)
-           System.out.println(1);
-       else{
-           while(range <= n){
-               range = range + (count * 6);
-               count++;
-           }
-           System.out.println(count);
-       }
-   }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int roomCount = 1;
+        int room = 1;
+        while (true) {
+            if (n <= room) {
+                break;
+            }
+            room += 6 * roomCount;
+            roomCount++;
+        }
+        System.out.println(roomCount);
+    }
 }
