48a36648a4731eeb7fdb27572b40c32ae7c9bff6
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-05 00:05:40 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        long a = Long.parseLong(st.nextToken());
-        long b = Long.parseLong(st.nextToken());
-        long v = Long.parseLong(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken()) - a;
 
-        double up = a - b;
-        double distace = v - a;
-
-        int day = 1;
-        day += (int)Math.ceil(distace / up);
-        System.out.println(day);
+        int day = 1 + (v / (a - b));
+        if (v % (a - b) == 0) {
+            System.out.println(day);
+        } else {
+            System.out.println(day + 1);
+        }
     }
 }
