5a63bedb8c456514b25c714c6dcce7d61653d818
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-06 02:37:07 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -1,23 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         while (true) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-
-            int sum = a + b + c;
-
-            if(a == 0 && b == 0 && c == 0)  break;
-
-            if(Math.max(Math.max(a,b),c) < (sum - Math.max(Math.max(a,b),c))) {
-                if (a == b && a == c && b == c) System.out.println("Equilateral");
-                else if (a == b || a == c || b == c) System.out.println("Isosceles");
-                else if (a != b && a != c && c != b) System.out.println("Scalene");
+            List<Integer> angles = new ArrayList<>();
+            for (int i = 0; i < 3; i++) {
+                int angle = sc.nextInt();
+                angles.add(angle);
             }
-            else System.out.println("Invalid");
+
+            if (angles.get(0) == 0 && angles.get(1) == 0 && angles.get(2) == 0) {
+                break;
+            }
+
+            int max = angles.stream()
+                    .mapToInt(Integer::intValue)
+                    .max()
+                    .getAsInt();
+
+            int sum = angles.stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
+
+            int restTwoLine = sum - max;
+            Set<Integer> set = new HashSet<>(angles);
+
+            if (restTwoLine > max) {
+                if (set.size() == 1) {
+                    System.out.println("Equilateral");
+                } else if (set.size() == 2) {
+                    System.out.println("Isosceles");
+                } else if (set.size() == 3) {
+                    System.out.println("Scalene");
+                }
+            } else {
+                System.out.println("Invalid");
+            }
         }
     }
 }
