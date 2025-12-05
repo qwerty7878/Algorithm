e9dc83126f8220bc8e0f8ed88ca402da7f0a0381
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-06 02:16:08 +0900

    [Bronze III] Title: 대지, Time: 828 ms, Memory: 109484 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -4,22 +4,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arrx[] = new int[n];
-        int arry[] = new int[n];
+        int t = sc.nextInt();
 
-        for(int i = 0; i < n; i++){
-            arrx[i] = sc.nextInt();
-            arry[i] = sc.nextInt();
+        int xarr[] = new int[t];
+        int yarr[] = new int[t];
+        for (int i = 0; i < t; i++) {
+            xarr[i] = sc.nextInt();
+            yarr[i] = sc.nextInt();
         }
-        Arrays.sort(arrx);
-        Arrays.sort(arry);
+        Arrays.sort(xarr);
+        Arrays.sort(yarr);
+        
+        int xsize = xarr[xarr.length - 1] - xarr[0];
+        int ysize = yarr[yarr.length - 1] - yarr[0];
 
-        int maxx = arrx[n -1];
-        int maxy = arry[n -1];
-        int minx = arrx[0];
-        int miny = arry[0];
-
-        System.out.println((maxx - minx) * (maxy - miny));
+        System.out.println(xsize * ysize);
     }
 }
