c84f516fa19504c6bdce81b164ed2e20c3737a9a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-05 16:49:57 +0900

    [Bronze I] Title: 약수들의 합, Time: 216 ms, Memory: 18480 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,39 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        while (true){
-            StringBuilder sb = new StringBuilder();
-            int n = Integer.parseInt(br.readLine());
-            int sum = 0;
-            ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int n = sc.nextInt();
 
-            if(n == -1) break;
-
-            for(int i = 1; i < n; i++){
-                if(n % i == 0){
+            if (n == -1) {
+                break;
+            }
+            List<Integer> list = new ArrayList<>();
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
                     list.add(i);
-                    sum += i;
                 }
             }
 
-            if(sum == n){
-                sb.append(n).append(" = ");
-                for(int i = 0; i < list.size(); i++){
-                    if(i != list.size() -1)
-                        sb.append(list.get(i)).append(" + ");
-                    else
-                        sb.append(list.get(i));
-                }
-            } else
-                sb.append(n).append(" is NOT perfect.");
+            int sum = list.stream()
+                    .mapToInt(Integer::intValue)
+                    .sum();
 
-            System.out.println(sb);
+            if (sum != n) {
+                System.out.printf("%d is NOT perfect.\n", n);
+            } else {
+                System.out.print(n + " = ");
+                for (int i = 0; i < list.size(); i++) {
+                    if (list.get(i) == list.get(list.size() - 1)) {
+                        System.out.println(list.get(i));
+                    } else {
+                        System.out.print(list.get(i) + " + ");
+                    }
+                }
+            }
         }
     }
 }
