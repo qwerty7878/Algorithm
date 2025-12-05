f711aeef04a574805aecac395357defdbd27c5fb
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-06 00:35:41 +0900

    [Bronze I] Title: 소인수분해, Time: 212 ms, Memory: 18196 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,21 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
-        int i = 2;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
 
-        while (n != 0){
-            if(n % i == 0){
-                System.out.println(i);
-                n /= i;
+        int div = 2;
+        while (n >= div) {
+            if (n % div == 0) {
+                list.add(div);
+                n /= div;
+            } else {
+                div += 1;
             }
-            else if(n == 1) break;
-            else i++;
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
     }
 }
