2766ec2c887fb895f6d4db682952f3cc91c4af1f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-06 00:39:05 +0900

    [Bronze V] Title: 직사각형, Time: 168 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/27323. 직사각형/직사각형.java
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -1,10 +1,12 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        System.out.println(A*B);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a * b);
     }
 }
