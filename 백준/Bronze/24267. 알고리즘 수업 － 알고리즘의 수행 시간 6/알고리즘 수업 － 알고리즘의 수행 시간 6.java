69078ee3902ac89ebd22bcc89f0d3324d450b4ac
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-06 03:02:55 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 164 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -1,12 +1,10 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long n = Long.parseLong(br.readLine());
-        
-        System.out.println((n-2)*(n-1)*n/6);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(n * (n - 1) * (n - 2) / 6);
         System.out.println(3);
-
     }
 }
