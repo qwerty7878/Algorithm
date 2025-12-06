a29ef2539cebd6caab77f5469c92f734e0f7b184
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-06 23:59:49 +0900

    [Bronze II] Title: 분해합, Time: 188 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,19 +1,21 @@
-import java.util.*;
-public class Main{
-    public static void main(String args[]){
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int result = 0;
-        
-        for(int i = 0; i < n; i++){
+
+        for (int i = 0; i < n; i++) {
+            int num = i;
             int sum = 0;
-            int number = i;
-            
-            while(number != 0){
-                sum += number%10;
-                number/= 10;
+
+            while (num != 0) {
+                sum += num % 10;
+                num /= 10;
             }
-            if(sum + i == n){
+
+            if (sum + i == n) {
                 result = i;
                 break;
             }
