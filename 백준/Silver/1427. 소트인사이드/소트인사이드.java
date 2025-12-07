be0a1985b360243fd2c2c931fe4ba82ad9c7ee4f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-08 08:31:37 +0900

    [Silver V] Title: 소트인사이드, Time: 92 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,23 +1,23 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        long n = Long.parseLong(br.readLine());
-        String s = "" + n;
-        String arr[] = s.split("");
-
+        String input = br.readLine();
+        char[] arr = input.toCharArray();
         Arrays.sort(arr);
-
-        for(int i = arr.length-1; i >= 0; i--){
-            sb.append(Long.parseLong(arr[i]));
+        for (int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
         }
-
         bw.write(sb.toString());
         bw.flush();
+        bw.close();
     }
 }
