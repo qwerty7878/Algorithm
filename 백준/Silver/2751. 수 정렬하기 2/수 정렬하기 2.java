f7aba1ed3f58e3f6a471bf8991bbdfbd13bdd824
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-07 22:53:35 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1456 ms, Memory: 177464 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,24 +1,27 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-
-        int arr[] = new int[n];
-
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            set.add(num);
         }
+
+        int arr[] = set.stream().mapToInt(Integer::intValue).toArray();
         Arrays.sort(arr);
-        for(int i = 0; i < arr.length; i++){
+
+        for (int i = 0; i < arr.length; i++) {
             sb.append(arr[i]).append("\n");
         }
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
