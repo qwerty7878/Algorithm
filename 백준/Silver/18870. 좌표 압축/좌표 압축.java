f9f53123c9bd8786d2041cd63662e193ae7bee04
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-08 14:54:43 +0900

    [Silver II] Title: 좌표 압축, Time: 1944 ms, Memory: 360744 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,41 +1,42 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-
         int arr[] = new int[n];
-        HashMap<Integer, Integer> hm = new HashMap<>();
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int rank[] = arr.clone();
-        //  값에 대한 순위 피악을 위헤 정렬이 필요함
-        //  정렬을 처음 배열에서 하면 결과값이 달라지기 때문에 복사된 배열을 하나 만듬
-        Arrays.sort(rank);
-
-        int index = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (!hm.containsKey(rank[i])) {   //  인덱스 중복방지
-                hm.put(rank[i], index); //  값, 순위
-                index++;
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(hm.get(arr[i])).append(" ");
+        int sorted[] = Arrays.stream(arr)
+                .distinct()
+                .sorted()
+                .toArray();
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < sorted.length; i++) {
+            map.put(sorted[i], i);
         }
 
-        System.out.println(sb.toString());
+        for(int i = 0; i < arr.length; i++){
+            sb.append(map.get(arr[i])).append(" ");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
