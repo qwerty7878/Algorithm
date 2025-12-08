e72c6038595f4be9b13a7212db8746408370fecb
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-08 16:23:54 +0900

    [Silver IV] Title: 문자열 집합, Time: 352 ms, Memory: 31060 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,28 +1,36 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =  new StringTokenizer(br.readLine()," ");
-
-        HashSet<String> hp = new HashSet<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int cnt = 0;
 
-        for(int i = 0; i < n; i++){
-            hp.add(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
         }
 
-        for(int i = 0; i < m; i++){
-            String s = br.readLine();
-            if(hp.contains(s))
-                cnt++;
+        int count = 0;
+        for (int j = 0; j < m; j++) {
+            if (set.contains(br.readLine())) {
+                count++;
+            }
         }
-        System.out.println(cnt);
-   }
+        sb.append(count);
+
+        bw.write(sb.toString().trim());
+        bw.flush();
+        bw.close();
+    }
 }
