4f04e06f435c11c892f10d056f59284976fa39df
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-09 12:20:30 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 484 ms, Memory: 54696 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,37 +1,43 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
-        HashMap<String, Integer> Smap = new HashMap<>();
-        HashMap<Integer, String> Nmap = new HashMap<>();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int m = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <= m; i++) {
-            String pockemon = br.readLine();
-            Smap.put(pockemon, i);
-            Nmap.put(i, pockemon);
-        }
+        Map<String, Integer> pocketmonToNumList = new HashMap<>();
+        Map<Integer, String> numToPocketnumList = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            String test = br.readLine();
-            if (Character.isDigit(test.charAt(0))) {
-                sb.append(Nmap.get(Integer.parseInt(test))).append("\n");
+            String name = br.readLine();
+            pocketmonToNumList.put(name, (i + 1));
+            numToPocketnumList.put(i + 1, name);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            String question = br.readLine();
+//            번호로 물어봣다면
+            if (Character.isDigit(question.charAt(0))) {
+                int num = Integer.parseInt(question);
+                sb.append(numToPocketnumList.get(num)).append("\n");
             } else {
-                sb.append(Smap.get(test)).append("\n");
+                sb.append(pocketmonToNumList.get(question)).append("\n");
             }
         }
-        System.out.println(sb);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
-
