e0d3c726dd29c0204025e3f50fb738d39b8c5e6e
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-10 00:54:13 +0900

    [Bronze I] Title: 최소공배수, Time: 128 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,20 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args) {;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = sc.nextInt();
+        int t = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            System.out.println(a*b / gcd(a,b));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a =  Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int gcd = findGcd(a, b);
+            int lcm = (a * b) / gcd;
+            sb.append(lcm).append("\n");
         }
-   }
 
-   public static int gcd(int n, int m){
-        if(m == 0)  return n;
-        return gcd(m, n%m);
-   }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static int findGcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return findGcd(b, a % b);
+        }
+    }
 }
