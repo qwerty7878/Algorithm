768ad13395a94851b615598a7169ea9493dc703f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-11 12:47:44 +0900

    [Silver II] Title: 베르트랑 공준, Time: 968 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,36 +1,46 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        StringBuilder sb = new StringBuilder();
         while (true) {
+            int count = 0;
             int n = Integer.parseInt(br.readLine());
-            if(n == 0)  break;
+            if (n == 0) break;
 
-            boolean isPrime[] = new boolean[(2*n) + 1];
-            Arrays.fill(isPrime,true);  //  일단 전체 true
-
-            isPrime[0] = isPrime[1] = false;    //  0 1 은 소수 아니니까 false
-
-            for (int i = 2; i <= (int) (Math.sqrt(2*n) + 1); i++) { //  소수 판별
-                if (isPrime[i]) {   // 현재 isPrime에 true인 애들 중
-                    for (int j = i * i; j <= 2*n; j += i) { // 소수가 아닌것들 false로 바꿔줌
-                        isPrime[j] = false;
-                    }
-                }   //  isPrime안에 true는 소수밖에없음
-            }
-            int cnt = 0;
-            for (int i = n + 1; i <= 2 * n; i++) {  //  n보다 크고 2n 보다 작거나 같은
-                if (isPrime[i]) {
-                    cnt++;
+            for (int i = n + 1; i <= 2 * n; i++) {
+                if (isPrime(i)) {
+                    count++;
                 }
             }
-            System.out.println(cnt);
+            sb.append(count).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        } else if (num == 2) {
+            return true;
+        } else if (num % 2 == 0) {
+            return false;
+        } else {
+            for (long i = 3; i <= Math.sqrt(num); i += 2) {
+                if(num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
