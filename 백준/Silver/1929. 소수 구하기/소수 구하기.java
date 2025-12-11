5d5c334ed390b7f15eb19cc04633ddc185093526
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-11 12:38:13 +0900

    [Silver III] Title: 소수 구하기, Time: 480 ms, Memory: 19288 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,37 +1,44 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-        boolean isPrime[] = new boolean[m + 1];
-        Arrays.fill(isPrime, true);
-
-        isPrime[0] = isPrime[1] = false;
-
-        for (int i = 2; i <= (int) (Math.sqrt(m) + 1); i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= m; j += i) {
-                    isPrime[j] = false;
-                }
-            }
-        }
-
-        for (int i = n; i <= m; i++) {
-            if (isPrime[i]) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i)) {
                 sb.append(i).append("\n");
             }
         }
-        System.out.println(sb);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static boolean isPrime(long num) {
+        if (num < 2) {
+            return false;
+        } else if (num == 2) {
+            return true;
+        } else if (num % 2 == 0) {
+            return false;
+        } else {
+            for (long i = 3; i <= Math.sqrt(num); i += 2) {
+                if(num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
