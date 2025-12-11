484e14f041e09191b25b985aa4c041de51a1746a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-11 15:41:35 +0900

    [Silver V] Title: 창문 닫기, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/13909. 창문 닫기/창문 닫기.java
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -1,19 +1,21 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
 
-        int cnt = 0;
-        for (int i = 1; i * i <= n; i++) {
-            cnt++;
+        int count = 1;
+        for(int i = 2; i * i <= n; i++){
+            count ++;
         }
-        sb.append(cnt).append("\n");
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append("\n");
 
         bw.write(sb.toString());
         bw.flush();
