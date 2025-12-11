9e3d893f29af023084ed68255927b90e1b8ec87d
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-11 12:30:32 +0900

    [Silver IV] Title: 다음 소수, Time: 252 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -1,29 +1,47 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
-
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < t; i++) {
             long num = Long.parseLong(br.readLine());
-            BigInteger prime = new BigInteger(String.valueOf(num));
-            //    BigInteger은 String 형태로 숫자를 처리하기 때문
-            //    isProbablePrime(10)은 소수인지 판별함 BigInteger과 같이 쓰임
-            if (prime.isProbablePrime(10)) {
-                sb.append(prime).append("\n");
-            } else {
-                sb.append(prime.nextProbablePrime()).append("\n");
-                //    nextProbablePrime()은 다음 소수를 나타냄
+
+            while (true) {
+                if (isPrime(num)) {
+                    break;
+                }
+                num++;
             }
+            sb.append(num).append("\n");
         }
-        System.out.println(sb);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static boolean isPrime(long num) {
+        if (num < 2) {
+            return false;
+        } else if (num == 2) {
+            return true;
+        } else if (num % 2 == 0) {
+            return false;
+        } else {
+            for (long i = 3; i <= Math.sqrt(num); i += 2) {
+                if(num % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
-
