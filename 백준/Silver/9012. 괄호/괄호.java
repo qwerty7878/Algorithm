bb4795ad0b64bf0a51bafdca2b721a6c98325e18
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-13 19:20:31 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,35 +1,41 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args)  throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-
         for (int i = 0; i < n; i++) {
-            String s = br.readLine();
             Stack<Character> stack = new Stack<>();
+            String input = br.readLine();
 
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == '(') {
-                    stack.push(s.charAt(j));
-                } else {
-                    if (stack.empty()) {
-                        stack.push(s.charAt(j));
-                        break;
-                    } else {
+            for (int j = 0; j < input.length(); j++) {
+                if (input.charAt(j) == '(') {
+                    stack.push(input.charAt(j));
+                } else if (input.charAt(j) == ')') {
+                    if (!stack.isEmpty() && stack.peek() == '(') {
                         stack.pop();
+                    } else {
+                        stack.push(input.charAt(j));
                     }
                 }
             }
-            if (stack.empty()) {
-                System.out.println("YES");
+
+            if (stack.isEmpty()) {
+                sb.append("YES").append('\n');
             } else {
-                System.out.println("NO");
+                sb.append("NO").append('\n');
             }
         }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
