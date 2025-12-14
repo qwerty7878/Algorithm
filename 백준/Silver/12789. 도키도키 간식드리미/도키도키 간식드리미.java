6f8bb6a8f47857bbe6f5341a6d4b077be43823f3
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-15 02:49:45 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 132 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -1,49 +1,55 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Stack<Integer> stack = new Stack<>();
-		Queue<Integer> queue = new LinkedList<>();
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int n = Integer.parseInt(br.readLine());
+        Queue<Integer> current_waiting_line = new LinkedList<>();
+        Stack<Integer> temp_waitting_line = new Stack<>();
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-		for(int i = 0; i < n; i++){
-			queue.add(Integer.parseInt(st.nextToken()));
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            current_waiting_line.add(Integer.parseInt(st.nextToken()));
+        }
 
-		int num = 1;
+        int current_waiting_num = 1;
+        while (!current_waiting_line.isEmpty()) {
+            int current_num = current_waiting_line.poll();
+            if (current_num == current_waiting_num) {
+                current_waiting_num++;
+            } else {
+                temp_waitting_line.push(current_num);
+            }
 
-		while(!queue.isEmpty()){
-			if(queue.peek() == num){
-				queue.poll();
-				num++;
-			}
-			else if(!stack.isEmpty() && stack.peek() == num){
-				stack.pop();
-				num++;
-			}
-			else
-				stack.push(queue.poll());
-		}
-		while(!stack.isEmpty()){
-			if(stack.peek() == num){
-				stack.pop();
-				num++;
-			}
-			else{
-				System.out.println("Sad");
-				return;
-			}
-		}
-		System.out.println("Nice");
-	}
+            while (!temp_waitting_line.isEmpty()) {
+                if (temp_waitting_line.peek() == current_waiting_num) {
+                    temp_waitting_line.pop();
+                    current_waiting_num++;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        if (current_waiting_num == n + 1) {
+            sb.append("Nice").append("\n");
+        } else {
+            sb.append("Sad").append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
