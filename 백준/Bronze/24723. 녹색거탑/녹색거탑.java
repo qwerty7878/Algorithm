a9acfecb39a9527fba295077e78a5cb0918ffd99
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-16 23:07:30 +0900

    [Bronze IV] Title: 녹색거탑, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/24723. 녹색거탑/녹색거탑.java
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -1,15 +1,20 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
+        int ans = (int) Math.pow(2, n);
+        sb.append(ans);
 
-        System.out.println((int)Math.pow(2,n));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
-
