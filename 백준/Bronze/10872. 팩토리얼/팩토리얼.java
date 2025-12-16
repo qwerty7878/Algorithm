86b0dccc7ab1d5d09c512a483cd46e4abf1b0286
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-16 23:08:55 +0900

    [Bronze III] Title: 팩토리얼, Time: 112 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,16 +1,23 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args)  throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        System.out.println(factorial(n));
+        sb.append(factorial(n));
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 
-    static long factorial(int n) {
+    private static long factorial(int n) {
         if (n <= 1) {
             return 1;
         }
