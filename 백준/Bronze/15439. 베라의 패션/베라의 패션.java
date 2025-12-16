6e81b0025d7fe7b5e8be71f05b0480a9614e8854
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-16 23:05:43 +0900

    [Bronze IV] Title: 베라의 패션, Time: 108 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/15439. 베라의 패션/베라의 패션.java
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -1,15 +1,19 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
+        sb.append(n * (n - 1));
 
-        System.out.println(n * (n-1));
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
-
