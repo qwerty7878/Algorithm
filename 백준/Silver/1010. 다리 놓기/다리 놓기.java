d1655f9effddc22fb6c36dc576d864cf887c01ea
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-17 22:06:07 +0900

    [Silver V] Title: 다리 놓기, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+        int dp[][] = new int[30][30];
+
+        for (int i = 0; i < 30; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+        }
+
+        for (int i = 1; i < 30; i++) {
+            for (int j = 1; j < 30; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(dp[m][n]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
