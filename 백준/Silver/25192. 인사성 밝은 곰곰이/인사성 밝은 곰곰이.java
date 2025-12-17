b2b064bf9f76c277563e4d2acce05a125ddec5d3
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-18 03:22:11 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 240 ms, Memory: 28300 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -1,29 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
-        HashSet<String> h = new HashSet<>();
+        int n = Integer.parseInt(br.readLine());
+        Set<String> name = new HashSet<>();
+        int count = 0;
 
-        int cnt = 0;
-        for (int i = 0; i < t; i++) {
-            String s = br.readLine();
-
-            if (s.equals("ENTER")) {
-                h.clear();
+        for (int i = 0; i < n; i++) {
+            String cmd = br.readLine();
+            if (cmd.equals("ENTER")) {
+                count += name.size();
+                name = new HashSet<>();
             } else {
-                if (h.contains(s)) {
-                    continue;
-                }
-                h.add(s);
-                cnt++;
+                name.add(cmd);
             }
         }
-        sb.append(cnt);
-        System.out.println(sb);
+        count += name.size();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append("\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
