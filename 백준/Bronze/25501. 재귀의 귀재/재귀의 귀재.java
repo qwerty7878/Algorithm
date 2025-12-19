ef7faf15cc026bc03b144a0647e1e3fa976b19b4
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-19 17:41:55 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 204 ms, Memory: 19176 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -1,23 +1,42 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class Main{
+public class Main {
+    static int count;
 
-    static int cnt = 0;
-    public static int recursion(String s, int l, int r){
-        cnt++;
-        if(l >= r) return 1;
-        else if(s.charAt(l) != s.charAt(r)) return 0;
-        else return recursion(s, l+1, r-1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            count = 0;
+            sb.append(isPalindrome(s)).append(" ").append(count).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
-    public static int isPalindrome(String s){
-        return recursion(s, 0, s.length()-1);
+
+    public static int recursion(String s, int l, int r) {
+        count++;
+        if (l >= r) {
+            return 1;
+        } else if (s.charAt(l) != s.charAt(r)) {
+            return 0;
+        } else {
+            return recursion(s, l + 1, r - 1);
+        }
     }
-    public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-       int T = sc.nextInt();
-       for(int i = 0; i < T; i++){
-            cnt = 0;
-            System.out.println(isPalindrome(sc.next()) + " " + cnt);
-       }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
     }
 }
