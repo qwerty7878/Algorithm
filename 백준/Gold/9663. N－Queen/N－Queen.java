bfa6b4bc2d468d0945973efd59771deabc31dad9
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-28 12:52:39 +0900

    [Gold IV] Title: N-Queen, Time: 6204 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,39 +1,52 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
     static int n;
-    static int cnt = 0;
+    static int count = 0;
     static int arr[];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
+        backTracking(0);
 
-        nqueen(0);
-        System.out.println(cnt);
+        StringBuilder sb = new StringBuilder();
+        sb.append(count);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 
-    static void nqueen(int depth) {
-        if (n == depth) {
-            cnt++;
+    private static void backTracking(int depth) {
+        if (depth == n) {
+            count++;
             return;
         }
+
         for (int i = 0; i < n; i++) {
             arr[depth] = i;
-            if (isPossible(depth)) {
-                nqueen(depth + 1);
+            if (isAttack(depth)) {
+                backTracking(depth + 1);
             }
         }
     }
 
-    static boolean isPossible(int depth) {
-        for (int i = 0; i < depth; i++) {
-            if (arr[depth] == arr[i]) {
+    private static boolean isAttack(int col) {
+        for (int i = 0; i < col; i++) {
+//            정중앙일 경우
+            if (arr[i] == arr[col]) {
                 return false;
-            } else if (Math.abs(arr[depth] - arr[i]) == Math.abs(depth - i)) {
+            }
+//            대각선인 경우
+            else if (Math.abs(col - i) == Math.abs(arr[col] - arr[i])) {
                 return false;
             }
         }
