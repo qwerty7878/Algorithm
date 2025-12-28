ba8fdb62b929756f0cba21b44aa4d9871bde67b8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-28 09:20:19 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int arr[];
+    static boolean isUsed[];
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        isUsed = new boolean[n];
+        backTracking(0, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void backTracking(int depth, int start) {
+        if (depth == m) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                arr[depth] = i + 1;
+                backTracking(depth + 1, i + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+}
