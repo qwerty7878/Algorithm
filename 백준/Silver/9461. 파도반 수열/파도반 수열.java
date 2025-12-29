5d082cd71801061024f2a52d71e961d864845cd2
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2025-12-29 11:07:44 +0900

    [Silver III] Title: 파도반 수열, Time: 112 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,29 +1,42 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int t = Integer.parseInt(br.readLine());
 
-        long dp[];
+        long dp[] = new long[101];
 
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[6] = 3;
+        dp[7] = 4;
+        dp[8] = 5;
+        dp[9] = 7;
+        dp[10] = 9;
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            dp = new long[101];
 
-            dp[1] = 1;
-            dp[2] = 1;
-            dp[3] = 1;
-            dp[4] = 2;
-            dp[5] = 2;
-
-            for (int j = 6; j <= n; j++) {
-                dp[j] = dp[j - 5] + dp[j - 1];
+            for (int j = 11; j <= n; j++) {
+                dp[j] = dp[j - 2] + dp[j - 3];
             }
-            System.out.println(dp[n]);
+
+            sb.append(dp[n]).append("\n");
         }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
