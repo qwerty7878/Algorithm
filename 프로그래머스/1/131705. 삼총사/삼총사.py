6d4cfcde1c1a000c832ba6d51dbdc08d91b220be
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-01-28 19:21:53 +0900

    [level 1] Title: 삼총사, Time: 0.09 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,8 +1,8 @@
 def solution(number):
     answer = 0
-    for a in range(len(number)):
-        for b in range(a + 1, len(number)):
-            for c in range(b + 1, len(number)):
-                if number[a] + number[b] + number[c] == 0:
+    for i in range(len(number)):
+        for j in range(i + 1, len(number)):
+            for k in range(j + 1, len(number)):
+                if number[i] + number[j] + number[k] == 0:
                     answer += 1
     return answer
