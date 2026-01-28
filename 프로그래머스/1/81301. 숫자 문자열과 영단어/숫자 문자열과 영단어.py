7dbdd544478b6e81d9b3f8ab5d0beb0e655441d5
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-01-29 00:49:36 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,25 +1,24 @@
 def solution(s):
     answer = 0
-    
-    if 'zero' in s:
-        s = s.replace('zero', '0')
-    if 'one' in s:
+    if "one" in s:
         s = s.replace('one', '1')
-    if 'two' in s:
+    if "two" in s:
         s = s.replace('two', '2')
-    if 'three' in s:
+    if "three" in s:
         s = s.replace('three', '3')
-    if 'four' in s:
+    if "four" in s:
         s = s.replace('four', '4')
-    if 'five' in s:
+    if "five" in s:
         s = s.replace('five', '5')
-    if 'six' in s:
+    if "six" in s:
         s = s.replace('six', '6')
-    if 'seven' in s:
+    if "seven" in s:
         s = s.replace('seven', '7')
-    if 'eight' in s:
+    if "eight" in s:
         s = s.replace('eight', '8')
-    if 'nine' in s:
+    if "nine" in s:
         s = s.replace('nine', '9')
+    if "zero" in s:
+        s = s.replace('zero', '0')
     answer = int(s)
     return answer
