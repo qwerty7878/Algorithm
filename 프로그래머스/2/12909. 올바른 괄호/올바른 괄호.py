17fbbdfeee4d6caacd6b8a2e26963a23cadd91f6
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-04 20:12:58 +0900

    [level 2] Title: 올바른 괄호, Time: 9.55 ms, Memory: 9.44 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,25 @@
 def solution(s):
     answer = True
-    st = []
+    temp = []
     
-    for i in s:
-        if len(st) == 0:
-            st.append(i)
-        else:
-            if st[-1] == '(' and i == ')':
-                st.pop()
+    for char in s:
+# 첫번째 문자
+        if not temp:
+#         ) 라면 바로 잘못된 형식임
+            if char == ')':
+                return False
+#         첫번째 값 임시 저장
+            temp.append(char)
+#     두번째 부터 형식 비교
+        elif temp[-1] == '(' :
+#     () 형태면 없애기
+            if char == ')':
+                temp.pop()
+#             ( ( 라면 더하기
             else:
-                st.append(i)
-    if len(st) != 0:
-        answer = False
-    return answer
+                temp.append(char)
+#     빈 상태가 아니라면
+    if temp:
+        return False
+    else:
+        return True
