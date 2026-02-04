b2a296d7cf0a988b6dc3f70d796aa66a3b9273d9
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-04 22:29:14 +0900

    [level 2] Title: 더 맵게, Time: 2060.76 ms, Memory: 50.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,27 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+#     기존 리스트 -> 힙으로 변환
+    heapq.heapify(scoville)
+    # print(scoville)
+    if scoville[0] >= K:
+        return answer
+    
+    while scoville[0] < K:
+#         2개의 수를 뽑을 수 없는 경우
+        if len(scoville) < 2:
+            return -1
+#         첫번쨰와 두번째 수 추출
+        first = heapq.heappop(scoville)
+        # print(first)
+        second = heapq.heappop(scoville)
+#         섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+        newScovilleNum = first + (second * 2)
+        heapq.heappush(scoville, newScovilleNum)
+        answer += 1
+        
+        #         모든 수가 K 이상이 되었을 경우
+        if all(num >= K for num in scoville):
+            return answer
+        
