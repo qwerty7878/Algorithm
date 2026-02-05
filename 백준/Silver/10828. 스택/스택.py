2e7ea2f3f82758b6583d68fe823c4bfc9758639e
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-05 20:49:35 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,30 +1,27 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
+n = int(input())
 stack = []
-for i in range(n):
-	command = sys.stdin.readline().split()
+for _ in range(n):
+    command = input().split()
 
-	if command[0] == 'push':
-		stack.append(int(command[1]))
-
-	elif command[0] == 'pop':
-		if len(stack) == 0:
-			print(-1)
-		else:
-			print(stack.pop())
-
-	elif command[0] == 'size':
-		print(len(stack))
-
-	elif command[0] == 'empty':
-		if len(stack) == 0:
-			print(1)
-		else:
-			print(0)
-
-	elif command[0] == 'top':
-		if len(stack) == 0:
-			print(-1)
-		else:
-			print(stack[-1])
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if not stack:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if not stack:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
