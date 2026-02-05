04180b41b0c1a44b711de0f57d1ae1fee9c9c8d8
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-05 21:22:52 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 80 ms, Memory: 34916 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,19 +1,16 @@
 import sys
 from collections import deque
+input = sys.stdin.readline
 
-n,k = map(int, sys.stdin.readline().split())
-dq = deque(range(1, n+1))
-arr = []
+n, k = map(int, input().split())
+queue = deque(num for num in range(1, n + 1))
+# print(queue)
 
-while True:
-    if len(dq) == 1:
-        break
+ans = []
+while queue:
+    for _ in range(k - 1):
+        queue.append(queue.popleft())
+    ans.append(queue.popleft())
+    # print(ans)
 
-    for i in range(k):
-        if (i + 1) != k:
-            dq.append(dq.popleft())
-        elif (i + 1) == k:
-            arr.append(dq.popleft())
-
-arr.append(dq.popleft())
-print(f'<{", ".join(map(str, arr))}>')
+print(f'<{", ".join(map(str, ans))}>')
