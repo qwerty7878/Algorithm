e21d7720f0590eda739c5f51aefcfa1ce89a34cc
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-05 20:59:21 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,33 +1,33 @@
 import sys
 from collections import deque
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
+n = int(input())
 queue = deque()
+for _ in range(n):
+    command = input().split()
 
-for i in range(n):
-    cmd = sys.stdin.readline().split()
-
-    if cmd[0] == 'push':
-        queue.append(cmd[1])
-    elif cmd[0] == 'pop':
-        if len(queue) != 0:
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if not queue:
+            print(-1)
+        else:
             print(queue.popleft())
-        else:
-            print(-1)
-    elif cmd[0] == 'size':
+    elif command[0] == 'size':
         print(len(queue))
-    elif cmd[0] == 'empty':
-        if len(queue) != 0:
-            print(0)
-        else:
+    elif command[0] == 'empty':
+        if not queue:
             print(1)
-    elif cmd[0] == 'front':
-        if len(queue) != 0:
+        else:
+            print(0)
+    elif command[0] == 'front':
+        if not queue:
+            print(-1)
+        else:
             print(queue[0])
-        else:
+    elif command[0] == 'back':
+        if not queue:
             print(-1)
-    elif cmd[0] == 'back':
-        if len(queue) != 0:
+        else:
             print(queue[-1])
-        else:
-            print(-1)
