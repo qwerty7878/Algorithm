cefa6a94aeb4a858a31b6454a2fa047dac540d62
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-05 19:36:05 +0900

    [Silver IV] Title: 설탕 배달, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,17 +1,17 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-cnt = 0
+n = int(input())
 
-while n >= 0:
-    if n % 5 == 0:
-        cnt += n // 5
+count = 0
+while True:
+    if n % 5 != 0 and n % 3 != 0 and n < 5:
+        count = -1
+        break
+    elif n % 5 == 0:
+        count += n // 5
         break
     else:
         n -= 3
-        cnt += 1
-
-if n < 0:
-    print(-1)
-else:
-    print(cnt)
+        count += 1
+print(count)
