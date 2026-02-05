a9c0c7629f503bf48b50ef9fc894ac0b66305cf3
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-06 00:23:49 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,27 +1,35 @@
 import sys
 
-n,m = map(int,sys.stdin.readline().split())
+input = sys.stdin.readline
+
+n, m  = map(int, input().split())
 board = []
 
 for _ in range(n):
-      board.append(list(sys.stdin.readline()))
+    line = input().strip()
+    board.append(line)
 
-ans = sys.maxsize
-for x in range(n - 7):
-      for y in range(m - 7):
-            bcnt = 0
-            wcnt = 0
-            for i in range(x,x + 8):
-                  for j in range(y,y + 8):
-                        if (i + j) % 2 == 0:
-                              if board[i][j] != 'B':
-                                    bcnt += 1
-                              else:
-                                    wcnt += 1
-                        else:
-                              if board[i][j] != 'W':
-                                    bcnt += 1
-                              else:
-                                    wcnt += 1
-            ans = min(ans, bcnt, wcnt)
-print(ans)
+# print(board)
+
+count = 10000
+for i in range(n - 7):
+    for j in range(m - 7):
+        Bcount = 0
+        Wcount = 0
+        for x in range(i, i + 8):
+            for y in range(j, j + 8):
+        #         print(board[x][y], end='')
+        #     print()
+        # print()
+                if (x + y) % 2 == 0:
+                    if board[x][y] != 'B':
+                        Bcount += 1
+                    else:
+                        Wcount += 1
+                else:
+                    if board[x][y] != 'W':
+                        Bcount += 1
+                    else:
+                        Wcount += 1
+        count = min(count, min(Bcount, Wcount))
+print(count)
