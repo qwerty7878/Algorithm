c604346dcfe6656bcdc344ec53b2e86aac90891c
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-06 01:43:20 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,14 @@
+l = int(input())
+s = input().lower()
+alpha = {
+    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14,
+    'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
+}
+
+ans = 0
+pos = 0
+for char in s:
+    if char in alpha:
+        ans += alpha[char] * ((31 ** pos) % 1234567891)
+        pos += 1
+print(ans)
