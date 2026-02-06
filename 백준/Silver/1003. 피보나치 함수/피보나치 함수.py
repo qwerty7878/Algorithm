ca2941918a046c842db81774418c5e75a91e7f4c
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-06 17:20:25 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,17 +1,15 @@
 import sys
+input = sys.stdin.readline
 
-t = int(sys.stdin.readline())
+t = int(input().strip())
+dp = [[]] * 41
+dp[0] = [1, 0]
+dp[1] = [0, 1]
+dp[2] = [1, 1]
 
-for i in range(t):
-    n = int(sys.stdin.readline())
-
-    dp = [[]] * 41
-    dp[0] = [1, 0]
-    dp[1] = [0, 1]
-    # n - 2, n - 1 이니까
-    dp[2] = [1, 1]
+for _ in range(t):
+    n = int(input().strip())
 
     for j in range(3, n + 1):
-        dp[j] = [dp[j - 1][0] + dp[j - 2][0], dp[j - 1][1] + dp[j - 2][1]]
-
+        dp[j] = dp[j - 1][0] + dp[j - 2][0], dp[j - 1][1] + dp[j - 2][1]
     print(dp[n][0], dp[n][1])
