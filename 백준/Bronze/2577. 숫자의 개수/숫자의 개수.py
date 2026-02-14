b919870001ea7a2fd8407c86fe3e30e40c02fa2a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-14 23:16:41 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,7 +1,14 @@
 a = int(input())
 b = int(input())
 c = int(input())
-res = a * b * c
+total = a * b * c
 
-for i in range(0, 10):
-	print(str(res).count(str(i)))
+arr = [0] * 10
+
+for char in str(total):
+    for idx in range(10):
+        if char == str(idx):
+            arr[idx] += 1
+            break
+
+print('\n'.join(map(str, arr)))
