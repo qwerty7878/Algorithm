669b2b34eeca1ded6204fc09d56187a069e6ee39
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-02-26 01:19:18 +0900

    [level 2] Title: 피보나치 수, Time: 507.61 ms, Memory: 455 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,9 +1,10 @@
+
 def solution(n):
-    arr = [0] * (n + 1)
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
     
-    arr[1] = 1
-    arr[0] = 0
-    
-    for i in range(2,n + 1):
-        arr[i] = arr[i - 2] + arr[i - 1]
-    return arr[n] % 1234567
+    for idx in range(2, n + 1):
+        dp[idx] = dp[idx - 1] + dp[idx - 2]
+    answer = dp[n] % 1234567
+    return answer
