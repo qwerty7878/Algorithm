5b32a3e476a7b6b7e3f0da5ade37ba1f6878151b
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-03-08 06:31:22 +0900

    [Silver I] Title: IOIOI, Time: 316 ms, Memory: 33388 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,16 +2,24 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-p = 'I' + ('OI' * n)
-
 m = int(input())
 s = input()
 
-count = 0
-for idx in range(len(s) - len(p) + 1):
-    # print(s[idx:idx + len(p)])
-    target = s[idx:idx + len(p)]
+answer, idx, count = 0, 0, 0
 
-    if target == p:
+while idx < (m - 1):
+    if s[idx:idx + 3] == 'IOI':
+        # 다음은 OI 이니까 2를 더함
+        idx += 2
         count += 1
-print(count)
+
+        # 범위까지 늘리며 확인하기
+        if count == n:
+            answer += 1
+            # 하나 확인했으니 바로 옆에가 맞는지 확인하기 위해서 1만 줄임
+            count -= 1
+    else:
+        # OOI 같은 경우
+        idx += 1
+        count = 0
+print(answer)
