7b3704ca820c05264fc55047b035706c2d2495c4
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-03-29 22:14:48 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,13 +2,11 @@ def solution(array, commands):
     answer = []
     
     for command in commands:
-        start = command[0]
-        end = command[1]
-        target = command[2]
-        
+        start, end, target = command[0], command[1], command[2]
         temp = []
-        for num in range(start - 1, end):
-            temp.append(array[num])
+        for idx in range(start - 1, end):
+            temp.append(array[idx])
+        # print(temp)
         temp.sort()
         answer.append(temp[target - 1])
     return answer
