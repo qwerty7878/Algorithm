b2cc78cd76d7def7cb390fd65999de357c0242ea
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-03-31 19:59:40 +0900

    [level 1] Title: 시저 암호, Time: 1.88 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,11 +1,12 @@
 def solution(s, n):
     answer = ''
+    
     for char in s:
         if char == ' ':
             answer += ' '
         else:
             if char.islower():
-                answer += chr((ord(char) - ord('a') + n) % 26 + ord('a'))
-            elif char.isupper():
-                answer += chr((ord(char) - ord('A') + n) % 26 + ord('A'))
+                answer += chr(((ord(char) + n - ord('a')) % 26) + ord('a'))
+            else:
+                answer += chr(((ord(char) + n - ord('A')) % 26) + ord('A'))
     return answer
