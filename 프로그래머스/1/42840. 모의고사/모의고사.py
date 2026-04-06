ca67b4b8320c655aceec4ba1e4e9287592cc5ce4
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-04-06 18:28:03 +0900

    [level 1] Title: 모의고사, Time: 7.12 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,30 +1,27 @@
+def check(answers, arr):
+    count = 0
+    
+    for idx in range(len(answers)):
+        if answers[idx] == arr[idx % len(arr)]:
+            count += 1
+    return count
+
 def solution(answers):
     answer = []
     
-    forgive_1 = [1,2,3,4,5]
-    forgive_2 = [2,1,2,3,2,4,2,5]
-    forgive_3 = [3,3,1,1,2,2,4,4,5,5]
+    sp1 = [1,2,3,4,5]
+    sp2 = [2,1,2,3,2,4,2,5]
+    sp3 = [3,3,1,1,2,2,4,4,5,5]
     
-    f1_count = 0
-    f2_count = 0
-    f3_count = 0
+    maxnum = max(check(answers, sp1), max(check(answers, sp2), check(answers, sp3)))
     
-    for idx in range(len(answers)):
-        if forgive_1[idx % len(forgive_1)] == answers[idx]:
-            f1_count += 1
-            
-        if forgive_2[idx % len(forgive_2)] == answers[idx]:
-            f2_count += 1
-            
-        if forgive_3[idx % len(forgive_3)] == answers[idx]:
-            f3_count += 1
+    if maxnum == check(answers, sp1):
+        answer.append(1)
+    
+    if maxnum == check(answers, sp2):
+        answer.append(2)
+    
+    if maxnum == check(answers, sp3):
+        answer.append(3)
         
-        if idx + 1 == len(answers):
-            break
-
-    most_collect = max(f1_count, max(f2_count,f3_count))
-    forgivers = [f1_count, f2_count, f3_count]
-    for idx in range(len(forgivers)):
-        if forgivers[idx] == most_collect:
-            answer.append(idx + 1)
     return answer
