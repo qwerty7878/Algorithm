45b6c6678f8aaf664d10de79a5cd852780f6d9ed
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-04-06 16:49:15 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,13 +1,17 @@
 def gcd(a, b):
     if b == 0:
         return a
-    return gcd(b, a % b)
+    else:
+        return gcd(b, a % b)
 
 def lcm(a, b):
     return (a * b) // gcd(a, b)
 
 def solution(arr):
-    answer = lcm(arr[0], arr[1])
-    for idx in range(2, len(arr)):
-        answer = lcm(answer, arr[idx])
+    answer = 0
+    temp = lcm(arr[0], arr[1])
+    for i in range(2, len(arr)):
+        temp = lcm(temp, arr[i])
+    answer = temp
+    
     return answer
