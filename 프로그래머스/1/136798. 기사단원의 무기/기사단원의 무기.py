5fb95176a521db9bdc102f433c869af029c2bcb6
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-04-30 17:41:42 +0900

    [level 1] Title: 기사단원의 무기, Time: 958.56 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,19 +1,16 @@
 def solution(number, limit, power):
     answer = 0
     
-    arr = []
     for num in range(1, number + 1):
-        count = 0
-        for div in range(1, int(num ** 0.5) + 1):
-            if num % div == 0:
-                count += 1
-                if div != num // div:
-                    count += 1
-        arr.append(count)
-    # print(arr)
-    
-    for idx in range(len(arr)):
-        if arr[idx] > limit:
-            arr[idx] = power
-        answer += arr[idx]
+        cnt = 0
+        for i in range(1, int(num ** 0.5) + 1):
+            if num % i == 0:
+                if i * i == num:
+                    cnt += 1
+                else:
+                    cnt += 2
+        if cnt > limit:
+            answer += power
+        else:
+            answer += cnt
     return answer
