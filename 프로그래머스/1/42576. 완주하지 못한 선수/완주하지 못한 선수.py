e1f8d23e4e558b53fea25cbf246bf9653ce6c4d9
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-04-30 18:14:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.80 ms, Memory: 25.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,10 +2,11 @@ def solution(participant, completion):
     answer = ''
     participant.sort()
     completion.sort()
-    for p_runner, c_runner in zip(participant, completion):
-        if p_runner != c_runner:
-            answer = p_runner
-            return answer
     
-    answer = participant[-1]
+    for idx in range(len(completion)):
+        if participant[idx] != completion[idx]:
+            answer = participant[idx]
+            break
+    else:
+        answer = participant[-1]    
     return answer
