de58ad962ba5a20bb3a82278860249b4bddabba0
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-06-16 21:53:00 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,10 @@
+def solution(id_pw, db):
+    answer = ''
+    
+    for user in db:
+        if user[0] == id_pw[0]:
+            if user[1] == id_pw[1]:
+                return 'login'
+            else:
+                return 'wrong pw'
+    return 'fail'
