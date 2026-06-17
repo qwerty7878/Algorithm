5b9e537fa4d4d7ccc4ef4160239e1117950baa3f
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-06-17 13:48:43 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.29 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,13 +1,13 @@
 def solution(n, control):
-    answer = 0
+    answer = n
+    
     for char in control:
         if char == 'w':
-            n += 1
+            answer += 1
         elif char == 's':
-            n -= 1
+            answer -= 1
         elif char == 'd':
-            n += 10
+            answer += 10
         elif char == 'a':
-            n -= 10
-    answer = n
+            answer -= 10
     return answer
