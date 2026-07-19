20c8ea806df105f27f956f7f1eac291c1fe9fcf9
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-07-19 15:24:43 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.00 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.py
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.py
@@ -1,6 +1,6 @@
 def solution(num_list, n):
     answer = []
-    for idx in range(len(num_list)):
-        if n - 1 >= idx:
-            answer.append(num_list[idx])
+    
+    for idx in range(n):
+        answer.append(num_list[idx])
     return answer
