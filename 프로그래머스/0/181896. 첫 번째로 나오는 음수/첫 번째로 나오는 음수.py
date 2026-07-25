d061f61b9d7f66e4edfbf6ff26746fc6dd42a659
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-07-25 16:49:24 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,11 +1,7 @@
 def solution(num_list):
-    answer = 0
-    isMinus = False
+    answer = -1
     for idx in range(len(num_list)):
         if num_list[idx] < 0:
-            isMinus = True
             answer = idx
             break
-    if isMinus == False:
-        answer = -1
     return answer
