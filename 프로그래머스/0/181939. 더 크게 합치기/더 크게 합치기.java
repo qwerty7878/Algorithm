2f0379cd422d3eb17e6d3e9c014288906a38b71a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-08-11 15:11:18 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.13 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,18 +1,9 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        
-        String aa = Integer.toString(a);
-        String bb = Integer.toString(b);
-        String ab = aa + bb;
-        String ba = bb + aa;
-        
-        int aab = Integer.valueOf(ab);
-        int bba = Integer.valueOf(ba);
-        
-        if(aab >= bba)
-            return aab;
-        else
-            return bba;
+        String aPb = "" + String.valueOf(a) + String.valueOf(b);
+        String bPa = "" + String.valueOf(b) + String.valueOf(a);
+        answer = Math.max(Integer.parseInt(aPb), Integer.parseInt(bPa));
+        return answer;
     }
 }
