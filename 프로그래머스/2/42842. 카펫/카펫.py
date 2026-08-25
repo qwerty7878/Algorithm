38ed4b664202942cb5c81426312a420ba815e982
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-08-25 14:07:20 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,9 +1,10 @@
 def solution(brown, yellow):
+    
     total = brown + yellow
-    for h in range(1, int(total ** 0.5) + 1):
+    for h in range(3, int(total ** 0.5) + 1):
         if total % h == 0:
             w = total // h
-
-            border = 2 * w + 2 * h - 4
-            if border == brown:
-                return [w, h]
+            if (w - 2) * (h - 2) == yellow:
+                break
+            
+    return [w, h]
