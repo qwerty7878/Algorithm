66cce52e60200bed265f240761145a13d122934a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-08-31 16:46:04 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,5 +1,6 @@
 def solution(seoul):
     answer = ''
-    idx = seoul.index('Kim')
-    answer = f"김서방은 {idx}에 있다"
+    
+    if "Kim" in seoul:
+        answer = f"김서방은 {seoul.index("Kim")}에 있다"
     return answer
