cf82fd2b7dc17a6f8a6bd422db99e15ed8a9677a
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-08-31 17:06:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,5 +1,3 @@
 def solution(phone_number):
-    answer = ''
-    answer = (len(phone_number) - 4) * '*'
-    answer += phone_number[len(phone_number) - 4:]
+    answer = '*' * (len(phone_number) - 4) + phone_number[(len(phone_number) - 4):]
     return answer
