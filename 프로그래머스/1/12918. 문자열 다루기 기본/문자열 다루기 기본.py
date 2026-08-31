49659349e09d88ce540f0f8ab415c171756ef280
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-09-01 00:16:03 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,7 +1,5 @@
 def solution(s):
-    answer = True
-    if s.isdigit() and (len(s) == 4 or len(s) == 6):
-        answer = True
+    if len(s) == 4 or len(s) == 6:
+        return s.isdigit()
     else:
-        answer = False
-    return answer
+        return False
