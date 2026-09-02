7af823915a7df48b315e3560b52febfc3f47152d
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-09-02 23:41:34 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,9 +1,11 @@
+from itertools import combinations
+
 def solution(number):
     answer = 0
     
-    for i in range(len(number)):
-        for j in range(i + 1, len(number)):
-            for k in range(j + 1, len(number)):
-                if number[i] + number[j] + number[k] == 0:
-                    answer += 1
+    for combi in combinations(number, 3):
+        # print(combi)
+        if sum(combi) == 0:
+            # print(combi)
+            answer += 1
     return answer
