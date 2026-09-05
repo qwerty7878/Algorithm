d51d4a211169968b2b2c2c1640863fcc90e0c671
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-09-06 05:52:29 +0900

    [level 1] Title: 콜라 문제, Time: 0.23 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,13 +1,8 @@
 def solution(a, b, n):
     answer = 0
-
-    while True:
+    
+    rest = 0
+    while n >= a:
         answer += (n // a) * b
-        # print(answer)
-        rest = n % a
-        new = (n // a) * b
-        n = new + rest
-        if n < a:
-            break
-            
+        n = (n // a) * b + (n % a) 
     return answer
