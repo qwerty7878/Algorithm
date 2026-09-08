6c6b656dc6ce3e1c24dafca88963f6b15809c329
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-09-08 22:46:21 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.89 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,22 +1,17 @@
+# 초기에 k일까지는 모든 출연 가수의 점수가 명예의 전당에 오르게 됩니다. k일 다음부터는 출연 가수의 점수가 기존의 명예의 전당 목록의 k번째 순위의 가수 점수보다 더 높으면, 출연 가수의 점수가 명예의 전당에 오르게 되고 기존의 k번째 순위의 점수는 명예의 전당에서 내려오게 됩니다.
+# 이 프로그램에서는 매일 "명예의 전당"의 최하위 점수를 발표합니다.
+
 def solution(k, score):
     answer = []
-    li = []
     
+    temp = []
     for num in score:
-        if len(li) < k:
-            if not li:
-                li.append(num)
-                answer.append(num)
-            else:
-                li.append(num)
-                li.sort()
-                answer.append(li[0])
-        elif len(li) == k:
-            if num > li[0]:
-                li.remove(li[0])
-                li.append(num)
-                li.sort()
-                answer.append(li[0])
-            else:
-                answer.append(li[0])
+        if len(temp) < k:
+            temp.append(num)
+        else:
+            if num > min(temp):
+                temp.remove(min(temp))
+                temp.append(num)
+        answer.append(min(temp))
+        # print(answer)
     return answer
