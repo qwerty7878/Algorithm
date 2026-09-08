173d56d19425432498e4b374d9932e47993ec887
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-09-09 00:56:56 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2804.47 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,16 +1,9 @@
 def solution(elements):
-    answer = 0
-    set_arr = set()
-    copy = elements * 2
-    # print(copy)
-
-    for leng in range(1, len(elements) + 1):
-        # print(leng)
-        for i in range(len(elements)):
-            arr = copy[i: (i + leng)]
-            if len(arr) == leng:
-                # print(arr)
-                set_arr.add(sum(arr))
-    # print(set_arr)
-    answer = len(set_arr)
-    return answer
+    d_elements = elements * 2
+    
+    arr = []
+    for i in range(1, len(elements) + 1):
+        for j in range(len(elements)):
+            temp = sum(d_elements[j:j + i])
+            arr.append(temp)
+    return len(set(arr))
