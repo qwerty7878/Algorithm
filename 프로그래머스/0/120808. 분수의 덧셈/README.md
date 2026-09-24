220db139002d950e0ae442cb01e8e7ac90a0db13
hyeonjune <142138946+qwerty7878@users.noreply.github.com>
2026-09-24 10:19:22 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 59.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
 
 ### 성능 요약
 
-메모리: 9.35 MB, 시간: 0.01 ms
+메모리: 59.4 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 27일 21:18:54
+2026년 09월 24일 10:19:19
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.kt
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.kt
@@ -1,16 +1,17 @@
 class Solution {
-    fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {
-        val resultNumer = (numer1 * denom2) + (numer2 * denom1)
-        val resultDenom = denom1 * denom2
+    fun solution(numer1: Int, denom1: Int, numer2: Int, denom2: Int): IntArray {        
+        var top = (numer1 * denom2) + (numer2 * denom1)
+        var down = denom1 * denom2
         
-        val gcdNum = gcd(resultNumer, resultDenom)
+        val num = gcd(top, down)
         
-        var answer: IntArray = intArrayOf(resultNumer/gcdNum, resultDenom/gcdNum)
-        return answer
+        return intArrayOf(top / num, down / num)
     }
     
-    fun gcd(a: Int, b: Int): Int {
-        if (b == 0) return a
+    fun gcd(a: Int, b: Int): Int{
+        if (b == 0){
+            return a
+        }
         return gcd(b, a % b)
     }
 }
